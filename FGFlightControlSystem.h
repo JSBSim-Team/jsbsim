3ddf78da80516fa9aca6406443cd47d7d4318362
jsb <jsb>
2000-04-04 11:57:41 +0000

    tabs=2 spaces

--- FGFlightControlSystem.h
+++ FGFlightControlSystem.h
@@ -1,25 +1,25 @@
 /*******************************************************************************
-
+ 
  Header:       FGFlightControlSystem.h
  Author:       Tony Peden
  Date started: 12/20/99
-
+ 
  ------------- Copyright (C) 1999  Tony Peden (apeden@earthlink.net) -------------
-
+ 
  This program is free software; you can redistribute it and/or modify it under
  the terms of the GNU General Public License as published by the Free Software
  Foundation; either version 2 of the License, or (at your option) any later
  version.
-
+ 
  This program is distributed in the hope that it will be useful, but WITHOUT
  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  details.
-
+ 
  You should have received a copy of the GNU General Public License along with
  this program; if not, write to the Free Software Foundation, Inc., 59 Temple
  Place - Suite 330, Boston, MA  02111-1307, USA.
-
+ 
  Further information about the GNU General Public License can also be found on
  the world wide web at http://www.gnu.org.
  */
@@ -84,30 +84,38 @@ class FGFilter;
 class FGFDMExec;
 
 class FGFlightControlSystem : public FGModel {
-public: 
-	FGFlightControlSystem(FGFDMExec* tt);
-	
-	~FGFlightControlSystem();
-	
-	bool Run(void);
-	
-	bool LoadControlSystem(ifstream &in);
-	
-	//generic
-	inline void  SetFCSControl(int which, float tt, int n=0) { controls[which]=tt; }
-	inline float GetFCSControl(const int which, int n=0) { return controls[which]; }
-	
-	inline float GetFCSSurface(const int which, int n=0) { return surfaces[which]; }
-	inline void  SetFCSSurface(const int which, float tt, int n=0) { surfaces[which]=tt; }
-	
+public:
+  FGFlightControlSystem(FGFDMExec* tt);
+
+  ~FGFlightControlSystem();
+
+  bool Run(void);
+
+  bool LoadControlSystem(ifstream &in);
+
+  //generic
+  inline void  SetFCSControl(int which, float tt, int n=0) {
+    controls[which]=tt;
+  }
+  inline float GetFCSControl(const int which, int n=0) {
+    return controls[which];
+  }
+
+  inline float GetFCSSurface(const int which, int n=0) {
+    return surfaces[which];
+  }
+  inline void  SetFCSSurface(const int which, float tt, int n=0) {
+    surfaces[which]=tt;
+  }
+
 private:
-	float controls[MAXCONTROLS];
-	float surfaces[MAXSURFACES];
-	int binding[MAXSURFACES];
-	FGFilter* fcslist[MAXSURFACES];
-	
-	
-	SystemMap systemmap;
+  float controls[MAXCONTROLS];
+  float surfaces[MAXSURFACES];
+  int binding[MAXSURFACES];
+  FGFilter* fcslist[MAXSURFACES];
+
+
+  SystemMap systemmap;
 
 };
 
