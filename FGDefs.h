f4e1fe024d0874f034a1af10422b516d4a44c678
jsb <jsb>
2000-05-07 12:44:12 +0000

    Added INVGRAVITY - 1/g

--- FGDefs.h
+++ FGDefs.h
@@ -1,32 +1,32 @@
 /*******************************************************************************
-
+ 
  Header:       FGDefs.h
  Author:       Jon S. Berndt
  Date started: 02/01/99
-
+ 
  ------------- Copyright (C) 1999  Jon S. Berndt (jsb@hal-pc.org) -------------
-
+ 
  This program is free software; you can redistribute it and/or modify it under
  the terms of the GNU General Public License as published by the Free Software
  Foundation; either version 2 of the License, or (at your option) any later
  version.
-
+ 
  This program is distributed in the hope that it will be useful, but WITHOUT
  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  details.
-
+ 
  You should have received a copy of the GNU General Public License along with
  this program; if not, write to the Free Software Foundation, Inc., 59 Temple
  Place - Suite 330, Boston, MA  02111-1307, USA.
-
+ 
  Further information about the GNU General Public License can also be found on
  the world wide web at http://www.gnu.org.
-
+ 
 HISTORY
 --------------------------------------------------------------------------------
 02/01/99  JSB   Created
-
+ 
 ********************************************************************************
 SENTRY
 *******************************************************************************/
@@ -37,6 +37,7 @@ SENTRY
 #define MAX_ENGINES     10
 #define MAX_TANKS       30
 #define GRAVITY         32.174
+#define INVGRAVITY      0.031081
 #define EARTHRAD        20898908.00       // feet
 #define OMEGAEARTH      7.2685E-3         // rad/sec
 #define EARTHRADSQRD    437882827922500.0
