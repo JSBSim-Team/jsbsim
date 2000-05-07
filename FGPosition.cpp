a9688ff42fada083facc6fa512c3db2961f208e0
jsb <jsb>
2000-05-07 12:43:14 +0000

    Added gamma (flight path angle), hdot getter

--- FGPosition.cpp
+++ FGPosition.cpp
@@ -1,39 +1,39 @@
 /*******************************************************************************
-
+ 
  Module:       FGPosition.cpp
  Author:       Jon S. Berndt
  Date started: 01/05/99
  Purpose:      Integrate the EOM to determine instantaneous position
  Called by:    FGFDMExec
-
+ 
  ------------- Copyright (C) 1999  Jon S. Berndt (jsb@hal-pc.org) -------------
-
+ 
  This program is free software; you can redistribute it and/or modify it under
  the terms of the GNU General Public License as published by the Free Software
  Foundation; either version 2 of the License, or (at your option) any later
  version.
-
+ 
  This program is distributed in the hope that it will be useful, but WITHOUT
  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  details.
-
+ 
  You should have received a copy of the GNU General Public License along with
  this program; if not, write to the Free Software Foundation, Inc., 59 Temple
  Place - Suite 330, Boston, MA  02111-1307, USA.
-
+ 
  Further information about the GNU General Public License can also be found on
  the world wide web at http://www.gnu.org.
-
+ 
 FUNCTIONAL DESCRIPTION
 --------------------------------------------------------------------------------
 This class encapsulates the integration of rates and accelerations to get the
 current position of the aircraft.
-
+ 
 HISTORY
 --------------------------------------------------------------------------------
 01/05/99   JSB   Created
-
+ 
 ********************************************************************************
 COMMENTS, REFERENCES,  and NOTES
 ********************************************************************************
@@ -48,7 +48,7 @@ COMMENTS, REFERENCES,  and NOTES
     Wiley & Sons, 1979 ISBN 0-471-03032-5
 [5] Bernard Etkin, "Dynamics of Flight, Stability and Control", Wiley & Sons,
     1982 ISBN 0-471-08936-2
-
+ 
 ********************************************************************************
 INCLUDES
 *******************************************************************************/
@@ -84,28 +84,26 @@ extern double globalSceneryAltitude;
 extern double globalSeaLevelRadius;
 
 FGPosition::FGPosition(FGFDMExec* fdmex) : FGModel(fdmex),
-                                           vUVW(3),
-                                           vVel(3)
-{
+    vUVW(3),
+vVel(3) {
   Name = "FGPosition";
   LongitudeDot = LatitudeDot = RadiusDot = 0.0;
   lastLongitudeDot = lastLatitudeDot = lastRadiusDot = 0.0;
   Longitude = Latitude = 0.0;
   h = 0.0;
+  gamma=Vt=0.0;
   RunwayElevation = 0.0;
 }
 
 /******************************************************************************/
 
-FGPosition::~FGPosition(void)
-{
-}
+FGPosition::~FGPosition(void) {}
 
 /******************************************************************************/
 
-bool FGPosition:: Run(void)
-{
+bool FGPosition:: Run(void) {
   double cosLat;
+  double hdot_Vt;
 
   if (!FGModel::Run()) {
     GetState();
@@ -126,6 +124,13 @@ bool FGPosition:: Run(void)
 
     DistanceAGL = h - RunwayElevation;
 
+    if(Vt > 0) {
+      hdot_Vt=RadiusDot/Vt;
+      //make sure that -Vt <= hdot <= Vt, which, of course, should always be the case
+      if(fabs(hdot_Vt) <= 1) gamma= asin(hdot_Vt);
+    } else
+      gamma=0.0;
+
     lastLatitudeDot = LatitudeDot;
     lastLongitudeDot = LongitudeDot;
     lastRadiusDot = RadiusDot;
@@ -139,12 +144,11 @@ bool FGPosition:: Run(void)
 
 /******************************************************************************/
 
-void FGPosition::GetState(void)
-{
+void FGPosition::GetState(void) {
   dt = State->Getdt();
 
   vUVW = Translation->GetUVW();
-
+  Vt = Translation->GetVt();
   invMass = 1.0 / Aircraft->GetMass();
   invRadius = 1.0 / (h + EARTHRAD);
   Radius = h + EARTHRAD;
--- FGPosition.h
+++ FGPosition.h
@@ -1,36 +1,36 @@
 /*******************************************************************************
-
+ 
  Header:       FGPosition.h
  Author:       Jon S. Berndt
  Date started: 1/5/99
-
+ 
  ------------- Copyright (C) 1999  Jon S. Berndt (jsb@hal-pc.org) -------------
-
+ 
  This program is free software; you can redistribute it and/or modify it under
  the terms of the GNU General Public License as published by the Free Software
  Foundation; either version 2 of the License, or (at your option) any later
  version.
-
+ 
  This program is distributed in the hope that it will be useful, but WITHOUT
  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  details.
-
+ 
  You should have received a copy of the GNU General Public License along with
  this program; if not, write to the Free Software Foundation, Inc., 59 Temple
  Place - Suite 330, Boston, MA  02111-1307, USA.
-
+ 
  Further information about the GNU General Public License can also be found on
  the world wide web at http://www.gnu.org.
-
+ 
 HISTORY
 --------------------------------------------------------------------------------
 01/05/99   JSB   Created
-
+ 
 ********************************************************************************
 COMMENTS, REFERENCES,  and NOTES
 ********************************************************************************
-
+ 
 ********************************************************************************
 SENTRY
 *******************************************************************************/
@@ -51,8 +51,7 @@ using namespace std;
 CLASS DECLARATION
 *******************************************************************************/
 
-class FGPosition : public FGModel
-{
+class FGPosition : public FGModel {
   FGColumnVector vUVW;
   FGColumnVector vVel;
 
@@ -64,6 +63,8 @@ class FGPosition : public FGModel
   float dt;
   double RunwayElevation;
   double DistanceAGL;
+  double gamma;
+  double Vt;
 
   void GetState(void);
 
@@ -71,23 +72,61 @@ public:
   FGPosition(FGFDMExec*);
   ~FGPosition(void);
 
-  inline FGColumnVector GetVel(void) {return vVel;}
-  inline FGColumnVector GetUVW(void) {return vUVW;}
-  inline double GetVn(void)  {return vVel(1);}
-  inline double GetVe(void)  {return vVel(2);}
-  inline double GetVd(void)  {return vVel(3);}
-  inline double Geth(void)  {return h;}
-  inline double GetLatitude(void) {return Latitude;}
-  inline double GetLongitude(void) {return Longitude;}
-  inline double GetRunwayElevation(void) {return RunwayElevation;}
-  inline double GetDistanceAGL(void)  {return DistanceAGL;}
+  inline FGColumnVector GetVel(void) {
+    return vVel;
+  }
+  inline FGColumnVector GetUVW(void) {
+    return vUVW;
+  }
+  inline double GetVn(void)  {
+    return vVel(1);
+  }
+  inline double GetVe(void)  {
+    return vVel(2);
+  }
+  inline double GetVd(void)  {
+    return vVel(3);
+  }
+  inline double Geth(void)  {
+    return h;
+  }
+  inline double Gethdot(void) {
+    return RadiusDot;
+  }
+  inline double GetLatitude(void) {
+    return Latitude;
+  }
+  inline double GetLongitude(void) {
+    return Longitude;
+  }
+  inline double GetRunwayElevation(void) {
+    return RunwayElevation;
+  }
+  inline double GetDistanceAGL(void)  {
+    return DistanceAGL;
+  }
+  inline double GetGamma(void) {
+    return gamma;
+  }
 
-  void SetvVel(const FGColumnVector& v) {vVel = v;}
-  void SetLatitude(float tt) {Latitude = tt;}
-  void SetLongitude(double tt) {Longitude = tt;}
-  void Seth(double tt) {h = tt;}
-  void SetRunwayElevation(double tt) {RunwayElevation = tt;}
-  void SetDistanceAGL(double tt) {DistanceAGL = tt;}
+  void SetvVel(const FGColumnVector& v) {
+    vVel = v;
+  }
+  void SetLatitude(float tt) {
+    Latitude = tt;
+  }
+  void SetLongitude(double tt) {
+    Longitude = tt;
+  }
+  void Seth(double tt) {
+    h = tt;
+  }
+  void SetRunwayElevation(double tt) {
+    RunwayElevation = tt;
+  }
+  void SetDistanceAGL(double tt) {
+    DistanceAGL = tt;
+  }
 
   bool Run(void);
 };
