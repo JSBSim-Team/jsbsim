caab762ce19f25cb3ac0e674134cb010524596d1
jsb <jsb>
2000-05-11 11:21:49 +0000

    Added setters for body speeds, engineering units

--- FGInitialCondition.cpp
+++ FGInitialCondition.cpp
@@ -1,49 +1,49 @@
 /*******************************************************************************
-
+ 
  Header:       FGInitialCondition.cpp
  Author:       Tony Peden
  Date started: 7/1/99
-
+ 
  ------------- Copyright (C) 1999  Anthony K. Peden (apeden@earthlink.net) -------------
-
+ 
  This program is free software; you can redistribute it and/or modify it under
  the terms of the GNU General Public License as published by the Free Software
  Foundation; either version 2 of the License, or (at your option) any later
  version.
-
+ 
  This program is distributed in the hope that it will be useful, but WITHOUT
  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  details.
-
+ 
  You should have received a copy of the GNU General Public License along with
  this program; if not, write to the Free Software Foundation, Inc., 59 Temple
  Place - Suite 330, Boston, MA  02111-1307, USA.
-
+ 
  Further information about the GNU General Public License can also be found on
  the world wide web at http://www.gnu.org.
-
-
+ 
+ 
  HISTORY
 --------------------------------------------------------------------------------
 7/1/99   TP   Created
-
-
+ 
+ 
 FUNCTIONAL DESCRIPTION
 --------------------------------------------------------------------------------
-
+ 
 The purpose of this class is to take a set of initial conditions and provide
 a kinematically consistent set of body axis velocity components, euler
 angles, and altitude.  This class does not attempt to trim the model i.e.
 the sim will most likely start in a very dynamic state (unless, of course,
 you have chosen your IC's wisely) even after setting it up with this class.
-
+ 
 CAVEAT: This class makes use of alpha=theta-gamma. This means that setting
         any of the three with this class is only valid for steady state
         (all accels zero) and zero pitch rate.  One example where this
         would produce invalid results is setting up for a trim in a pull-up
         or pushover (both have nonzero pitch rate).  Maybe someday...
-
+ 
 ********************************************************************************
 INCLUDES
 *******************************************************************************/
@@ -65,171 +65,196 @@ INCLUDES
 
 
 
-FGInitialCondition::FGInitialCondition(FGFDMExec *FDMExec)
-{
-    vt=vc=ve=0;
-    mach=0;
-    alpha=beta=gamma=0;
-    theta=phi=psi=0;
-    altitude=hdot=0;
-    latitude=longitude=0;
-    lastSpeedSet=setvt;
-    if(fdmex != NULL ) {
-        fdmex=FDMExec;
-        fdmex->GetPosition()->Seth(altitude);
-        fdmex->GetAtmosphere()->Run();
-    } else {
-        cout << "FGInitialCondition: This class requires a pointer to an initialized FGFDMExec object" << endl;
-    }
+FGInitialCondition::FGInitialCondition(FGFDMExec *FDMExec) {
+  vt=vc=ve=0;
+  mach=0;
+  alpha=beta=gamma=0;
+  theta=phi=psi=0;
+  altitude=hdot=0;
+  latitude=longitude=0;
+  u=v=w=0;
+  lastSpeedSet=setvt;
+  if(fdmex != NULL ) {
+    fdmex=FDMExec;
+    fdmex->GetPosition()->Seth(altitude);
+    fdmex->GetAtmosphere()->Run();
+  } else {
+    cout << "FGInitialCondition: This class requires a pointer to an initialized FGFDMExec object" << endl;
+  }
 
 }
 
 
-FGInitialCondition::~FGInitialCondition(void) {};
+FGInitialCondition::~FGInitialCondition(void) {}
+;
 
 
-void FGInitialCondition::SetVcalibratedKtsIC(float tt)
-{
+void FGInitialCondition::SetVcalibratedKtsIC(float tt) {
 
-    if(getMachFromVcas(&mach,tt*KTSTOFPS)) {
-        //cout << "Mach: " << mach << endl;
-        lastSpeedSet=setvc;
-        vc=tt*KTSTOFPS;
-        vt=mach*fdmex->GetAtmosphere()->GetSoundSpeed();
-        ve=vt*sqrt(fdmex->GetAtmosphere()->GetDensityRatio());
-        //cout << "Vt: " << vt*FPSTOKTS << " Vc: " << vc*FPSTOKTS << endl;
-    } else {
-        cout << "Failed to get Mach number for given Vc and altitude, Vc unchanged." << endl;
-        cout << "Please mail the set of initial conditions used to apeden@earthlink.net" << endl;
-    }
+  if(getMachFromVcas(&mach,tt*KTSTOFPS)) {
+    //cout << "Mach: " << mach << endl;
+    lastSpeedSet=setvc;
+    vc=tt*KTSTOFPS;
+    vt=mach*fdmex->GetAtmosphere()->GetSoundSpeed();
+    ve=vt*sqrt(fdmex->GetAtmosphere()->GetDensityRatio());
+    //cout << "Vt: " << vt*FPSTOKTS << " Vc: " << vc*FPSTOKTS << endl;
+  }
+  else {
+    cout << "Failed to get Mach number for given Vc and altitude, Vc unchanged." << endl;
+    cout << "Please mail the set of initial conditions used to apeden@earthlink.net" << endl;
+  }
 }
 
 void FGInitialCondition::SetVequivalentKtsIC(float tt) {
-    ve=tt*KTSTOFPS;
-    lastSpeedSet=setve;
-    vt=ve*1/sqrt(fdmex->GetAtmosphere()->GetDensityRatio());
-    mach=vt/fdmex->GetAtmosphere()->GetSoundSpeed();
-    vc=calcVcas(mach);
+  ve=tt*KTSTOFPS;
+  lastSpeedSet=setve;
+  vt=ve*1/sqrt(fdmex->GetAtmosphere()->GetDensityRatio());
+  mach=vt/fdmex->GetAtmosphere()->GetSoundSpeed();
+  vc=calcVcas(mach);
 }
 
-void FGInitialCondition::SetVtrueKtsIC(float tt)
-{
-    vt=tt*KTSTOFPS;
-    lastSpeedSet=setvt;
-    mach=vt/fdmex->GetAtmosphere()->GetSoundSpeed();
-    vc=calcVcas(mach);
-    ve=vt*sqrt(fdmex->GetAtmosphere()->GetDensityRatio());
+void FGInitialCondition::SetVtrueKtsIC(float tt) {
+  vt=tt*KTSTOFPS;
+  lastSpeedSet=setvt;
+  mach=vt/fdmex->GetAtmosphere()->GetSoundSpeed();
+  vc=calcVcas(mach);
+  ve=vt*sqrt(fdmex->GetAtmosphere()->GetDensityRatio());
 }
 
-void FGInitialCondition::SetMachIC(float tt)
-{
-    mach=tt;
-    lastSpeedSet=setmach;
-    vt=mach*fdmex->GetAtmosphere()->GetSoundSpeed();
-    vc=calcVcas(mach);
-    ve=vt*sqrt(fdmex->GetAtmosphere()->GetDensityRatio());
-    //cout << "Vt: " << vt*FPSTOKTS << " Vc: " << vc*FPSTOKTS << endl;
+void FGInitialCondition::SetMachIC(float tt) {
+  mach=tt;
+  lastSpeedSet=setmach;
+  vt=mach*fdmex->GetAtmosphere()->GetSoundSpeed();
+  vc=calcVcas(mach);
+  ve=vt*sqrt(fdmex->GetAtmosphere()->GetDensityRatio());
+  //cout << "Vt: " << vt*FPSTOKTS << " Vc: " << vc*FPSTOKTS << endl;
 }
+
 
 
 void FGInitialCondition::SetClimbRateFpmIC(float tt) {
 
-    if(vt != 0) {
-        hdot=tt/60;
-        gamma=asin(hdot/vt);
-    }
+  if(vt != 0) {
+    hdot=tt/60;
+    gamma=asin(hdot/vt);
+  }
 }
 
-void FGInitialCondition::SetAltitudeFtIC(float tt)
-{
-    altitude=tt;
-    fdmex->GetPosition()->Seth(altitude);
-    fdmex->GetAtmosphere()->Run();
-    //lets try to make sure the user gets what they intended
-    switch(lastSpeedSet) {
-setvt:   SetVtrueKtsIC(vt); break;
-setvc:   SetVcalibratedKtsIC(vc); break;
-setve:   SetVequivalentKtsIC(ve); break;
-setmach: SetMachIC(mach); break;
-    }
+void FGInitialCondition::SetUBodyFpsIC(float tt) {
+  u=tt;
+  vt=u*u+v*v+w*w;
+  lastSpeedSet=setvt;
+}
+
+void FGInitialCondition::SetVBodyFpsIC(float tt) {
+  v=tt;
+  vt=u*u+v*v+w*w;
+  lastSpeedSet=setvt;
+}
+
+void FGInitialCondition::SetWBodyFpsIC(float tt) {
+  w=tt;
+  vt=u*u+v*v+w*w;
+  lastSpeedSet=setvt;
+}
+
+
+void FGInitialCondition::SetAltitudeFtIC(float tt) {
+  altitude=tt;
+  fdmex->GetPosition()->Seth(altitude);
+  fdmex->GetAtmosphere()->Run();
+  //lets try to make sure the user gets what they intended
+  switch(lastSpeedSet) {
+setvt:
+    SetVtrueKtsIC(vt);
+    break;
+setvc:
+    SetVcalibratedKtsIC(vc);
+    break;
+setve:
+    SetVequivalentKtsIC(ve);
+    break;
+setmach:
+    SetMachIC(mach);
+    break;
+  }
 }
 
 float FGInitialCondition::calcVcas(float Mach) {
 
-    float p=fdmex->GetAtmosphere()->GetPressure();
-    float psl=fdmex->GetAtmosphere()->GetPressureSL();
-    float rhosl=fdmex->GetAtmosphere()->GetDensitySL();
-    float pt,A,B,D,vcas;
+  float p=fdmex->GetAtmosphere()->GetPressure();
+  float psl=fdmex->GetAtmosphere()->GetPressureSL();
+  float rhosl=fdmex->GetAtmosphere()->GetDensitySL();
+  float pt,A,B,D,vcas;
 
-    if(Mach < 1)    //calculate total pressure assuming isentropic flow
-        pt=p*pow((1 + 0.2*Mach*Mach),3.5);
-    else
-    {
-        // shock in front of pitot tube, we'll assume its normal and use
-        // the Rayleigh Pitot Tube Formula, i.e. the ratio of total
-        // pressure behind the shock to the static pressure in front
+  if(Mach < 1)    //calculate total pressure assuming isentropic flow
+    pt=p*pow((1 + 0.2*Mach*Mach),3.5);
+  else {
+    // shock in front of pitot tube, we'll assume its normal and use
+    // the Rayleigh Pitot Tube Formula, i.e. the ratio of total
+    // pressure behind the shock to the static pressure in front
 
 
-        //the normal shock assumption should not be a bad one -- most supersonic
-        //aircraft place the pitot probe out front so that it is the forward
-        //most point on the aircraft.  The real shock would, of course, take
-        //on something like the shape of a rounded-off cone but, here again,
-        //the assumption should be good since the opening of the pitot probe
-        //is very small and, therefore, the effects of the shock curvature
-        //should be small as well. AFAIK, this approach is fairly well accepted
-        //within the aerospace community
+    //the normal shock assumption should not be a bad one -- most supersonic
+    //aircraft place the pitot probe out front so that it is the forward
+    //most point on the aircraft.  The real shock would, of course, take
+    //on something like the shape of a rounded-off cone but, here again,
+    //the assumption should be good since the opening of the pitot probe
+    //is very small and, therefore, the effects of the shock curvature
+    //should be small as well. AFAIK, this approach is fairly well accepted
+    //within the aerospace community
 
-        B = 5.76*Mach*Mach/(5.6*Mach*Mach - 0.8);
+    B = 5.76*Mach*Mach/(5.6*Mach*Mach - 0.8);
 
-        // The denominator above is zero for Mach ~ 0.38, for which
-        // we'll never be here, so we're safe
+    // The denominator above is zero for Mach ~ 0.38, for which
+    // we'll never be here, so we're safe
 
-        D = (2.8*Mach*Mach-0.4)*0.4167;
-        pt = p*pow(B,3.5)*D;
-    }
+    D = (2.8*Mach*Mach-0.4)*0.4167;
+    pt = p*pow(B,3.5)*D;
+  }
 
-    A = pow(((pt-p)/psl+1),0.28571);
-    vcas = sqrt(7*psl/rhosl*(A-1));
-    //cout << "calcVcas: vcas= " << vcas*FPSTOKTS << " mach= " << Mach << " pressure: " << p << endl;
-    return vcas;
+  A = pow(((pt-p)/psl+1),0.28571);
+  vcas = sqrt(7*psl/rhosl*(A-1));
+  //cout << "calcVcas: vcas= " << vcas*FPSTOKTS << " mach= " << Mach << " pressure: " << p << endl;
+  return vcas;
 }
 
 bool FGInitialCondition::findMachInterval(float *mlo, float *mhi, float vcas) {
-    //void find_interval(inter_params &ip,eqfunc f,float y,float constant, int &flag){
+  //void find_interval(inter_params &ip,eqfunc f,float y,float constant, int &flag){
 
-    int i=0;
-    bool found=false;
-    float flo,fhi,fguess;
-    float lo,hi,guess,step;
-    step=0.1;
-    guess=1.5;
-    fguess=calcVcas(guess)-vcas;
-    lo=hi=guess;
-    do{
-        step=2*step;
-        lo-=step;
-        if(lo < 0)
-            lo=0;
-        hi+=step;
-        i++;
-        flo=calcVcas(lo)-vcas;
-        fhi=calcVcas(hi)-vcas;
-        if(flo*fhi <=0){  //found interval with root
-            found=true;
-            if(flo*fguess <= 0){  //narrow interval down a bit
-                hi=lo+step;    //to pass solver interval that is as
-                //small as possible
-            }
-            else if(fhi*fguess <= 0){
-                lo=hi-step;
-            }
-        }
-        //cout << "findMachInterval: i=" << i << " Lo= " << lo << " Hi= " << hi << endl;
+  int i=0;
+  bool found=false;
+  float flo,fhi,fguess;
+  float lo,hi,guess,step;
+  step=0.1;
+  guess=1.5;
+  fguess=calcVcas(guess)-vcas;
+  lo=hi=guess;
+  do {
+    step=2*step;
+    lo-=step;
+    if(lo < 0)
+      lo=0;
+    hi+=step;
+    i++;
+    flo=calcVcas(lo)-vcas;
+    fhi=calcVcas(hi)-vcas;
+    if(flo*fhi <=0) {  //found interval with root
+      found=true;
+      if(flo*fguess <= 0) {  //narrow interval down a bit
+        hi=lo+step;    //to pass solver interval that is as
+        //small as possible
+      }
+      else if(fhi*fguess <= 0) {
+        lo=hi-step;
+      }
     }
-    while((found == 0) && (i <= 100));
-    *mlo=lo;
-    *mhi=hi;
-    return found;
+    //cout << "findMachInterval: i=" << i << " Lo= " << lo << " Hi= " << hi << endl;
+  }
+  while((found == 0) && (i <= 100));
+  *mlo=lo;
+  *mhi=hi;
+  return found;
 }
 
 
@@ -237,47 +262,46 @@ bool FGInitialCondition::findMachInterval(float *mlo, float *mhi, float vcas) {
 bool FGInitialCondition::getMachFromVcas(float *Mach,float vcas) {
 
 
-    float x1,x2,x3,f1,f2,f3,d,d0;
-    float eps=1E-3;
-    float const relax =0.9;
-    int i;
-    bool success=false;
+  float x1,x2,x3,f1,f2,f3,d,d0;
+  float eps=1E-3;
+  float const relax =0.9;
+  int i;
+  bool success=false;
 
-    //initializations
-    d=1;
-    if(findMachInterval(&x1,&x3,vcas)) {
+  //initializations
+  d=1;
+  if(findMachInterval(&x1,&x3,vcas)) {
 
 
-        f1=calcVcas(x1)-vcas;
-        f3=calcVcas(x3)-vcas;
-        d0=fabs(x3-x1);
+    f1=calcVcas(x1)-vcas;
+    f3=calcVcas(x3)-vcas;
+    d0=fabs(x3-x1);
 
-        //iterations
-        i=0;
-        while ((fabs(d) > eps) && (i < 100)){
-            //cout << "getMachFromVcas x1,x2,x3: " << x1 << "," << x2 << "," << x3 << endl;
-            d=(x3-x1)/d0;
-            x2=x1-d*d0*f1/(f3-f1);
-            f2=calcVcas(x2)-vcas;
-            if(f1*f2 <= 0.0){
-                x3=x2;
-                f3=f2;
-                f1=relax*f1;
-            }
-            else if(f2*f3 <= 0){
-                x1=x2;
-                f1=f2;
-                f3=relax*f3;
-            }
-            //cout << i << endl;
-            i++;
-        }//end while
-        if(i < 100) {
-            success=true;
-            *Mach=x2;
-        }
-
+    //iterations
+    i=0;
+    while ((fabs(d) > eps) && (i < 100)) {
+      //cout << "getMachFromVcas x1,x2,x3: " << x1 << "," << x2 << "," << x3 << endl;
+      d=(x3-x1)/d0;
+      x2=x1-d*d0*f1/(f3-f1);
+      f2=calcVcas(x2)-vcas;
+      if(f1*f2 <= 0.0) {
+        x3=x2;
+        f3=f2;
+        f1=relax*f1;
+      } else if(f2*f3 <= 0) {
+        x1=x2;
+        f1=f2;
+        f3=relax*f3;
+      }
+      //cout << i << endl;
+      i++;
+    }//end while
+    if(i < 100) {
+      success=true;
+      *Mach=x2;
     }
-    //cout << "Success= " << success << " Vcas: " << vcas*FPSTOKTS << " Mach: " << x2 << endl;
-    return success;
+
+  }
+  //cout << "Success= " << success << " Vcas: " << vcas*FPSTOKTS << " Mach: " << x2 << endl;
+  return success;
 }
--- FGInitialCondition.h
+++ FGInitialCondition.h
@@ -1,49 +1,49 @@
 /*******************************************************************************
-
+ 
  Header:       FGInitialCondition.h
  Author:       Tony Peden
  Date started: 7/1/99
-
+ 
  ------------- Copyright (C) 1999  Anthony K. Peden (apeden@earthlink.net) -------------
-
+ 
  This program is free software; you can redistribute it and/or modify it under
  the terms of the GNU General Public License as published by the Free Software
  Foundation; either version 2 of the License, or (at your option) any later
  version.
-
+ 
  This program is distributed in the hope that it will be useful, but WITHOUT
  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  details.
-
+ 
  You should have received a copy of the GNU General Public License along with
  this program; if not, write to the Free Software Foundation, Inc., 59 Temple
  Place - Suite 330, Boston, MA  02111-1307, USA.
-
+ 
  Further information about the GNU General Public License can also be found on
  the world wide web at http://www.gnu.org.
-
-
+ 
+ 
  HISTORY
 --------------------------------------------------------------------------------
 7/1/99   TP   Created
-
-
+ 
+ 
 FUNCTIONAL DESCRIPTION
 --------------------------------------------------------------------------------
-
+ 
 The purpose of this class is to take a set of initial conditions and provide
 a kinematically consistent set of body axis velocity components, euler
 angles, and altitude.  This class does not attempt to trim the model i.e.
 the sim will most likely start in a very dynamic state (unless, of course,
 you have chosen your IC's wisely) even after setting it up with this class.
-
+ 
 CAVEAT: This class makes use of alpha=theta-gamma. This means that setting
         any of the three with this class is only valid for steady state
         (all accels zero) and zero pitch rate.  One example where this
         would produce invalid results is setting up for a trim in a pull-up
         or pushover (both have nonzero pitch rate).  Maybe someday...
-
+ 
 ********************************************************************************
 SENTRY
 *******************************************************************************/
@@ -66,16 +66,31 @@ CLASS DECLARATION
 typedef enum { none,setvt, setvc, setve, setmach } speedset;
 
 /* USAGE NOTES
-   FGFDMExec:
-     A valid FGFDMExec object is required at instantiation by this
-	 class.  This is so that FGAtmosphere can be used. 
+   With a valid object of FGFDMExec and an aircraft model loaded
+   FGInitialCondition fgic=new FGInitialCondition(FDMExec);
+   fgic->SetVcalibratedKtsIC()
+   fgic->SetAltitudeFtIC();
+   .
+   .
+   .
+   //to directly into Run
+   FDMExec->GetState()->Initialize(fgic)
+   delete fgic;
+   FDMExec->Run()
+   
+   //or to loop the sim w/o integrating
+   FDMExec->RunIC(fgic)
+   
+   
    
    Speed:
 	 Since vc, ve, vt, and mach all represent speed, the remaining
 	 three are recalculated each time one of them is set (using the
 	 current altitude).  The most recent speed set is remembered so 
 	 that if and when altitude is reset, the last set speed is used 
-	 to recalculate the remaining three. 
+	 to recalculate the remaining three. Setting any of the body 
+	 components forces a recalculation of vt and vt then becomes the
+	 most recent speed set.
    
    Alpha,Gamma, and Theta:
      This class assumes that it will be used to set up the sim for a
@@ -87,92 +102,211 @@ typedef enum { none,setvt, setvc, setve, setmach } speedset;
     	  set alpha : recalculate theta using gamma as currently set
 		  set theta : recalculate alpha using gamma as currently set
 		  set gamma : recalculate theta using alpha as currently set
-
+ 
 	 The idea being that gamma is most interesting to pilots (since it 
 	 is indicative of climb rate). 
 	 
 	 Setting climb rate is, for the purpose of this discussion, 
 	 considered equivalent to setting gamma.
-
+ 
 */
-class FGInitialCondition
-{
+class FGInitialCondition {
 public:
 
-    FGInitialCondition(FGFDMExec *fdmex);
-    ~FGInitialCondition(void);
+  FGInitialCondition(FGFDMExec *fdmex);
+  ~FGInitialCondition(void);
 
-    void SetVcalibratedKtsIC(float tt);
-    void SetVequivalentKtsIC(float tt);
-    void SetVtrueKtsIC(float tt);
-    void SetMachIC(float tt);
+  void SetVcalibratedKtsIC(float tt);
+  void SetVequivalentKtsIC(float tt);
+  void SetVtrueKtsIC(float tt);
+  void SetMachIC(float tt);
 
-    void SetAltitudeFtIC(float tt);
+  void SetUBodyFpsIC(float tt);
+  void SetVBodyFpsIC(float tt);
+  void SetWBodyFpsIC(float tt);
 
-    //"vertical" flight path, recalculate theta
-    inline void SetFlightPathAngleDegIC(float tt) { gamma=tt*DEGTORAD;theta=alpha+gamma;}
-    //set speed first
-    void SetClimbRateFpmIC(float tt);
-    //use currently stored gamma, recalcualte theta
-    inline void SetAlphaDegIC(float tt)      { alpha=tt*DEGTORAD;theta=alpha+gamma;}
-    //use currently stored gamma, recalcualte alpha
-    inline void SetPitchAngleDegIC(float tt) { theta=tt*DEGTORAD;alpha=theta-gamma;}
+  void SetAltitudeFtIC(float tt);
 
-    inline void SetBetaDegIC(float tt)       { beta=tt*DEGTORAD; }
+  //"vertical" flight path, recalculate theta
+  inline void SetFlightPathAngleDegIC(float tt) {
+    gamma=tt*DEGTORAD;
+    theta=alpha+gamma;
+  }
+  inline void SetFlightPathAngleRadIC(float tt) {
+    gamma=tt;
+    theta=alpha+gamma;
+  }
+  //set speed first
+  void SetClimbRateFpmIC(float tt);
+  //use currently stored gamma, recalcualte theta
+  inline void SetAlphaDegIC(float tt)      {
+    alpha=tt*DEGTORAD;
+    theta=alpha+gamma;
+  }
+  inline void SetAlphaRadIC(float tt)      {
+    alpha=tt;
+    theta=alpha+gamma;
+  }
+  //use currently stored gamma, recalcualte alpha
+  inline void SetPitchAngleDegIC(float tt) {
+    theta=tt*DEGTORAD;
+    alpha=theta-gamma;
+  }
+  inline void SetPitchAngleRadIC(float tt) {
+    theta=tt;
+    alpha=theta-gamma;
+  }
 
-    inline void SetRollAngleDegIC(float tt) { phi=tt*DEGTORAD; }
-    inline void SetHeadingDegIC(float tt)   { psi=tt*DEGTORAD; }
+  inline void SetBetaDegIC(float tt)       {
+    beta=tt*DEGTORAD;
+  }
 
-    inline void SetLatitudeDegIC(float tt)  { latitude=tt*DEGTORAD; }
-    inline void SetLongitudeDegIC(float tt) { longitude=tt*DEGTORAD; }
+  inline void SetRollAngleDegIC(float tt) {
+    phi=tt*DEGTORAD;
+  }
+  inline void SetRollAngleRadIC(float tt) {
+    phi=tt;
+  }
+
+  inline void SetHeadingDegIC(float tt)   {
+    psi=tt*DEGTORAD;
+  }
+  inline void SetHeadingRadIC(float tt)   {
+    psi=tt;
+  }
+
+  inline void SetLatitudeDegIC(float tt)  {
+    latitude=tt*DEGTORAD;
+  }
+  inline void SetLatitudeRadIC(float tt)  {
+    latitude=tt;
+  }
+
+  inline void SetLongitudeDegIC(float tt) {
+    longitude=tt*DEGTORAD;
+  }
+  inline void SetLongitudeRadIC(float tt) {
+    longitude=tt;
+  }
+
+  inline float GetVcalibratedKtsIC(void) {
+    return vc*FPSTOKTS;
+  }
+  inline float GetVequivalentKtsIC(void) {
+    return ve*FPSTOKTS;
+  }
+  inline float GetVtrueKtsIC(void) {
+    return vt*FPSTOKTS;
+  }
+  inline float GetMachIC(void) {
+    return mach;
+  }
+
+  inline float GetAltitudeFtIC(void) {
+    return altitude;
+  }
+
+  inline float GetFlightPathAngleDegIC(void) {
+    return gamma*RADTODEG;
+  }
+  inline float GetFlightPathAngleRadIC(void) {
+    return gamma;
+  }
+
+  inline float GetClimbRateFpmIC(void) {
+    return hdot*60;
+  }
+  inline float GetClimbRateFpsIC(void) {
+    return hdot;
+  }
+
+  inline float GetAlphaDegIC(void)      {
+    return alpha*RADTODEG;
+  }
+  inline float GetAlphaRadIC(void)      {
+    return alpha;
+  }
+
+  inline float GetPitchAngleDegIC(void) {
+    return theta*RADTODEG;
+  }
+  inline float GetPitchAngleRadIC(void) {
+    return theta;
+  }
 
 
-    inline float GetVcalibratedKtsIC(void) { return vc*FPSTOKTS; }
-    inline float GetVequivalentKtsIC(void){ return ve*FPSTOKTS; }
-    inline float GetVtrueKtsIC(void){ return vt*FPSTOKTS; }
-    inline float GetMachIC(void){ return mach; }
+  inline float GetBetaDegIC(void)       {
+    return beta*RADTODEG;
+  }
+  inline float GetBetaRadIC(void)       {
+    return beta*RADTODEG;
+  }
 
-    inline float GetAltitudeFtIC(void){ return altitude; }
+  inline float GetRollAngleDegIC(void) {
+    return phi*RADTODEG;
+  }
+  inline float GetRollAngleRadIC(void) {
+    return phi;
+  }
 
-    inline float GetFlightPathAngleDegIC(void) { return gamma*RADTODEG;}
-    inline float GetClimbRateFpmIC(void) { return hdot*60; }
-    inline float GetAlphaDegIC(void)      { return alpha*RADTODEG;}
-    inline float GetPitchAngleDegIC(void) { return theta*RADTODEG;}
+  inline float GetHeadingDegIC(void)   {
+    return psi*RADTODEG;
+  }
+  inline float GetHeadingRadIC(void)   {
+    return psi;
+  }
 
-    inline float GetBetaDegIC(void)       { return beta*RADTODEG; }
+  inline float GetLatitudeDegIC(void)  {
+    return latitude*RADTODEG;
+  }
+  inline float GetLatitudeRadIC(void) {
+    return latitude;
+  }
 
-    inline float GetRollAngleDegIC(void) { return phi*RADTODEG; }
-    inline float GetHeadingDegIC(void)   { return psi*RADTODEG; }
+  inline float GetLongitudeDegIC(void) {
+    return longitude*RADTODEG;
+  }
+  inline float GetLongitudeRadIC(void) {
+    return longitude;
+  }
 
-    inline float GetLatitudeDegIC(void)  { return latitude*RADTODEG; }
-    inline float GetLongitudeDegIC(void) { return longitude*RADTODEG; }
+  inline float GetUBodyFpsIC(void) {
+    return vt*cos(alpha)*cos(beta);
+  }
+  inline float GetVBodyFpsIC(void) {
+    return vt*sin(beta);
+  }
+  inline float GetWBodyFpsIC(void) {
+    return vt*sin(alpha)*cos(beta);
+  }
 
+  inline float GetThetaRadIC(void) {
+    return theta;
+  }
+  inline float GetPhiRadIC(void)   {
+    return phi;
+  }
+  inline float GetPsiRadIC(void)   {
+    return psi;
+  }
 
-    inline float GetUBodyFpsIC(void) { return vt*cos(alpha)*cos(beta); }
-    inline float GetVBodyFpsIC(void) { return vt*sin(beta);}
-    inline float GetWBodyFpsIC(void) { return vt*sin(alpha)*cos(beta);}
-
-    inline float GetThetaRadIC(void) { return theta;}
-    inline float GetPhiRadIC(void)   { return phi;}
-    inline float GetPsiRadIC(void)   { return psi; }
-
-    inline float GetLatitudeRadIC(void) { return latitude; }
-    inline float GetLongitudeRadIC(void) { return longitude; }
 
 
 private:
-    float vt,vc,ve;
-    float alpha,beta,gamma,theta,phi,psi;
-    float mach;
-    float altitude,hdot;
-    float latitude,longitude;
-    speedset lastSpeedSet;
+  float vt,vc,ve;
+  float alpha,beta,gamma,theta,phi,psi;
+  float mach;
+  float altitude,hdot;
+  float latitude,longitude;
+  float u,v,w;
 
-    FGFDMExec *fdmex;
+  speedset lastSpeedSet;
 
-    float calcVcas(float Mach);
-    bool findMachInterval(float *mlo, float *mhi,float vcas);
-    bool getMachFromVcas(float *Mach,float vcas);
+  FGFDMExec *fdmex;
+
+  float calcVcas(float Mach);
+  bool findMachInterval(float *mlo, float *mhi,float vcas);
+  bool getMachFromVcas(float *Mach,float vcas);
 };
 
 #endif
