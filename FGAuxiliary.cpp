d59e18bdecd88eebaf98f4410ad35811f0ee1eef
jsb <jsb>
2000-05-11 11:23:37 +0000

    Added comments regarding Vc and Ve

--- FGAuxiliary.cpp
+++ FGAuxiliary.cpp
@@ -1,41 +1,41 @@
 /*******************************************************************************
-
+ 
  Module:       FGAuxiliary.cpp
  Author:       Jon Berndt
  Date started: 01/26/99
  Purpose:      Calculates additional parameters needed by the visual system, etc.
  Called by:    FGSimExec
-
+ 
  ------------- Copyright (C) 1999  Jon S. Berndt (jsb@hal-pc.org) -------------
-
+ 
  This program is free software; you can redistribute it and/or modify it under
  the terms of the GNU General Public License as published by the Free Software
  Foundation; either version 2 of the License, or (at your option) any later
  version.
-
+ 
  This program is distributed in the hope that it will be useful, but WITHOUT
  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  details.
-
+ 
  You should have received a copy of the GNU General Public License along with
  this program; if not, write to the Free Software Foundation, Inc., 59 Temple
  Place - Suite 330, Boston, MA  02111-1307, USA.
-
+ 
  Further information about the GNU General Public License can also be found on
  the world wide web at http://www.gnu.org.
-
+ 
 FUNCTIONAL DESCRIPTION
 --------------------------------------------------------------------------------
 This class calculates various auxiliary parameters.
-
+ 
 REFERENCES
   Anderson, John D. "Introduction to Flight", 3rd Edition, McGraw-Hill, 1989
                     pgs. 112-126
 HISTORY
 --------------------------------------------------------------------------------
 01/26/99   JSB   Created
-
+ 
 ********************************************************************************
 INCLUDES
 *******************************************************************************/
@@ -55,59 +55,57 @@ INCLUDES
 ************************************ CODE **************************************
 *******************************************************************************/
 
-FGAuxiliary::FGAuxiliary(FGFDMExec* fdmex) : FGModel(fdmex)
-{
+FGAuxiliary::FGAuxiliary(FGFDMExec* fdmex) : FGModel(fdmex) {
   Name = "FGAuxiliary";
   vcas = veas = mach = qbar = pt = 0;
   psl = rhosl = 1;
 }
 
 
-FGAuxiliary::~FGAuxiliary()
-{
-}
+FGAuxiliary::~FGAuxiliary() {}
 
 
-bool FGAuxiliary::Run()
-{
+
+bool FGAuxiliary::Run() {
   float A,B,D;
 
   if (!FGModel::Run()) {
     GetState();
     if(mach < 1)    //calculate total pressure assuming isentropic flow
       pt=p*pow((1 + 0.2*mach*mach),3.5);
-  else
-  {
-    // shock in front of pitot tube, we'll assume its normal and use
-    // the Rayleigh Pitot Tube Formula, i.e. the ratio of total
-    // pressure behind the shock to the static pressure in front
+    else {
+      // shock in front of pitot tube, we'll assume its normal and use
+      // the Rayleigh Pitot Tube Formula, i.e. the ratio of total
+      // pressure behind the shock to the static pressure in front
 
-    B = 5.76*mach*mach/(5.6*mach*mach - 0.8);
+      B = 5.76*mach*mach/(5.6*mach*mach - 0.8);
 
-    // The denominator above is zero for Mach ~ 0.38, for which
-    // we'll never be here, so we're safe
+      // The denominator above is zero for Mach ~ 0.38, for which
+      // we'll never be here, so we're safe
 
-    D = (2.8*mach*mach-0.4)*0.4167;
-    pt = p*pow(B,3.5)*D;
-  }
+      D = (2.8*mach*mach-0.4)*0.4167;
+      pt = p*pow(B,3.5)*D;
+    }
 
-  A = pow(((pt-p)/psl+1),0.28571);
-  vcas = sqrt(7*psl/rhosl*(A-1));
-  veas = sqrt(2*qbar/rhosl);
+    A = pow(((pt-p)/psl+1),0.28571);
+    vcas = sqrt(7*psl/rhosl*(A-1));
+    veas = sqrt(2*qbar/rhosl);
+
+
 
   } else {
-
   }
+
 
   return false;
 }
 
-void FGAuxiliary::GetState(void)
-{
+void FGAuxiliary::GetState(void) {
   qbar = Translation->Getqbar();
   mach = Translation->GetMach();
   p = Atmosphere->GetPressure();
   rhosl = Atmosphere->GetDensitySL();
   psl = Atmosphere->GetPressureSL();
+
 }
 
--- FGAuxiliary.h
+++ FGAuxiliary.h
@@ -1,33 +1,33 @@
 /*******************************************************************************
-
+ 
  Header:       FGAuxiliary.h
  Author:       Jon Berndt
  Date started: 01/26/99
-
+ 
  ------------- Copyright (C) 1999  Jon S. Berndt (jsb@hal-pc.org) -------------
-
+ 
  This program is free software; you can redistribute it and/or modify it under
  the terms of the GNU General Public License as published by the Free Software
  Foundation; either version 2 of the License, or (at your option) any later
  version.
-
+ 
  This program is distributed in the hope that it will be useful, but WITHOUT
  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  details.
-
+ 
  You should have received a copy of the GNU General Public License along with
  this program; if not, write to the Free Software Foundation, Inc., 59 Temple
  Place - Suite 330, Boston, MA  02111-1307, USA.
-
+ 
  Further information about the GNU General Public License can also be found on
  the world wide web at http://www.gnu.org.
-
+ 
 HISTORY
 --------------------------------------------------------------------------------
 11/22/98   JSB   Created
   1/1/00   TP    Added calcs and getters for VTAS, VCAS, VEAS, Vground, in knots
-
+ 
 ********************************************************************************
 SENTRY
 *******************************************************************************/
@@ -50,21 +50,29 @@ using namespace std;
 CLASS DECLARATION
 *******************************************************************************/
 
-class FGAuxiliary : public FGModel
-{
+class FGAuxiliary : public FGModel {
 public:
   FGAuxiliary(FGFDMExec*);
   ~FGAuxiliary(void);
-   
+
   bool Run(void);
-  
-  
-  inline float GetVcalibratedFPS(void) { return vcas; }
-  inline float GetVcalibratedKTS(void) { return vcas*FPSTOKTS; }
-  inline float GetVequivalentFPS(void) { return veas; }
-  inline float GetVequivalentKTS(void) { return veas*FPSTOKTS; }
-  
-  
+
+  //use FGInitialCondition to set these speeds
+  inline float GetVcalibratedFPS(void) {
+    return vcas;
+  }
+  inline float GetVcalibratedKTS(void) {
+    return vcas*FPSTOKTS;
+  }
+  inline float GetVequivalentFPS(void) {
+    return veas;
+  }
+  inline float GetVequivalentKTS(void) {
+    return veas*FPSTOKTS;
+  }
+
+
+
 protected:
 
 private:
@@ -72,7 +80,13 @@ private:
   float veas;
   float mach;
   float qbar,rhosl,rho,p,psl,pt;
-  
+  //Don't add a getter for pt!
+  //pt above is freestream total pressure for subsonic only
+  //for supersonic it is the 1D total pressure behind a normal shock
+  //if a general freestream total is needed, e-mail Tony Peden
+  // (apeden@earthlink.net) or you can add it your self using the
+  // isentropic flow equations
+
   void GetState(void);
 };
 
