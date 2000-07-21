ec384657ebde5e50e4441e33c7c00651bc513ad5
jsb <jsb>
2000-07-21 12:18:11 +0000

    Fixed a missing parenthesis in recent mod of FGMatrix

--- FGMatrix.cpp
+++ FGMatrix.cpp
@@ -554,7 +554,7 @@ FGColumnVector FGColumnVector::Normalize(void)
 {
   double Mag = Magnitude();
 
-  if(Mag != 0 {
+  if (Mag != 0) {
     for (unsigned int i=1; i<=Rows(); i++)
       for (unsigned int j=1; j<=Cols(); j++)
         data[i][j] = data[i][j]/Mag;
