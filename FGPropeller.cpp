4996afb9f95b2e4e684bf160e7b7f3a58d04bcde
jberndt <jberndt>
2001-12-10 13:25:22 +0000

    Adding some debug capabilities

--- FGPropeller.cpp
+++ FGPropeller.cpp
@@ -38,7 +38,7 @@ INCLUDES
 #include "FGPropeller.h"
 #include "FGFCS.h"
 
-static const char *IdSrc = "$Id: FGPropeller.cpp,v 1.44 2001/12/08 22:54:26 jberndt Exp $";
+static const char *IdSrc = "$Id: FGPropeller.cpp,v 1.45 2001/12/10 13:25:22 jberndt Exp $";
 static const char *IdHdr = ID_PROPELLER;
 
 /*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -239,9 +239,35 @@ FGColumnVector3 FGPropeller::GetPFactor()
 }
 
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//    The bitmasked value choices are as follows:
+//    unset: In this case (the default) JSBSim would only print
+//       out the normally expected messages, essentially echoing
+//       the config files as they are read. If the environment
+//       variable is not set, debug_lvl is set to 1 internally
+//    0: This requests JSBSim not to output any messages
+//       whatsoever.
+//    1: This value explicity requests the normal JSBSim
+//       startup messages
+//    2: This value asks for a message to be printed out when
+//       a class is instantiated
+//    4: When this value is set, a message is displayed when a
+//       FGModel object executes its Run() method
+//    8: When this value is set, various runtime state variables
+//       are printed out periodically
+//    16: When set various parameters are sanity checked and
+//       a message is printed out when they go out of bounds
 
 void FGPropeller::Debug(void)
 {
-    //TODO: Add your source code here
+  if (debug_lvl & 16) { // Sanity checking
+  }
+  if (debug_lvl & 8 ) { // Runtime state variables
+  }
+  if (debug_lvl & 4 ) { // Run() method entry print for FGModel-derived objects
+  }
+  if (debug_lvl & 2 ) { // Instantiation/Destruction notification
+  }
+  if (debug_lvl & 1 ) { // Standard console startup message output
+  }
 }
 
