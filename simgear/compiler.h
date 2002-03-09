f6983ef8ba2cabb431f1a749aacfc24d792a5d54
apeden <apeden>
2002-03-09 12:04:35 +0000

    Provides the preprocessor defines needed to compile the properties code without any dependencies on simgear.

---
+++ simgear/compiler.h
@@ -0,0 +1,13 @@
+/*compiler.h
+
+This file provided the preprocessor defines needed to compile the properties
+code without any dependencies on simgear
+
+*/
+
+#define NOSIMGEAR 1
+
+#define STL_STRING <string>
+#define STL_IOSTREAM <iostream>
+#define SG_LOG(type, level, message) (cout <<message << endl)
+#define SG_USING_STD(X) using std::X
