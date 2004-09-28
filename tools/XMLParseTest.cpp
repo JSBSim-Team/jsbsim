f02e1dcd1af27d54edb859f75de14754cd6fa231
jberndt <jberndt>
2004-09-28 11:38:15 +0000

    Adjustments to XML parse test

--- tools/XMLParseTest.cpp
+++ tools/XMLParseTest.cpp
@@ -3,7 +3,7 @@
 #include <fstream>
 
 /*
-g++ FGXMLParse.cpp XMLParseTest.cpp -oXMLParse -L/usr/local/lib -lsgxml -lsgstructure
+g++ ../FGXMLParse.cpp XMLParseTest.cpp -I../ -oXMLParse -L/usr/local/lib -lsgxml -lsgstructure
 */
 
 using namespace std;
@@ -11,7 +11,7 @@ using namespace JSBSim;
 
 int main (int argc, char** argv)
 {
-
+  Element *element, *element1;
   ifstream inputfile(argv[1]);
   if (!inputfile) {
     cerr << "Could not open XML file " << argv[1] << endl << endl;
@@ -19,5 +19,14 @@ int main (int argc, char** argv)
   }
   FGXMLParse myXMLFile;
   readXML (inputfile, myXMLFile);
-  myXMLFile.GetDocument()->Print();
+  element1 = myXMLFile.GetDocument()->FindElement("AERODYNAMICS");
+  while (element = element1->FindNextElement("AXIS")) {
+    element->Print();
+  }
+  element1 = myXMLFile.GetDocument();
+  element = element1->FindElement();
+  while (element) {
+    cout << "Element: " << element->GetName() << endl;
+    element = element1->FindNextElement();
+  }
 }
