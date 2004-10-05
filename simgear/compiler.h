8e7253a968bb0c44a7f2e41734f0d9ce92acc957
jberndt <jberndt>
2004-10-05 07:52:07 +0000

    Mods for future easyXML compilation

--- simgear/compiler.h
+++ simgear/compiler.h
@@ -8,6 +8,8 @@ code without any dependencies on simgear
 #define NOSIMGEAR 1
 
 #define STL_STRING <string>
+#define STL_FSTREAM <fstream>
 #define STL_IOSTREAM <iostream>
+
 #define SG_LOG(type, level, message) (cout <<message << endl)
 #define SG_USING_STD(X) using std::X
