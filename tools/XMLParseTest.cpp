82907eddc7e7f82cb6e8a231ff5493e5d9b7ff99
jberndt <jberndt>
2004-10-05 03:38:29 +0000

    Update XMLParseTest.

--- tools/XMLParseTest.cpp
+++ tools/XMLParseTest.cpp
@@ -3,7 +3,7 @@
 #include <fstream>
 
 /*
-g++ ../FGXMLParse.cpp XMLParseTest.cpp -I../ -oXMLParse -L/usr/local/lib -lsgxml -lsgstructure
+g++ ../FGXMLParse.cpp ../FGXMLElement.cpp XMLParseTest.cpp -I../ -oXMLParse -L/usr/local/lib -lsgxml -lsgstructure
 */
 
 using namespace std;
@@ -11,7 +11,6 @@ using namespace JSBSim;
 
 int main (int argc, char** argv)
 {
-  Element *element, *element1;
   ifstream inputfile(argv[1]);
   if (!inputfile) {
     cerr << "Could not open XML file " << argv[1] << endl << endl;
@@ -19,14 +18,5 @@ int main (int argc, char** argv)
   }
   FGXMLParse myXMLFile;
   readXML (inputfile, myXMLFile);
-  element1 = myXMLFile.GetDocument()->FindElement("AERODYNAMICS");
-  while (element = element1->FindNextElement("AXIS")) {
-    element->Print();
-  }
-  element1 = myXMLFile.GetDocument();
-  element = element1->FindElement();
-  while (element) {
-    cout << "Element: " << element->GetName() << endl;
-    element = element1->FindNextElement();
-  }
+  myXMLFile.GetDocument()->Print();
 }
