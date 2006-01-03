1b37ca7910eda56c90f76b866ea9b622832b522c
dpculp <dpculp>
2006-01-03 10:37:34 +0000

    Use internal table definition.

--- src/utilities/aeromatic/prop2.php
+++ src/utilities/aeromatic/prop2.php
@@ -114,17 +114,15 @@ if($ac_pitch == 0) {
  }
 
 if($ac_pitch == 1) {
-  printf("  <minrpm>%7.0f </minrpm>\n", $max_rpm * 0.8);
+  printf("  <minrpm>%7.0f </minrpm>\n", $max_rpm * 0.85);
   printf("  <maxrpm>%7.0f </maxrpm>\n", $max_rpm);
 }
 
 print("\n");
 
 if($ac_pitch == 0) {          // fixed pitch
- print("  <function name=\"C_THRUST\">\n");
- print("    <table>\n");
- print("      <independentVar>propulsion/advance-ratio</independentVar>\n");
- print("      <tableData>\n");
+ print("  <table name=\"C_THRUST\" type=\"internal\">\n");
+ print("     <tableData>\n");
  printf("       0.0  %5.4f\n", $ct0 * 1.0);
  printf("       0.1  %5.4f\n", $ct0 * 0.959);
  printf("       0.2  %5.4f\n", $ct0 * 0.917);
@@ -137,45 +135,38 @@ if($ac_pitch == 0) {          // fixed pitch
  printf("       1.0  %5.4f\n", $ct0 * -0.075);
  printf("       1.2  %5.4f\n", $ct0 * -0.394);
  printf("       1.4  %5.4f\n", $ct0 * -0.708);
- print("      </tableData>\n");
- print("    </table>\n");
- print("  </function>\n");
+ print("     </tableData>\n");
+ print("  </table>\n\n");
 } else {                      // variable pitch
- print("  <function name=\"C_THRUST\">\n");
- print("    <table>\n");
- print("      <independentVar lookup=\"row\">propulsion/advance-ratio</independentVar>\n");
- print("      <independentVar lookup=\"column\">propulsion/blade-angle</independentVar>\n");
+ print("  <table name=\"C_THRUST\" type=\"internal\">\n");
  print("      <tableData>\n");
  print("                12         30\n");
  $ct30 = $ct0 + 0.04;
- printf("       0.0  %5.4f %5.4f\n", $ct0 * 1.0, $ct30 * 1.0);
- printf("       0.1  %5.4f %5.4f\n", $ct0 * 0.917, $ct30 * 1.0);
- printf("       0.2  %5.4f %5.4f\n", $ct0 * 0.833, $ct30 * 1.0);
- printf("       0.3  %5.4f %5.4f\n", $ct0 * 0.692, $ct30 * 0.995);
- printf("       0.4  %5.4f %5.4f\n", $ct0 * 0.526, $ct30 * 0.990);
- printf("       0.5  %5.4f %5.4f\n", $ct0 * 0.359, $ct30 * 0.977);
- printf("       0.6  %5.4f %5.4f\n", $ct0 * 0.154, $ct30 * 0.925);
- printf("       0.7  %5.4f %5.4f\n", $ct0 * -0.013, $ct30 * 0.832);
- printf("       0.8  %5.4f %5.4f\n", $ct0 * -0.295, $ct30 * 0.738);
- printf("       1.0  %5.4f %5.4f\n", $ct0 * -0.641, $ct30 * 0.491);
- printf("       1.2  %5.4f %5.4f\n", $ct0 * -1.050, $ct30 * 0.262);
- printf("       1.4  %5.4f %5.4f\n", $ct0 * -1.436, $ct30 * 0.019);
- printf("       1.6  %5.4f %5.4f\n", $ct0 * -1.880, $ct30 * -0.215);
- printf("       1.8  %5.4f %5.4f\n", $ct0 * -2.300, $ct30 * -0.448);
- printf("       2.0  %5.4f %5.4f\n", $ct0 * -2.700, $ct30 * -0.692);
- printf("       2.2  %5.4f %5.4f\n", $ct0 * -3.100, $ct30 * -0.940);
- printf("       2.4  %5.4f %5.4f\n", $ct0 * -3.500, $ct30 * -1.190);
+ printf("       0.0      %5.4f   %5.4f\n", $ct0 * 1.0, $ct30 * 1.0);
+ printf("       0.1      %5.4f   %5.4f\n", $ct0 * 0.917, $ct30 * 1.0);
+ printf("       0.2      %5.4f   %5.4f\n", $ct0 * 0.833, $ct30 * 1.0);
+ printf("       0.3      %5.4f   %5.4f\n", $ct0 * 0.692, $ct30 * 0.995);
+ printf("       0.4      %5.4f   %5.4f\n", $ct0 * 0.526, $ct30 * 0.990);
+ printf("       0.5      %5.4f   %5.4f\n", $ct0 * 0.359, $ct30 * 0.977);
+ printf("       0.6      %5.4f   %5.4f\n", $ct0 * 0.154, $ct30 * 0.925);
+ printf("       0.7      %5.4f   %5.4f\n", $ct0 * -0.013, $ct30 * 0.832);
+ printf("       0.8      %5.4f   %5.4f\n", $ct0 * -0.295, $ct30 * 0.738);
+ printf("       1.0      %5.4f   %5.4f\n", $ct0 * -0.641, $ct30 * 0.491);
+ printf("       1.2      %5.4f   %5.4f\n", $ct0 * -1.050, $ct30 * 0.262);
+ printf("       1.4      %5.4f   %5.4f\n", $ct0 * -1.436, $ct30 * 0.019);
+ printf("       1.6      %5.4f   %5.4f\n", $ct0 * -1.880, $ct30 * -0.215);
+ printf("       1.8      %5.4f   %5.4f\n", $ct0 * -2.300, $ct30 * -0.448);
+ printf("       2.0      %5.4f   %5.4f\n", $ct0 * -2.700, $ct30 * -0.692);
+ printf("       2.2      %5.4f   %5.4f\n", $ct0 * -3.100, $ct30 * -0.940);
+ printf("       2.4      %5.4f   %5.4f\n", $ct0 * -3.500, $ct30 * -1.190);
  print("      </tableData>\n");
- print("    </table>\n");
- print("  </function>\n");
+ print("  </table>\n");
 }
 
 print("\n");
 if($ac_pitch == 0) {          // fixed pitch
- print("  <function name=\"C_POWER\">\n");
- print("    <table>\n");
- print("      <independentVar>propulsion/advance-ratio</independentVar>\n");
- print("      <tableData>\n");
+ print("  <table name=\"C_POWER\" type=\"internal\">\n");
+ print("     <tableData>\n");
  printf("       0.0  %5.4f\n", $cp0 * 1.0);
  printf("       0.1  %5.4f\n", $cp0 * 1.0);
  printf("       0.2  %5.4f\n", $cp0 * 0.976);
@@ -189,37 +180,32 @@ if($ac_pitch == 0) {          // fixed pitch
  printf("       1.2  %5.4f\n", $cp0 * -0.296);
  printf("       1.4  %5.4f\n", $cp0 * -0.890);
  printf("       1.6  %5.4f\n", $cp0 * -1.511);
- print("      </tableData>\n");
- print("    </table>\n");
- print("  </function>\n");
+ print("     </tableData>\n");
+ print("  </table>\n");
 } else {                      // variable pitch
- print("  <function name=\"C_POWER\">\n");
- print("    <table>\n");
- print("      <independentVar lookup=\"row\">propulsion/advance-ratio</independentVar>\n");
- print("      <independentVar lookup=\"column\">propulsion/blade-angle</independentVar>\n");
- print("      <tableData>\n");
+ print("  <table name=\"C_POWER\" type=\"internal\">\n");
+ print("     <tableData>\n");
  print("                12         30\n");
  $cp30 = $cp0 + 0.06;
- printf("       0.0  %5.4f %5.4f\n", $cp0 * 1.0, $cp30 * 1.0);
- printf("       0.1  %5.4f %5.4f\n", $cp0 * 1.0, $cp30 * 1.0);
- printf("       0.2  %5.4f %5.4f\n", $cp0 * 0.953, $cp30 * 1.0);
- printf("       0.3  %5.4f %5.4f\n", $cp0 * 0.906, $cp30 * 1.0);
- printf("       0.4  %5.4f %5.4f\n", $cp0 * 0.797, $cp30 * 1.0);
- printf("       0.5  %5.4f %5.4f\n", $cp0 * 0.656, $cp30 * 0.989);
- printf("       0.6  %5.4f %5.4f\n", $cp0 * 0.531, $cp30 * 0.978);
- printf("       0.7  %5.4f %5.4f\n", $cp0 * 0.313, $cp30 * 0.956);
- printf("       0.8  %5.4f %5.4f\n", $cp0 * 0.125, $cp30 * 0.911);
- printf("       1.0  %5.4f %5.4f\n", $cp0 * -0.375, $cp30 * 0.744);
- printf("       1.2  %5.4f %5.4f\n", $cp0 * -1.093, $cp30 * 0.500);
- printf("       1.4  %5.4f %5.4f\n", $cp0 * -2.030, $cp30 * 0.250);
- printf("       1.6  %5.4f %5.4f\n", $cp0 * -3.0, $cp30 * -0.022);
- printf("       1.8  %5.4f %5.4f\n", $cp0 * -4.0, $cp30 * -0.610);
- printf("       2.0  %5.4f %5.4f\n", $cp0 * -5.0, $cp30 * -1.220);
- printf("       2.2  %5.4f %5.4f\n", $cp0 * -6.0, $cp30 * -1.830);
- printf("       2.4  %5.4f %5.4f\n", $cp0 * -7.0, $cp30 * -2.440);
- print("      </tableData>\n");
- print("    </table>\n");
- print("  </function>\n");
+ printf("       0.0      %5.4f   %5.4f\n", $cp0 * 1.0, $cp30 * 1.0);
+ printf("       0.1      %5.4f   %5.4f\n", $cp0 * 1.0, $cp30 * 1.0);
+ printf("       0.2      %5.4f   %5.4f\n", $cp0 * 0.953, $cp30 * 1.0);
+ printf("       0.3      %5.4f   %5.4f\n", $cp0 * 0.906, $cp30 * 1.0);
+ printf("       0.4      %5.4f   %5.4f\n", $cp0 * 0.797, $cp30 * 1.0);
+ printf("       0.5      %5.4f   %5.4f\n", $cp0 * 0.656, $cp30 * 0.989);
+ printf("       0.6      %5.4f   %5.4f\n", $cp0 * 0.531, $cp30 * 0.978);
+ printf("       0.7      %5.4f   %5.4f\n", $cp0 * 0.313, $cp30 * 0.956);
+ printf("       0.8      %5.4f   %5.4f\n", $cp0 * 0.125, $cp30 * 0.911);
+ printf("       1.0      %5.4f   %5.4f\n", $cp0 * -0.375, $cp30 * 0.744);
+ printf("       1.2      %5.4f   %5.4f\n", $cp0 * -1.093, $cp30 * 0.500);
+ printf("       1.4      %5.4f   %5.4f\n", $cp0 * -2.030, $cp30 * 0.250);
+ printf("       1.6      %5.4f   %5.4f\n", $cp0 * -3.0, $cp30 * -0.022);
+ printf("       1.8      %5.4f   %5.4f\n", $cp0 * -4.0, $cp30 * -0.610);
+ printf("       2.0      %5.4f   %5.4f\n", $cp0 * -5.0, $cp30 * -1.220);
+ printf("       2.2      %5.4f   %5.4f\n", $cp0 * -6.0, $cp30 * -1.830);
+ printf("       2.4      %5.4f   %5.4f\n", $cp0 * -7.0, $cp30 * -2.440);
+ print("     </tableData>\n");
+ print("  </table>\n");
 }
 
 print("\n</propeller>\n");
