ac767145ffddc714c703fb76ff2ac23418350227
jberndt <jberndt>
2008-05-15 02:54:50 +0000

    Added Adam Dershowitz' autogen.sh patch for libtool fix

--- autogen.sh
+++ autogen.sh
@@ -37,8 +37,9 @@ DIE=0
   DIE=1
 }
 
+LIBTOOL=`(which glibtool || which libtool || echo "libtool") 2> /dev/null`
 (grep "^AC_PROG_LIBTOOL" configure.in >/dev/null) && {
-  (libtool --version) < /dev/null > /dev/null 2>&1 || {
+  ($LIBTOOL --version) < /dev/null > /dev/null 2>&1 || {
     echo
     echo "**Error**: You must have \`libtool' installed to compile $PKG_NAME."
     echo "Get ftp://ftp.gnu.org/pub/gnu/libtool-1.2d.tar.gz"
