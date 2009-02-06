c74838c31685f1181b286c35a018360c790068e9
andgi <andgi>
2009-02-06 19:13:41 +0000

    Renamed /consumables/fuel/tank[i]/level-lb to /consumables/fuel/tank[i]/level-lbs.

--- src/JSBSim.cxx
+++ src/JSBSim.cxx
@@ -18,7 +18,7 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 //
-// $Id: JSBSim.cxx,v 1.38 2009/01/24 20:36:53 andgi Exp $
+// $Id: JSBSim.cxx,v 1.39 2009/02/06 19:13:41 andgi Exp $
 
 
 #ifdef HAVE_CONFIG_H
@@ -211,7 +211,7 @@ FGJSBsim::FGJSBsim( double dt )
       if (node->getChild("level-gal_us", 0, false) != 0) {
         Propulsion->GetTank(i)->SetContents(node->getDoubleValue("level-gal_us") * 6.6);
       } else {
-        node->setDoubleValue("level-lb", Propulsion->GetTank(i)->GetContents());
+        node->setDoubleValue("level-lbs", Propulsion->GetTank(i)->GetContents());
         node->setDoubleValue("level-gal_us", Propulsion->GetTank(i)->GetContents() / 6.6);
       }
       node->setDoubleValue("capacity-gal_us",
@@ -626,7 +626,7 @@ bool FGJSBsim::copy_to_JSBsim()
       SGPropertyNode * node = fgGetNode("/consumables/fuel/tank", i, true);
       FGTank * tank = Propulsion->GetTank(i);
       tank->SetContents(node->getDoubleValue("level-gal_us") * 6.6);
-//       tank->SetContents(node->getDoubleValue("level-lb"));
+//       tank->SetContents(node->getDoubleValue("level-lbs"));
     }
 
     Propulsion->SetFuelFreeze((fgGetNode("/sim/freeze/fuel",true))->getBoolValue());
@@ -866,7 +866,7 @@ bool FGJSBsim::copy_from_JSBsim()
         double contents = tank->GetContents();
         double temp = tank->GetTemperature_degC();
         node->setDoubleValue("level-gal_us", contents/6.6);
-        node->setDoubleValue("level-lb", contents);
+        node->setDoubleValue("level-lbs", contents);
         if (temp != -9999.0) node->setDoubleValue("temperature_degC", temp);
       }
     }
