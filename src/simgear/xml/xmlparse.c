2d275a1bc342faa505eb63d69edb703870d6245b
jberndt <jberndt>
2009-05-17 13:47:41 +0000

    Turn off debugging (due to assert() problem)

--- src/simgear/xml/xmlparse.c
+++ src/simgear/xml/xmlparse.c
@@ -1,6 +1,7 @@
 /* Copyright (c) 1998, 1999, 2000 Thai Open Source Software Center Ltd
    See the file COPYING for copying permission.
 */
+#define NDEBUG
 
 #include <stddef.h>
 #include <string.h>                     /* memset(), memcpy() */
