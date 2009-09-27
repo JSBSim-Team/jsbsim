bc16e773f87488dd322c21bb3909703a3e0aca60
dpculp <dpculp>
2009-09-27 15:28:52 +0000

    Updates from Ron for FGPiston, also added upper bounds for turbine tables.

--- src/utilities/aeromatic/engine2.php
+++ src/utilities/aeromatic/engine2.php
@@ -1,6 +1,6 @@
 <?php
 
-$version = 0.81;
+$version = 0.82;
 
 //****************************************************
 //                                                   *
@@ -85,18 +85,24 @@ function MakePiston() {
   $displacement = $ac_enginepower * 1.9;
 
   print("<piston_engine name=\"$ac_enginename\">\n");
-  print("  <minmp unit=\"INHG\">          6.0 </minmp><!-- Deprecated -->\n");
-  print("  <maxmp unit=\"INHG\">         29.9 </maxmp><!-- Deprecated -->\n");
+  print("  <minmp unit=\"INHG\">          6.0 </minmp>\n");
+  print("  <maxmp unit=\"INHG\">         28.5 </maxmp>\n");
   printf("  <displacement unit=\"IN3\"> %3.2f </displacement>\n", $displacement);
-  print("<!-- Defining <bsfc> over-rides the bsfc value calculated from <maxhp> -->\n");
-  print("  <!--<bsfc>           0.45 </bsfc>-->\n");
   printf("  <maxhp>        %3.2f </maxhp>\n", $ac_enginepower);
   print("  <cycles>         4.0 </cycles>\n");
   print("  <idlerpm>      700.0 </idlerpm>\n");
   print("  <maxrpm>      2800.0 </maxrpm>\n");
-  print("  <maxthrottle>    1.0 </maxthrottle>\n");
-  print("  <minthrottle>    0.2 </minthrottle>\n");
-  print("  <sparkfaildrop>  0.0 </sparkfaildrop>\n");
+  print("  <maxthrottle>    1.0 </maxthrottle><!-- Deprecated -->\n");
+  print("  <minthrottle>    0.1 </minthrottle><!-- Deprecated -->\n");
+  print("  <sparkfaildrop>  0.1 </sparkfaildrop>\n");
+  print("  <volumetric-efficiency> 0.85 </volumetric-efficiency>\n");
+  print("<!-- Optional items that affect engine performance -->\n");
+  print(" <!-- Defining <bsfc> over-rides the built-in horsepower calculations -->\n");
+  print(" <!--<bsfc>           0.45 </bsfc>-->\n");
+  print(" <!--<stroke unit=\"IN\">  4.375  </stroke>-->\n");
+  print(" <!--<bore unit=\"IN\">    5.125 </bore>-->\n");
+  print(" <!--<cylinders>         4.0  </cylinders>-->\n");
+  print(" <!--<compression-ratio> 8.0 </compression-ratio>-->\n");
   print("</piston_engine>\n");
   }
 
@@ -148,13 +154,13 @@ function MakeTurbine() {
   print("    <independentVar lookup=\"row\">velocities/mach</independentVar>\n");  
   print("    <independentVar lookup=\"column\">atmosphere/density-altitude</independentVar>\n");  
   print("    <tableData>\n");
-  print("         -10000     0     10000   20000   30000   40000   50000\n");
-  print("     0.0  0.0430  0.0488  0.0528  0.0694  0.0899  0.1183  0.1467\n");
-  print("     0.2  0.0500  0.0501  0.0335  0.0544  0.0797  0.1049  0.1342\n");
-  print("     0.4  0.0040  0.0047  0.0020  0.0272  0.0595  0.0891  0.1203\n");
-  print("     0.6  0.0     0.0     0.0     0.0     0.0276  0.0718  0.1073\n");
-  print("     0.8  0.0     0.0     0.0     0.0     0.0474  0.0868  0.0900\n");
-  print("     1.0  0.0     0.0     0.0     0.0     0.0     0.0552  0.0800\n");
+  print("         -10000     0     10000   20000   30000   40000   50000   60000\n");
+  print("     0.0  0.0430  0.0488  0.0528  0.0694  0.0899  0.1183  0.1467  0\n");
+  print("     0.2  0.0500  0.0501  0.0335  0.0544  0.0797  0.1049  0.1342  0\n");
+  print("     0.4  0.0040  0.0047  0.0020  0.0272  0.0595  0.0891  0.1203  0\n");
+  print("     0.6  0.0     0.0     0.0     0.0     0.0276  0.0718  0.1073  0\n");
+  print("     0.8  0.0     0.0     0.0     0.0     0.0474  0.0868  0.0900  0\n");
+  print("     1.0  0.0     0.0     0.0     0.0     0.0     0.0552  0.0800  0\n");
   print("    </tableData>\n");
   print("   </table>\n");
   print("  </function>\n\n");
@@ -164,15 +170,15 @@ function MakeTurbine() {
   print("    <independentVar lookup=\"row\">velocities/mach</independentVar>\n");  
   print("    <independentVar lookup=\"column\">atmosphere/density-altitude</independentVar>\n");  
   print("    <tableData>\n");
-  print("          -10000       0   10000   20000   30000   40000   50000\n");
-  print("     0.0   1.2600  1.0000  0.7400  0.5340  0.3720  0.2410  0.1490\n");
-  print("     0.2   1.1710  0.9340  0.6970  0.5060  0.3550  0.2310  0.1430\n");
-  print("     0.4   1.1500  0.9210  0.6920  0.5060  0.3570  0.2330  0.1450\n");
-  print("     0.6   1.1810  0.9510  0.7210  0.5320  0.3780  0.2480  0.1540\n");
-  print("     0.8   1.2580  1.0200  0.7820  0.5820  0.4170  0.2750  0.1700\n");
-  print("     1.0   1.3690  1.1200  0.8710  0.6510  0.4750  0.3150  0.1950\n");
-  print("     1.2   1.4850  1.2300  0.9750  0.7440  0.5450  0.3640  0.2250\n");
-  print("     1.4   1.5941  1.3400  1.0860  0.8450  0.6280  0.4240  0.2630\n");
+  print("          -10000       0   10000   20000   30000   40000   50000   60000\n");
+  print("     0.0   1.2600  1.0000  0.7400  0.5340  0.3720  0.2410  0.1490  0\n");
+  print("     0.2   1.1710  0.9340  0.6970  0.5060  0.3550  0.2310  0.1430  0\n");
+  print("     0.4   1.1500  0.9210  0.6920  0.5060  0.3570  0.2330  0.1450  0\n");
+  print("     0.6   1.1810  0.9510  0.7210  0.5320  0.3780  0.2480  0.1540  0\n");
+  print("     0.8   1.2580  1.0200  0.7820  0.5820  0.4170  0.2750  0.1700  0\n");
+  print("     1.0   1.3690  1.1200  0.8710  0.6510  0.4750  0.3150  0.1950  0\n");
+  print("     1.2   1.4850  1.2300  0.9750  0.7440  0.5450  0.3640  0.2250  0\n");
+  print("     1.4   1.5941  1.3400  1.0860  0.8450  0.6280  0.4240  0.2630  0\n");
   print("    </tableData>\n");
   print("   </table>\n");
   print("  </function>\n\n");
@@ -183,21 +189,21 @@ function MakeTurbine() {
   print("    <independentVar lookup=\"row\">velocities/mach</independentVar>\n");  
   print("    <independentVar lookup=\"column\">atmosphere/density-altitude</independentVar>\n");  
   print("    <tableData>\n");
-  print("           -10000       0   10000   20000   30000   40000   50000\n");
-  print("     0.0    1.1816  1.0000  0.8184  0.6627  0.5280  0.3756  0.2327\n");
-  print("     0.2    1.1308  0.9599  0.7890  0.6406  0.5116  0.3645  0.2258\n");
-  print("     0.4    1.1150  0.9474  0.7798  0.6340  0.5070  0.3615  0.2240\n");
-  print("     0.6    1.1284  0.9589  0.7894  0.6420  0.5134  0.3661  0.2268\n");
-  print("     0.8    1.1707  0.9942  0.8177  0.6647  0.5309  0.3784  0.2345\n");
-  print("     1.0    1.2411  1.0529  0.8648  0.7017  0.5596  0.3983  0.2467\n");
-  print("     1.2    1.3287  1.1254  0.9221  0.7462  0.5936  0.4219  0.2614\n");
-  print("     1.4    1.4365  1.2149  0.9933  0.8021  0.6360  0.4509  0.2794\n");
-  print("     1.6    1.5711  1.3260  1.0809  0.8700  0.6874  0.4860  0.3011\n");
-  print("     1.8    1.7301  1.4579  1.1857  0.9512  0.7495  0.5289  0.3277\n");
-  print("     2.0    1.8314  1.5700  1.3086  1.0474  0.8216  0.5786  0.3585\n");
-  print("     2.2    1.9700  1.6900  1.4100  1.2400  0.9100  0.6359  0.3940\n");
-  print("     2.4    2.0700  1.8000  1.5300  1.3400  1.0000  0.7200  0.4600\n");
-  print("     2.6    2.2000  1.9200  1.6400  1.4400  1.1000  0.8000  0.5200\n");
+  print("           -10000       0   10000   20000   30000   40000   50000   60000\n");
+  print("     0.0    1.1816  1.0000  0.8184  0.6627  0.5280  0.3756  0.2327  0\n");
+  print("     0.2    1.1308  0.9599  0.7890  0.6406  0.5116  0.3645  0.2258  0\n");
+  print("     0.4    1.1150  0.9474  0.7798  0.6340  0.5070  0.3615  0.2240  0\n");
+  print("     0.6    1.1284  0.9589  0.7894  0.6420  0.5134  0.3661  0.2268  0\n");
+  print("     0.8    1.1707  0.9942  0.8177  0.6647  0.5309  0.3784  0.2345  0\n");
+  print("     1.0    1.2411  1.0529  0.8648  0.7017  0.5596  0.3983  0.2467  0\n");
+  print("     1.2    1.3287  1.1254  0.9221  0.7462  0.5936  0.4219  0.2614  0\n");
+  print("     1.4    1.4365  1.2149  0.9933  0.8021  0.6360  0.4509  0.2794  0\n");
+  print("     1.6    1.5711  1.3260  1.0809  0.8700  0.6874  0.4860  0.3011  0\n");
+  print("     1.8    1.7301  1.4579  1.1857  0.9512  0.7495  0.5289  0.3277  0\n");
+  print("     2.0    1.8314  1.5700  1.3086  1.0474  0.8216  0.5786  0.3585  0\n");
+  print("     2.2    1.9700  1.6900  1.4100  1.2400  0.9100  0.6359  0.3940  0\n");
+  print("     2.4    2.0700  1.8000  1.5300  1.3400  1.0000  0.7200  0.4600  0\n");
+  print("     2.6    2.2000  1.9200  1.6400  1.4400  1.1000  0.8000  0.5200  0\n");
   print("    </tableData>\n");
   print("   </table>\n");
   print("  </function>\n\n");
@@ -254,9 +260,9 @@ if(($ac_engineunits == 0) || ($ac_engineunits == 1)) {
   print("    <independentVar lookup=\"column\">atmosphere/density-altitude</independentVar>\n");  
   print("    <tableData>\n");
   print("         -10000       0   10000   20000   30000   40000   50000\n");
-  print("     0.0  0.0430  0.0488  0.0528  0.0694  0.0899  0.1183  0.1467\n");
-  print("     0.2  0.0500  0.0501  0.0335  0.0544  0.0797  0.1049  0.1342\n");
-  print("     0.4  0.0040  0.0047  0.0020  0.0272  0.0595  0.0891  0.1203\n");
+  print("     0.0  0.0430  0.0488  0.0528  0.0694  0.0899  0.1183  0.0\n");
+  print("     0.2  0.0500  0.0501  0.0335  0.0544  0.0797  0.1049  0.0\n");
+  print("     0.4  0.0040  0.0047  0.0020  0.0272  0.0595  0.0891  0.0\n");
   print("     0.6  0.0     0.0     0.0     0.0276  0.0718  0.0430  0.0\n");
   print("     0.8  0.0     0.0     0.0     0.0     0.0174  0.0086  0.0\n");
   print("     1.0  0.0     0.0     0.0     0.0     0.0     0.0     0.0\n");
@@ -270,10 +276,10 @@ if(($ac_engineunits == 0) || ($ac_engineunits == 1)) {
   print("    <independentVar lookup=\"column\">atmosphere/density-altitude</independentVar>\n");  
   print("    <tableData>\n");
   print("         -10000       0   10000   20000   30000   40000   50000\n");
-  print("     0.0  1.1260  1.0000  0.7400  0.5340  0.3720  0.2410  0.1490\n");
-  print("     0.2  1.1000  0.9340  0.6970  0.5060  0.3550  0.2310  0.1430\n");
-  print("     0.4  1.0000  0.6410  0.6120  0.4060  0.3570  0.2330  0.1450\n");
-  print("     0.6  0.4430  0.3510  0.2710  0.2020  0.1780  0.1020  0.0640\n");
+  print("     0.0  1.1260  1.0000  0.7400  0.5340  0.3720  0.2410  0.0\n");
+  print("     0.2  1.1000  0.9340  0.6970  0.5060  0.3550  0.2310  0.0\n");
+  print("     0.4  1.0000  0.6410  0.6120  0.4060  0.3570  0.2330  0.0\n");
+  print("     0.6  0.4430  0.3510  0.2710  0.2020  0.1780  0.1020  0.0\n");
   print("     0.8  0.0240  0.0200  0.0160  0.0130  0.0110  0.0100  0.0\n");
   print("     1.0  0.0     0.0     0.0     0.0     0.0     0.0     0.0\n");
   print("    </tableData>\n");
