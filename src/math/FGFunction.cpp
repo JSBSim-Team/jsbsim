bacad70c9a196655aca2bd1f5e9fff99ea8020e6
jberndt <jberndt>
2012-09-05 04:54:49 +0000

    Added an interpolation function and a 'copy tp' attribute to functions.

--- src/math/FGFunction.cpp
+++ src/math/FGFunction.cpp
@@ -43,7 +43,7 @@ using namespace std;
 
 namespace JSBSim {
 
-static const char *IdSrc = "$Id: FGFunction.cpp,v 1.44 2012/08/11 15:02:19 jberndt Exp $";
+static const char *IdSrc = "$Id: FGFunction.cpp,v 1.45 2012/09/05 04:54:49 jberndt Exp $";
 static const char *IdHdr = ID_FUNCTION;
 
 /*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -101,6 +101,7 @@ const std::string FGFunction::or_string = "or";
 const std::string FGFunction::not_string = "not";
 const std::string FGFunction::ifthen_string = "ifthen";
 const std::string FGFunction::switch_string = "switch";
+const std::string FGFunction::interpolate1d_string = "interpolate1d";
 
 FGFunction::FGFunction(FGPropertyManager* propMan, Element* el, const string& prefix)
                                       : PropertyManager(propMan), Prefix(prefix)
@@ -110,11 +111,18 @@ FGFunction::FGFunction(FGPropertyManager* propMan, Element* el, const string& pr
   cached = false;
   cachedValue = -HUGE_VAL;
   invlog2val = 1.0/log10(2.0);
+  pCopyTo = 0L;
 
   Name = el->GetAttributeValue("name");
   operation = el->GetName();
 
   if (operation == function_string) {
+    sCopyTo = el->GetAttributeValue("copyto");
+    if (!sCopyTo.empty()) {
+      pCopyTo = PropertyManager->GetNode(sCopyTo);
+      if (pCopyTo == 0L) cerr << "Property \"" << sCopyTo << "\" must be previously defined in function "
+                              << Name << endl;
+    }
     Type = eTopLevel;
   } else if (operation == product_string) {
     Type = eProduct;
@@ -198,6 +206,8 @@ FGFunction::FGFunction(FGPropertyManager* propMan, Element* el, const string& pr
     Type = eIfThen;
   } else if (operation == switch_string) {
     Type = eSwitch;
+  } else if (operation == interpolate1d_string) {
+    Type = eInterpolate1D;
   } else if (operation != description_string) {
     cerr << "Bad operation " << operation << " detected in configuration file" << endl;
   }
@@ -279,7 +289,8 @@ FGFunction::FGFunction(FGPropertyManager* propMan, Element* el, const string& pr
                operation == or_string ||
                operation == not_string ||
                operation == ifthen_string ||
-               operation == switch_string)
+               operation == switch_string ||
+               operation == interpolate1d_string)
     {
       Parameters.push_back(new FGFunction(PropertyManager, element, Prefix));
     } else if (operation != description_string) {
@@ -338,6 +349,7 @@ double FGFunction::GetValue(void) const
   
   switch (Type) {
   case eTopLevel:
+    if (pCopyTo) pCopyTo->setDoubleValue(temp);
     break;
   case eProduct:
     for (i=1;i<Parameters.size();i++) {
@@ -496,6 +508,27 @@ double FGFunction::GetValue(void) const
       } else {
         throw(string("The switch function index selected a value above the range of supplied values"
                      " - not enough values were supplied."));
+      }
+    }
+    break;
+  case eInterpolate1D:
+    {
+      unsigned int sz = Parameters.size();
+      if (temp <= Parameters[1]->GetValue()) {
+        temp = Parameters[2]->GetValue();
+      } else if (temp >= Parameters[sz-2]->GetValue()) {
+        temp = Parameters[sz-1]->GetValue();
+      } else {
+        for (unsigned int i=1; i<=sz-4; i+=2) {
+          if (temp < Parameters[i+2]->GetValue()) {
+            double factor = (temp - Parameters[i]->GetValue()) /
+                            (Parameters[i+2]->GetValue() - Parameters[i]->GetValue());
+            double span = Parameters[i+3]->GetValue() - Parameters[i+1]->GetValue();
+            double val = factor*span;
+            temp = Parameters[i+1]->GetValue() + val;
+            break;
+          }
+        }
       }
     }
     break;
@@ -731,7 +764,11 @@ void FGFunction::bind(void)
       }
     }
 
+    if (PropertyManager->HasNode(tmp)) {
+      cout << "Property " << tmp << " has already been successfully bound (late)." << endl;
+    } else {
     PropertyManager->Tie( tmp, this, &FGFunction::GetValue);
+    }
   }
 }
 
