e50b0effd0f3db7d4fdb35976e5b3495f63b5304
bcoconni <bcoconni>
2014-07-18 22:08:40 +0000

    Updated the regression test to reproduce the new problem reported in the bug #1503 of FlightGear: when <rate_limit> is reading its value from a property, the <actuator> output value is constantly increasing whatever the input.

--- tests/CheckFGBug1503.py
+++ tests/CheckFGBug1503.py
@@ -19,6 +19,12 @@
 # this program; if not, see <http://www.gnu.org/licenses/>
 #
 
+# This test checks that:
+# 1. The JSBSim does not hang when the parameter 'sense' of actuator
+#    <rate_limit> is used.
+# 2. The property 'fcs/left-aileron-pos-rad' remains equal to 0.0 during the
+#    execution of the script c1724.xml when <rate_limit> is used with a property.
+
 import os, shutil, time, sys
 import xml.etree.ElementTree as et
 from multiprocessing import Process
@@ -29,19 +35,23 @@ def ScriptExecution(fdm, script_path):
     fdm.run_ic()
 
     while fdm.run():
-        pass
+        if fdm.get_property_value('fcs/left-aileron-pos-rad') != 0.0:
+            sys.exit(-1)
 
 sandbox = SandBox()
 
-# Run the script c1724.xml
+# First, the execution time of the script c1724.xml is measured. It will be used
+# as a reference to check if JSBSim hangs or not.
 script_path = sandbox.path_to_jsbsim_file('scripts', 'c1724.xml')
-
 fdm = CreateFDM(sandbox)
 start_time = time.time()
 ScriptExecution(fdm, script_path)
 exec_time = time.time() - start_time
 
-# First, extract the aircraft name from the script
+# Since we will alter the aircraft definition file, we need make a copy of it
+# and all the files it is refering to.
+
+# Get the aircraft name
 tree = et.parse(sandbox.elude(script_path))
 use_element = tree.getroot().find('use')
 aircraft_name = use_element.attrib['aircraft']
@@ -57,10 +67,24 @@ if not os.path.exists(aircraft_path):
 IC_file = append_xml(use_element.attrib['initialize'])
 shutil.copy(os.path.join(path_to_jsbsim_aircrafts, IC_file), aircraft_path)
 
-# Modify the rate_limit element and split in 2 elements : one with sense 'decr'
-# the other with sense 'incr'.
 tree = et.parse(os.path.join(path_to_jsbsim_aircrafts, aircraft_name+'.xml'))
-actuator_element = tree.getroot().find('flight_control/channel/actuator//rate_limit/..')
+root = tree.getroot()
+
+# The aircraft definition file may also load some data from external files.
+# If so, we need to copy these files in our directory build/.../aircraft/aircraft_name
+# Only the external files that are in the original directory aircraft/aircraft_name
+# will be copied. The files located in 'engine' and 'systems' do not need to be
+# copied.
+for element in list(root):
+    if 'file' in element.keys():
+        name = append_xml(element.attrib['file'])
+        name_with_path = os.path.join(path_to_jsbsim_aircrafts, name)
+        if os.path.exists(name_with_path):
+            shutil.copy(name_with_path, aircraft_path)
+
+# Now the copy of the aircraft definition file will be altered: the <rate_limit>
+# element is split in two: one with sense 'decr', the other with sense 'incr'.
+actuator_element = root.find('flight_control/channel/actuator//rate_limit/..')
 rate_element = actuator_element.find('rate_limit')
 rate_element.attrib['sense'] = 'decr'
 new_rate_element = et.SubElement(actuator_element, 'rate_limit')
@@ -69,27 +93,49 @@ new_rate_element.text = str(float(rate_element.text) * 0.5)
 
 tree.write(sandbox('aircraft', aircraft_name, aircraft_name+'.xml'))
 
-# The aircraft definition file may already load some data from external files.
-# If so, we need to copy these files in our directory build/.../aircraft/aircraft_name
-# Only the external files that are in the original directory aircraft/aircraft_name
-# will be copied. The files located in 'engine' and 'systems' do not need to be
-# copied.
-for element in list(tree.getroot()):
-    if 'file' in element.keys():
-        name = append_xml(element.attrib['file'])
-        name_with_path = os.path.join(path_to_jsbsim_aircrafts, name)
-        if os.path.exists(name_with_path):
-            shutil.copy(name_with_path, aircraft_path)
-
-# Run the script
+# Run the script with the modified aircraft
 fdm = CreateFDM(sandbox)
 fdm.set_aircraft_path('aircraft')
 
+# A new process is created that launches the script. We wait for 10 times the
+# reference execution time for the script completion. Beyond that time, if the
+# process is not completed, it is terminated and the test is failed.
 p = Process(target=ScriptExecution, args=(fdm, script_path))
 p.start()
-p.join(exec_time * 10.0)
+p.join(exec_time * 10.0) # Wait 10 times the reference time
 if p.is_alive():
+    # The process has not yet completed: it means it probably hanged.
     p.terminate()
     sys.exit(-1)
+
+# Second part of the test.
+# #######################
+#
+# The test is run again but this time, <rate_limit> will be read from a
+# property instead of being read from a value hard coded in the aircraft
+# definition file. It has been reported in the bug 1503 of FlightGear that for
+# such a configuration the <actuator> output is constantly increasing even if
+# the input is null. For this script the <actuator> output is stored in the
+# property fcs/left-aileron-pos-rad. The function ScriptExecution will monitor
+# that property and if it changes then the test is failed.
+
+tree = et.parse(os.path.join(path_to_jsbsim_aircrafts, aircraft_name+'.xml'))
+flight_control_element = tree.getroot().find('flight_control')
+property = et.SubElement(flight_control_element, 'property')
+property.text = 'fcs/rate-limit-value'
+property.attrib['value'] = rate_element.text
+actuator_element = flight_control_element.find('channel/actuator//rate_limit/..')
+rate_element = actuator_element.find('rate_limit')
+rate_element.attrib['sense'] = 'decr'
+rate_element.text = property.text
+new_rate_element = et.SubElement(actuator_element, 'rate_limit')
+new_rate_element.attrib['sense'] = 'incr'
+new_rate_element.text = rate_element.text
+
+tree.write(sandbox('aircraft', aircraft_name, aircraft_name+'.xml'))
+
+fdm = CreateFDM(sandbox)
+fdm.set_aircraft_path('aircraft')
+ScriptExecution(fdm, script_path)
 
 sandbox.erase()
