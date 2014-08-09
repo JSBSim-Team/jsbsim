066f317ff1a3fc5f48eba0f821c81debd679cc71
bcoconni <bcoconni>
2014-08-09 17:36:22 +0000

    Updated the regression test to reproduce a new problem reported in FG bug #1503 : the rate_limit is ignored when set by a property.

--- tests/CheckFGBug1503.py
+++ tests/CheckFGBug1503.py
@@ -24,17 +24,19 @@
 #    <rate_limit> is used.
 # 2. The property 'fcs/left-aileron-pos-rad' remains equal to 0.0 during the
 #    execution of the script c1724.xml when <rate_limit> is used with a property.
+# 3. That the actuator output value is correctly driven by rate_limit.
 
-import os, shutil, time, sys
+import os, shutil, time, sys, string
 import xml.etree.ElementTree as et
 from multiprocessing import Process
+from scipy import stats
 from JSBSim_utils import SandBox, CreateFDM, append_xml
 
-def ScriptExecution(fdm, script_path):
+def ScriptExecution(fdm, script_path, time_limit = 1E+9):
     fdm.load_script(script_path)
     fdm.run_ic()
 
-    while fdm.run():
+    while fdm.run() and fdm.get_sim_time() < time_limit:
         if fdm.get_property_value('fcs/left-aileron-pos-rad') != 0.0:
             sys.exit(-1)
 
@@ -137,5 +139,135 @@ tree.write(sandbox('aircraft', aircraft_name, aircraft_name+'.xml'))
 fdm = CreateFDM(sandbox)
 fdm.set_aircraft_path('aircraft')
 ScriptExecution(fdm, script_path)
+
+# Third part of the test.
+########################
+#
+# The test is run again but this time we are checking that rate_limit drives
+# the actuator output value as expected. The idea is to store the output value
+# of the actuator output vs the time and check with a linear regression that
+# 1. The actuator output value is evolving linearly
+# 2. The slope of the actuator output is equal to the rate_limit value
+# The test is run with the rate_limit given by a value, a property, different
+# values of the ascending and descending rates and a number of combinations
+# thereof.
+
+def CheckRateValue(fdm, output_prop, rate_value):
+    aileron_course = []
+
+    t0 = fdm.get_sim_time()
+    while fdm.run() and fdm.get_sim_time() <= t0 + 1.0:
+        aileron_course += [(fdm.get_sim_time(),
+                            fdm.get_property_value(output_prop))]
+
+    # Thanks to a linear regression on the values, we can check that the
+    # value is following a slope equal to the rate limit. The correlation
+    # coefficient r_value is also checked to verify that the output is evolving
+    # linearly.
+    slope, intercept, r_value, p_value, std_err = stats.linregress(aileron_course)
+    if abs(slope - rate_value) > 1E-9 or abs(r_value) - 1.0 > 1E-9:
+        sys.exit(-1)
+
+def CheckRateLimit(script_path, input_prop, output_prop, incr_limit, decr_limit):
+    fdm = CreateFDM(sandbox)
+    fdm.set_aircraft_path('aircraft')
+
+    ScriptExecution(fdm, script_path, 1.0)
+
+    fdm.set_property_value(input_prop, 1.0)
+
+    CheckRateValue(fdm, output_prop, incr_limit)
+
+    fdm.set_property_value(input_prop, 0.0)
+    CheckRateValue(fdm, output_prop, decr_limit)
+
+# The aircraft file definition is modified such that the actuator element
+# input is driven by a unique property. The name of this unique property
+# is built in the variable 'input_prop' below. When setting that property to 1.0
+# (resp. -1.0) the ascending (resp. descending) rate is triggered.
+tree = et.parse(os.path.join(path_to_jsbsim_aircrafts, aircraft_name+'.xml'))
+flight_control_element = tree.getroot().find('flight_control')
+actuator_element = flight_control_element.find('channel/actuator//rate_limit/..')
+
+# Remove the hysteresis. We want to make sure we are measuring the rate_limit
+# and just that.
+hysteresis_element = actuator_element.find('hysteresis')
+actuator_element.remove(hysteresis_element)
+input_element = actuator_element.find('input')
+input_prop = string.split(actuator_element.attrib['name'], '-')
+input_prop[-1] = 'input'
+input_prop = string.join(input_prop, '-')
+input_element.text = input_prop
+output_element = actuator_element.find('output')
+output_prop = string.strip(output_element.text)
+
+# Add the new properties to <flight_control> so that we can make reference to
+# them without JSBSim complaining
+property = et.SubElement(flight_control_element, 'property')
+property.text = input_prop
+property.attrib['value'] = '0.0'
+property = et.SubElement(flight_control_element, 'property')
+property.text = 'fcs/rate-limit-value'
+property.attrib['value'] = '0.15'
+property = et.SubElement(flight_control_element, 'property')
+property.text = 'fcs/rate-limit-value2'
+property.attrib['value'] = '0.05'
+
+# First check with rate_limit set to 0.1
+
+rate_element = actuator_element.find('rate_limit')
+rate_element.text = '0.1'
+
+output_file = sandbox('aircraft', aircraft_name, aircraft_name+'.xml')
+tree.write(output_file)
+
+CheckRateLimit(script_path, input_prop, output_prop, 0.1, -0.1)
+
+# Check when rate_limit is set by the property 'fcs/rate-limit-value'
+
+tree = et.parse(output_file)
+flight_control_element = tree.getroot().find('flight_control')
+actuator_element = flight_control_element.find('channel/actuator//rate_limit/..')
+rate_element = actuator_element.find('rate_limit')
+rate_element.text = 'fcs/rate-limit-value'
+tree.write(output_file)
+
+CheckRateLimit(script_path, input_prop, output_prop, 0.15, -0.15)
+
+# Checking when the ascending and descending rates are different.
+# First with the 2 rates set by hard coded values (0.1 and 0.2 respectively)
+
+rate_element.attrib['sense'] = 'decr'
+rate_element.text = '0.1'
+new_rate_element = et.SubElement(actuator_element, 'rate_limit')
+new_rate_element.attrib['sense'] = 'incr'
+new_rate_element.text = '0.2'
+tree.write(output_file)
+
+CheckRateLimit(script_path, input_prop, output_prop, 0.2, -0.1)
+
+# Check when the descending rate is set by a property and the ascending rate is
+# set by a value.
+
+rate_element.text = 'fcs/rate-limit-value'
+tree.write(output_file)
+
+CheckRateLimit(script_path, input_prop, output_prop, 0.2, -0.15)
+
+# Check when the ascending rate is set by a property and the descending rate is
+# set by a value.
+
+rate_element.text = '0.1'
+new_rate_element.text = 'fcs/rate-limit-value'
+tree.write(output_file)
+
+CheckRateLimit(script_path, input_prop, output_prop, 0.15, -0.1)
+
+# Check when the ascending and descending rates are set by properties
+
+rate_element.text = 'fcs/rate-limit-value2'
+tree.write(output_file)
+
+CheckRateLimit(script_path, input_prop, output_prop, 0.15, -0.05)
 
 sandbox.erase()
