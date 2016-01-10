644262b9df2a26dc1a174256e092ee577362e679
bcoconni <bcoconni>
2016-01-10 19:30:59 +0000

    Check that when the wind direction changes, the CAS changes accordingly and that the Pitot tube angle is taken into account

--- tests/TestPitotAngle.py
+++ tests/TestPitotAngle.py
@@ -18,9 +18,9 @@
 # this program; if not, see <http://www.gnu.org/licenses/>
 #
 
-import unittest, sys, os
+import unittest, sys, os, math
 import xml.etree.ElementTree as et
-from JSBSim_utils import SandBox, CreateFDM, CopyAircraftDef, append_xml
+from JSBSim_utils import SandBox, CreateFDM, CopyAircraftDef, append_xml, ExecuteUntil
 
 
 class TestPitotAngle(unittest.TestCase):
@@ -32,7 +32,10 @@ class TestPitotAngle(unittest.TestCase):
 
     def test_CAS_ic(self):
         script_name = 'Short_S23_3.xml'
-        script_path = self.sandbox.path_to_jsbsim_file('scripts', script_name)
+        script_path = self.sandbox.path_to_jsbsim_file('scripts',
+                                                       script_name)
+
+        # Add a Pitot angle to the Short S23
         tree, aircraft_name, path_to_jsbsim_aircrafts = CopyAircraftDef(script_path, self.sandbox)
         metrics_tag = tree.getroot().find('./metrics')
         pitot_tag = et.SubElement(metrics_tag, 'pitot_angle')
@@ -41,22 +44,89 @@ class TestPitotAngle(unittest.TestCase):
         tree.write(self.sandbox('aircraft', aircraft_name,
                                 aircraft_name+'.xml'))
 
+        # Read the CAS specified in the IC file
         tree = et.parse(self.sandbox.elude(script_path))
         use_element = tree.getroot().find('use')
         IC_file = use_element.attrib['initialize']
-        tree = et.parse(os.path.join(path_to_jsbsim_aircrafts, append_xml(IC_file)))
+        tree = et.parse(os.path.join(path_to_jsbsim_aircrafts,
+                                     append_xml(IC_file)))
         vc_tag = tree.getroot().find('./vc')
         VCAS = float(vc_tag.text)
         if 'unit' in vc_tag.attrib and vc_tag.attrib['unit'] == 'FT/SEC':
             VCAS /= 1.68781
 
+        # Run the IC and check that the model is initialized correctly
         fdm = CreateFDM(self.sandbox)
         fdm.set_aircraft_path('aircraft')
         fdm.load_script(script_path)
         fdm.run_ic()
 
+        self.assertAlmostEqual(fdm.get_property_value('ic/vc-kts'),
+                               VCAS, delta=1E-7)
         self.assertAlmostEqual(fdm.get_property_value('velocities/vc-kts'),
                                VCAS, delta=1E-7)
+
+    def test_pitot_angle(self):
+        script_name = 'ball_chute.xml'
+        script_path = self.sandbox.path_to_jsbsim_file('scripts', script_name)
+
+        # Add a Pitot angle to the Cessna 172
+        tree, aircraft_name, path_to_jsbsim_aircrafts = CopyAircraftDef(script_path, self.sandbox)
+        root = tree.getroot()
+        metrics_tag = root.find('./metrics')
+        pitot_tag = et.SubElement(metrics_tag, 'pitot_angle')
+        pitot_tag.attrib['unit'] = 'DEG'
+        pitot_tag.text = '5.0'
+        contact_tag = root.find('./ground_reactions/contact')
+        contact_tag.attrib['type'] = 'STRUCTURE'
+        tree.write(self.sandbox('aircraft', aircraft_name,
+                                aircraft_name+'.xml'))
+
+        fdm = CreateFDM(self.sandbox)
+        fdm.set_aircraft_path('aircraft')
+        fdm.load_model('ball')
+        pitot_angle = float(pitot_tag.text) * math.pi / 180.
+        weight = fdm.get_property_value('inertia/weight-lbs')
+        spring_tag = contact_tag.find('./spring_coeff')
+        spring_coeff = float(spring_tag.text)
+        print "Weight=%d Spring=%d" % (weight, spring_coeff)
+        fdm.set_property_value('ic/h-sl-ft', weight / spring_coeff)
+        fdm.set_property_value('forces/hold-down', 1.0)
+        fdm.run_ic()
+
+        ExecuteUntil(fdm, 10.)
+
+        for i in xrange(36):
+            for j in xrange(-9, 10):
+                angle = math.pi * i / 18.0
+                angle2 = math.pi * j / 18.0
+                ca2 = math.cos(angle2)
+                fdm.set_property_value('atmosphere/wind-north-fps',
+                                       10. * math.cos(angle) * ca2)
+                fdm.set_property_value('atmosphere/wind-east-fps',
+                                       10. * math.sin(angle) * ca2)
+                fdm.set_property_value('atmosphere/wind-down-fps',
+                                       10. * math.sin(angle2))
+                fdm.run()
+
+                vg = fdm.get_property_value('velocities/vg-fps')
+                self.assertAlmostEqual(vg, 0.0, delta=1E-7)
+
+                vt = fdm.get_property_value('velocities/vt-fps')
+                self.assertAlmostEqual(vt, 10., delta=1E-7)
+
+                mach = vt / fdm.get_property_value('atmosphere/a-fps')
+                P = fdm.get_property_value('atmosphere/P-psf')
+                pt = P * math.pow(1+0.2*mach*mach, 3.5)
+                psl = fdm.get_property_value('atmosphere/P-sl-psf')
+                rhosl = fdm.get_property_value('atmosphere/rho-sl-slugs_ft3')
+                A = math.pow((pt-P)/psl+1.0, 1.0/3.5)
+                alpha = fdm.get_property_value('aero/alpha-rad')
+                beta = fdm.get_property_value('aero/beta-rad')
+                vc = math.sqrt(7.0*psl/rhosl*(A-1.0))*math.cos(alpha+pitot_angle)*math.cos(beta)
+
+                self.assertAlmostEqual(fdm.get_property_value('velocities/vc-kts'),
+                                       max(0.0, vc) / 1.68781, delta=1E-7)
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestPitotAngle)
 test_result = unittest.TextTestRunner(verbosity=2).run(suite)
