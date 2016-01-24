Fixed the computation of the accelerations. JSBSim code is correct, the test was not.

tests/TestAccelerometer.py
@@ -21,7 +21,8 @@
 
 import sys, unittest, math, os
 import xml.etree.ElementTree as et
-from JSBSim_utils import CreateFDM, SandBox, CopyAircraftDef
+import numpy as np
+from JSBSim_utils import CreateFDM, SandBox, CopyAircraftDef, ExecuteUntil
 
 
 class TestAccelerometer(unittest.TestCase):
@@ -53,22 +54,19 @@ class TestAccelerometer(unittest.TestCase):
         fdm.set_aircraft_path('aircraft')
         fdm.load_script(script_name)
         # Switch the accel on
-        fdm.set_property_value('fcs/accelerometer/on', 1.0)
+        fdm['fcs/accelerometer/on'] = 1.0
         fdm.run_ic()
 
         while fdm.run():
-            self.assertAlmostEqual(fdm.get_property_value('fcs/accelerometer/X'),
-                                   0.0, delta=1E-8)
-            self.assertAlmostEqual(fdm.get_property_value('fcs/accelerometer/Y'),
-                                   0.0, delta=1E-8)
-            self.assertAlmostEqual(fdm.get_property_value('fcs/accelerometer/Z'),
-                                   0.0, delta=1E-8)
-            self.assertAlmostEqual(fdm.get_property_value('accelerations/a-pilot-x-ft_sec2'),
-                                   0.0, delta=1E-8)
-            self.assertAlmostEqual(fdm.get_property_value('accelerations/a-pilot-y-ft_sec2'),
-                                   0.0, delta=1E-8)
-            self.assertAlmostEqual(fdm.get_property_value('accelerations/a-pilot-z-ft_sec2'),
-                                   0.0, delta=1E-8)
+            self.assertAlmostEqual(fdm['fcs/accelerometer/X'], 0.0, delta=1E-8)
+            self.assertAlmostEqual(fdm['fcs/accelerometer/Y'], 0.0, delta=1E-8)
+            self.assertAlmostEqual(fdm['fcs/accelerometer/Z'], 0.0, delta=1E-8)
+            self.assertAlmostEqual(fdm['accelerations/a-pilot-x-ft_sec2'], 0.0,
+                                   delta=1E-8)
+            self.assertAlmostEqual(fdm['accelerations/a-pilot-y-ft_sec2'], 0.0,
+                                   delta=1E-8)
+            self.assertAlmostEqual(fdm['accelerations/a-pilot-z-ft_sec2'], 0.0,
+                                   delta=1E-8)
 
         del fdm
 
@@ -82,36 +80,30 @@ class TestAccelerometer(unittest.TestCase):
         fdm.load_script(script_path)
 
         # Switch the accel on
-        fdm.set_property_value('fcs/accelerometer/on', 1.0)
+        fdm['fcs/accelerometer/on'] = 1.0
         # Use the standard gravity (i.e. GM/r^2)
-        fdm.set_property_value('simulation/gravity-model', 0)
+        fdm['simulation/gravity-model'] = 0
         # Simplifies the transformation to compare the accelerometer with the
         # gravity
-        fdm.set_property_value('ic/psi-true-rad', 0.0)
+        fdm['ic/psi-true-rad'] = 0.0
         fdm.run_ic()
 
-        for i in xrange(500):
+        for i in xrange(1000):
             fdm.run()
 
-        ax = fdm.get_property_value('accelerations/udot-ft_sec2')
-        ay = fdm.get_property_value('accelerations/vdot-ft_sec2')
-        az = fdm.get_property_value('accelerations/wdot-ft_sec2')
-        g = fdm.get_property_value('accelerations/gravity-ft_sec2')
-        theta = fdm.get_property_value('attitude/theta-rad')
+        r = fdm['position/radius-to-vehicle-ft']
+        g = fdm['accelerations/gravity-ft_sec2']
+        latitude = fdm['position/lat-gc-rad']
+        pitch = fdm['attitude/theta-rad']
+        omega = 0.00007292115  # Earth rotation rate in rad/sec
+        fc = r * math.cos(latitude) * omega * omega
 
-        # There is a lag of one time step between the computations of the
-        # accelerations and the update of the accelerometer
-        fdm.run()
-        fax = fdm.get_property_value('fcs/accelerometer/X')
-        fay = fdm.get_property_value('fcs/accelerometer/Y')
-        faz = fdm.get_property_value('fcs/accelerometer/Z')
+        fax = fc * math.sin(latitude - pitch) + g * math.sin(pitch)
+        faz = fc * math.cos(latitude - pitch) - g * math.cos(pitch)
 
-        fax -= ax
-        faz -= az
-
-        self.assertAlmostEqual(fay, 0.0, delta=1E-6)
-        self.assertAlmostEqual(fax / (g * math.sin(theta)), 1.0, delta=1E-5)
-        self.assertAlmostEqual(faz / (g * math.cos(theta)), -1.0, delta=1E-7)
+        self.assertAlmostEqual(fdm['fcs/accelerometer/X'], fax, delta=1E-7)
+        self.assertAlmostEqual(fdm['fcs/accelerometer/Y'], 0.0, delta=1E-7)
+        self.assertAlmostEqual(fdm['fcs/accelerometer/Z'], faz, delta=1E-7)
 
         del fdm
 
@@ -124,40 +116,48 @@ class TestAccelerometer(unittest.TestCase):
         fdm.set_aircraft_path('aircraft')
         fdm.load_script(script_path)
         # Switch the accel on
-        fdm.set_property_value('fcs/accelerometer/on', 1.0)
+        fdm['fcs/accelerometer/on'] = 1.0
         # Use the standard gravity (i.e. GM/r^2)
-        fdm.set_property_value('simulation/gravity-model', 0)
-        # Simplifies the transformation to compare the accelerometer with the
-        # gravity
-        fdm.set_property_value('ic/psi-true-rad', 0.0)
+        fdm['simulation/gravity-model'] = 0
+        # Select an orientation such that frame transformations simplify
+        fdm['ic/psi-true-rad'] = 0.0
         fdm.run_ic()
 
-        while fdm.get_property_value('simulation/sim-time-sec') <= 0.5:
-            fdm.run()
+        ExecuteUntil(fdm, 0.1)
 
-        fdm.set_property_value('simulation/do_simple_trim', 1)
-        ax = fdm.get_property_value('accelerations/udot-ft_sec2')
-        ay = fdm.get_property_value('accelerations/vdot-ft_sec2')
-        az = fdm.get_property_value('accelerations/wdot-ft_sec2')
-        g = fdm.get_property_value('accelerations/gravity-ft_sec2')
-        theta = fdm.get_property_value('attitude/theta-rad')
+        fdm['simulation/do_simple_trim'] = 1
 
-        # There is a lag of one time step between the computations of the
-        # accelerations and the update of the accelerometer
-        fdm.run()
-        fax = fdm.get_property_value('fcs/accelerometer/X')
-        fay = fdm.get_property_value('fcs/accelerometer/Y')
-        faz = fdm.get_property_value('fcs/accelerometer/Z')
+        r = fdm['position/radius-to-vehicle-ft']
+        pitch = fdm['attitude/theta-rad']
+        roll = fdm['attitude/phi-rad']
+        latitude = fdm['position/lat-gc-rad']
+        g = fdm['accelerations/gravity-ft_sec2']
+        omega = 0.00007292115  # Earth rotation rate in rad/sec
+        fc = r * math.cos(latitude) * omega * omega  # Centrifugal force
 
-        fax -= ax
-        fay -= ay
-        faz -= az
+        uvw = np.array(fdm.get_propagate().get_uvw().T)[0]
+        Omega = omega * np.array([math.cos(pitch - latitude),
+                                  math.sin(pitch - latitude) * math.sin(roll),
+                                  math.sin(pitch - latitude) * math.cos(roll)])
+
+        # Compute the acceleration measured by the accelrometer as the sum of
+        # the gravity and the centrifugal and Coriolis forces.
+        fa_yz = (fc * math.cos(latitude - pitch) - g * math.cos(pitch))
+        fa = np.array([(fc * math.sin(latitude - pitch) + g * math.sin(pitch)),
+                       fa_yz * math.sin(roll),
+                       fa_yz * math.cos(roll)]) + np.cross(2.0*Omega, uvw)
+
+        # After the trim we are close to the equilibrium but there remains a
+        # small residual that we have to take the bias into account
+        fax = fa[0] + fdm['accelerations/udot-ft_sec2']
+        fay = fa[1] + fdm['accelerations/vdot-ft_sec2']
+        faz = fa[2] + fdm['accelerations/wdot-ft_sec2']
 
         # Deltas are relaxed because the tolerances of the trimming algorithm
         # are quite relaxed themselves.
-        self.assertAlmostEqual(faz / (g * math.cos(theta)), -1.0, delta=1E-5)
-        self.assertAlmostEqual(fax / (g * math.sin(theta)), 1.0, delta=1E-5)
-        self.assertAlmostEqual(math.sqrt(fax*fax+fay*fay+faz*faz)/g, 1.0, delta=1E-6)
+        self.assertAlmostEqual(fdm['fcs/accelerometer/X'], fax, delta=1E-6)
+        self.assertAlmostEqual(fdm['fcs/accelerometer/Y'], fay, delta=1E-4)
+        self.assertAlmostEqual(fdm['fcs/accelerometer/Z'], faz, delta=1E-5)
 
         del fdm
 
@@ -170,35 +170,35 @@ class TestAccelerometer(unittest.TestCase):
         fdm.set_aircraft_path('aircraft')
         fdm.load_model('ball')
         # Offset the CG along Y (by 30")
-        fdm.set_property_value('inertia/pointmass-weight-lbs[1]', 50.0)
+        fdm['inertia/pointmass-weight-lbs[1]'] = 50.0
 
         aircraft_path = self.sandbox.elude(self.sandbox.path_to_jsbsim_file('aircraft', 'ball'))
         fdm.load_ic(os.path.join(aircraft_path, 'reset00.xml'), False)
         # Switch the accel on
-        fdm.set_property_value('fcs/accelerometer/on', 1.0)
+        fdm['fcs/accelerometer/on'] = 1.0
         # Set the orientation such that the spinning axis is Z.
-        fdm.set_property_value('ic/phi-rad', 0.5*math.pi)
+        fdm['ic/phi-rad'] = 0.5*math.pi
 
         # Set the angular velocities to 0.0 in the ECEF frame. The angular
         # velocity R_{inertial} will therefore be equal to the Earth rotation
         # rate 7.292115E-5 rad/sec
-        fdm.set_property_value('ic/p-rad_sec', 0.0)
-        fdm.set_property_value('ic/q-rad_sec', 0.0)
-        fdm.set_property_value('ic/r-rad_sec', 0.0)
+        fdm['ic/p-rad_sec'] = 0.0
+        fdm['ic/q-rad_sec'] = 0.0
+        fdm['ic/r-rad_sec'] = 0.0
         fdm.run_ic()
 
-        fax = fdm.get_property_value('fcs/accelerometer/X')
-        fay = fdm.get_property_value('fcs/accelerometer/Y')
-        faz = fdm.get_property_value('fcs/accelerometer/Z')
-        cgy_ft = fdm.get_property_value('inertia/cg-y-in') / 12.
-        omega = 0.00007292115 # Earth rotation rate in rad/sec
+        fax = fdm['fcs/accelerometer/X']
+        fay = fdm['fcs/accelerometer/Y']
+        faz = fdm['fcs/accelerometer/Z']
+        cgy_ft = fdm['inertia/cg-y-in'] / 12.
+        omega = 0.00007292115  # Earth rotation rate in rad/sec
 
-        self.assertAlmostEqual(fdm.get_property_value('accelerations/a-pilot-x-ft_sec2'),
-                               fax, delta=1E-8)
-        self.assertAlmostEqual(fdm.get_property_value('accelerations/a-pilot-y-ft_sec2'),
-                               fay, delta=1E-8)
-        self.assertAlmostEqual(fdm.get_property_value('accelerations/a-pilot-z-ft_sec2'),
-                               faz, delta=1E-8)
+        self.assertAlmostEqual(fdm['accelerations/a-pilot-x-ft_sec2'], fax,
+                               delta=1E-8)
+        self.assertAlmostEqual(fdm['accelerations/a-pilot-y-ft_sec2'], fay,
+                               delta=1E-8)
+        self.assertAlmostEqual(fdm['accelerations/a-pilot-z-ft_sec2'], faz,
+                               delta=1E-8)
 
         # Acceleration along X should be zero
         self.assertAlmostEqual(fax, 0.0, delta=1E-8)