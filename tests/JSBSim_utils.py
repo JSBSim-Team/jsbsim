9be63c56ec09369987d9a75fdeebda1ae3c7c2ea
bcoconni <bcoconni>
2016-05-01 16:35:43 +0000

    The CVS module is no longer needed.

--- tests/JSBSim_utils.py
+++ tests/JSBSim_utils.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License along with
 # this program; if not, see <http://www.gnu.org/licenses/>
 
-import os, sys, csv, string, tempfile, shutil, unittest
+import os, sys, string, tempfile, shutil, unittest
 import xml.etree.ElementTree as et
 import numpy as np
 import pandas as pd
