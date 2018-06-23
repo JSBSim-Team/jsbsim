JSBSim-Team/jsbsim

commit a4b78ea12c496835de324910ed58c43b504d4bef

Cleaned up the C++ of FGStandardAtmosphere
* The table StdAtmostemperatureTable does not need to be a pointer. This saves quite a number of dereferencing '*' and of pairs of parenthesis.
* The length of LapseRateVector does not need to be precalculated: it saves an extra slot (its minimum length is actually 1 less than StdAtmostemperatureTable length).
* The constant Reng is now the ratio of Rstar and Mair which corresponds to its definition.
* Replaced the ratios 'Rstar/Mair' by Reng wherever possible.
* Removed FGStandardAtmosphere::GetStdPressure100k which is no longer used.
* Rewrote the iterations to find the current base altitudes. Replaced the while loops with multiple conditions by for loops with a conditional break. It is now easier to understand (at least to me !).

src/models/atmosphere/FGStandardAtmosphere.cpp
@@ -61,13 +61,11 @@ CLASS IMPLEMENTATION
 // r0 = 6356766 m
 const double FGStandardAtmosphere::EarthRadius = 6356766.0/FGJSBBase::fttom;
 
-FGStandardAtmosphere::FGStandardAtmosphere(FGFDMExec* fdmex) : FGAtmosphere(fdmex),
-                                                               TemperatureBias(0.0),
-                                                               TemperatureDeltaGradient(0.0)
+FGStandardAtmosphere::FGStandardAtmosphere(FGFDMExec* fdmex)
+  : FGAtmosphere(fdmex), TemperatureBias(0.0), TemperatureDeltaGradient(0.0),
+    StdAtmosTemperatureTable(9)
 {
   Name = "FGStandardAtmosphere";
-
-  StdAtmosTemperatureTable = new FGTable(9);
 
   // This is the U.S. Standard Atmosphere table for temperature in degrees
   // Rankine, based on geometric altitude. The table values are often given
@@ -76,37 +74,35 @@ FGStandardAtmosphere::FGStandardAtmosphere(FGFDMExec* fdmex) : FGAtmosphere(fdme
   //                        GeoMet Alt    Temp      GeoPot Alt  GeoMet Alt
   //                           (ft)      (deg R)      (km)        (km)
   //                         --------   --------    ----------  ----------
-  //*StdAtmosTemperatureTable <<      0.00 << 518.67  //    0.000       0.000
-  //                          <<  36151.80 << 389.97  //   11.000      11.019
-  //                          <<  65823.90 << 389.97  //   20.000      20.063
-  //                          << 105518.06 << 411.60  //   32.000      32.162
-  //                          << 155348.07 << 487.20  //   47.000      47.350
-  //                          << 168676.12 << 487.20  //   51.000      51.413
-  //                          << 235570.77 << 386.40  //   71.000      71.802
-  //                          << 282152.08 << 336.50  //   84.852      86.000
-  //                          << 298556.40 << 336.50; //               91.000 - First layer in high altitude regime 
+  //StdAtmosTemperatureTable <<      0.00 << 518.67  //    0.000       0.000
+  //                         <<  36151.80 << 389.97  //   11.000      11.019
+  //                         <<  65823.90 << 389.97  //   20.000      20.063
+  //                         << 105518.06 << 411.60  //   32.000      32.162
+  //                         << 155348.07 << 487.20  //   47.000      47.350
+  //                         << 168676.12 << 487.20  //   51.000      51.413
+  //                         << 235570.77 << 386.40  //   71.000      71.802
+  //                         << 282152.08 << 336.50  //   84.852      86.000
+  //                         << 298556.40 << 336.50; //               91.000 - First layer in high altitude regime 
 
   //                            GeoPot Alt    Temp       GeoPot Alt  GeoMet Alt
   //                               (ft)      (deg R)        (km)        (km)
   //                           -----------   --------     ----------  ----------
-  *StdAtmosTemperatureTable <<      0.0000 << 518.67  //    0.000       0.000
-                            <<  36089.2388 << 389.97  //   11.000      11.019
-                            <<  65616.7979 << 389.97  //   20.000      20.063
-                            << 104986.8766 << 411.57  //   32.000      32.162
-                            << 154199.4751 << 487.17  //   47.000      47.350
-                            << 167322.8346 << 487.17  //   51.000      51.413
-                            << 232939.6325 << 386.37  //   71.000      71.802
-                            << 278385.8268 << 336.5028  //   84.852      86.000
-                            << 298556.40   << 336.5028; //               91.000 - First layer in high altitude regime 
+  StdAtmosTemperatureTable <<      0.0000 << 518.67  //    0.000       0.000
+                           <<  36089.2388 << 389.97  //   11.000      11.019
+                           <<  65616.7979 << 389.97  //   20.000      20.063
+                           << 104986.8766 << 411.57  //   32.000      32.162
+                           << 154199.4751 << 487.17  //   47.000      47.350
+                           << 167322.8346 << 487.17  //   51.000      51.413
+                           << 232939.6325 << 386.37  //   71.000      71.802
+                           << 278385.8268 << 336.5028  //   84.852      86.000
+                           << 298556.40   << 336.5028; //               91.000 - First layer in high altitude regime 
 
-  LapseRateVector.resize(StdAtmosTemperatureTable->GetNumRows()-1);
-  PressureBreakpointVector.resize(StdAtmosTemperatureTable->GetNumRows());
-  StdPressureBreakpointVector.resize(StdAtmosTemperatureTable->GetNumRows());
+  PressureBreakpointVector.resize(StdAtmosTemperatureTable.GetNumRows());
 
   // Assume the altitude to fade out the gradient at is at the highest
   // altitude in the table. Above that, other functions are used to
   // calculate temperature.
-  GradientFadeoutAltitude = (*StdAtmosTemperatureTable)(StdAtmosTemperatureTable->GetNumRows(),0);
+  GradientFadeoutAltitude = StdAtmosTemperatureTable(StdAtmosTemperatureTable.GetNumRows(),0);
 
   bind();
   Debug(0);
@@ -116,8 +112,6 @@ FGStandardAtmosphere::FGStandardAtmosphere(FGFDMExec* fdmex) : FGAtmosphere(fdme
 
 FGStandardAtmosphere::~FGStandardAtmosphere()
 {
-  delete StdAtmosTemperatureTable;
-  LapseRateVector.clear();
   Debug(1);
 }
 
@@ -131,18 +125,18 @@ bool FGStandardAtmosphere::InitModel(void)
   CalculateLapseRates();
   CalculatePressureBreakpoints();
 
-  StdSLtemperature = SLtemperature = (*StdAtmosTemperatureTable)(1, 1);
+  StdSLtemperature = SLtemperature = StdAtmosTemperatureTable(1, 1);
   StdSLdensity     = SLdensity = StdSLpressure / (Reng * StdSLtemperature);
 
   // Density and pressure altitude parameters
   // Density altitude formula only valid up until top of the Troposhere
-  TroposphereMaxAltitude = GeometricAltitude((*StdAtmosTemperatureTable)(2, 0));
+  TroposphereMaxAltitude = GeometricAltitude(StdAtmosTemperatureTable(2, 0));
   // Standard sea level temp / Troposphere lapse rate
   TroposphereAltitudeScaleFactor = -StdSLtemperature / LapseRateVector[0];
   // Troposphere density altitude exponent = LR*/(g0M - LR*)
-  DATroposphereExponent = (-LapseRateVector[0] * Rstar) / (g0 * Mair - -LapseRateVector[0] * Rstar);
+  DATroposphereExponent = (-LapseRateVector[0] * Rstar) / (g0 * Mair + LapseRateVector[0] * Rstar);
   // Troposphere pressure altitude exponent  LR* / g0M 
-  PATroposphereExponent = (-LapseRateVector[0] * Rstar) / (g0 * Mair);
+  PATroposphereExponent = (-LapseRateVector[0] * Reng) / g0;
 
   Calculate(0.0);
   StdSLsoundspeed  = SLsoundspeed = Soundspeed;
@@ -160,43 +154,39 @@ bool FGStandardAtmosphere::InitModel(void)
 
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 // Get the actual pressure as modeled at a specified altitude
-// These calculations are from equations 33a and 33b in the U.S. Standard Atmosphere
-// document referenced in the documentation for this code.
+// These calculations are from equations 33a and 33b in the U.S. Standard
+// Atmosphere document referenced in the documentation for this code.
 
 double FGStandardAtmosphere::GetPressure(double altitude) const
 {
-  unsigned int b=0;
-  double pressure = 0.0;
-  double Lmb, Exp, Tmb, deltaH, factor;
-  double numRows = StdAtmosTemperatureTable->GetNumRows();
+  double GeoPotAlt = GeopotentialAltitude(altitude);
 
   // Iterate through the altitudes to find the current Base Altitude
   // in the table. That is, if the current altitude (the argument passed in)
   // is 20000 ft, then the base altitude from the table is 0.0. If the
   // passed-in altitude is 40000 ft, the base altitude is 36089.2388 ft (and
   // the index "b" is 2 - the second entry in the table).
-  double testAlt = (*StdAtmosTemperatureTable)(b+1,0);
-  double GeoPotAlt = GeopotentialAltitude(altitude);
-  while ((GeoPotAlt >= testAlt) && (b <= numRows-2)) {
-    b++;
-    testAlt = (*StdAtmosTemperatureTable)(b+1,0);
-  }
-  if (b>0) b--;
+  double BaseAlt = StdAtmosTemperatureTable(1,0);
+  unsigned int numRows = StdAtmosTemperatureTable.GetNumRows();
+  unsigned int b;
 
-  double BaseAlt = (*StdAtmosTemperatureTable)(b+1,0);
-  Tmb = GetTemperature(GeometricAltitude(BaseAlt));
-  deltaH = GeoPotAlt - BaseAlt;
-
-  if (LapseRateVector[b] != 0.00) {
-    Lmb = LapseRateVector[b];
-    Exp = (g0*Mair)/(Rstar*Lmb);
-    factor = Tmb/(Tmb + Lmb*deltaH);
-    pressure = PressureBreakpointVector[b]*pow(factor, Exp);
-  } else {
-    pressure = PressureBreakpointVector[b]*exp(-g0*Mair*deltaH/(Rstar*Tmb));
+  for (b=0; b < numRows-2; ++b) {
+    double testAlt = StdAtmosTemperatureTable(b+2,0);
+    if (GeoPotAlt < testAlt)
+      break;
+    BaseAlt = testAlt;
   }
 
-  return pressure;
+  double Tmb = GetTemperature(GeometricAltitude(BaseAlt));
+  double deltaH = GeoPotAlt - BaseAlt;
+  double Lmb = LapseRateVector[b];
+
+  if (Lmb != 0.0) {
+    double Exp = g0 / (Reng*Lmb);
+    double factor = Tmb/(Tmb + Lmb*deltaH);
+    return PressureBreakpointVector[b]*pow(factor, Exp);
+  } else
+    return PressureBreakpointVector[b]*exp(-g0*deltaH/(Reng*Tmb));
 }
 
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -220,9 +210,9 @@ double FGStandardAtmosphere::GetTemperature(double altitude) const
   double T;
 
   if (GeoPotAlt >= 0.0)
-    T = StdAtmosTemperatureTable->GetValue(GeoPotAlt);
+    T = StdAtmosTemperatureTable.GetValue(GeoPotAlt);
   else
-    T = StdAtmosTemperatureTable->GetValue(0.0) + GeoPotAlt*LapseRateVector[0];
+    T = StdAtmosTemperatureTable.GetValue(0.0) + GeoPotAlt*LapseRateVector[0];
 
   T += TemperatureBias;
 
@@ -246,9 +236,9 @@ double FGStandardAtmosphere::GetStdTemperature(double altitude) const
     double GeoPotAlt = GeopotentialAltitude(altitude);
 
     if (GeoPotAlt >= 0.0)
-      temp = StdAtmosTemperatureTable->GetValue(GeoPotAlt);
+      temp = StdAtmosTemperatureTable.GetValue(GeoPotAlt);
     else
-      temp = StdAtmosTemperatureTable->GetValue(0.0) + GeoPotAlt*LapseRateVector[0];
+      temp = StdAtmosTemperatureTable.GetValue(0.0) + GeoPotAlt*LapseRateVector[0];
 
   } else if (altitude < 360892.4) {        // 110 km - station 9
 
@@ -273,65 +263,34 @@ double FGStandardAtmosphere::GetStdTemperature(double altitude) const
 
 double FGStandardAtmosphere::GetStdPressure(double altitude) const
 {
-  unsigned int b=0;
-  double pressure = 0.0;
-  double Lmb, Exp, Tmb, deltaH, factor;
-  double numRows = StdAtmosTemperatureTable->GetNumRows();
+  double GeoPotAlt = GeopotentialAltitude(altitude);
 
   // Iterate through the altitudes to find the current Base Altitude
   // in the table. That is, if the current altitude (the argument passed in)
   // is 20000 ft, then the base altitude from the table is 0.0. If the
   // passed-in altitude is 40000 ft, the base altitude is 36089.2388 ft (and
   // the index "b" is 2 - the second entry in the table).
-  double testAlt = (*StdAtmosTemperatureTable)(b+1,0);
-  double GeoPotAlt = GeopotentialAltitude(altitude);
-  while ((GeoPotAlt >= testAlt) && (b <= numRows-2)) {
-    b++;
-    testAlt = (*StdAtmosTemperatureTable)(b+1,0);
-  }
-  if (b>0) b--;
+  double BaseAlt = StdAtmosTemperatureTable(1,0);
+  unsigned int numRows = StdAtmosTemperatureTable.GetNumRows();
+  unsigned int b;
 
-  double BaseAlt = (*StdAtmosTemperatureTable)(b+1,0);
-  Tmb = GetStdTemperature(BaseAlt);
-  deltaH = GeoPotAlt - BaseAlt;
-
-  if (LapseRateVector[b] != 0.00) {
-    Lmb = LapseRateVector[b];
-    Exp = (g0*Mair) / (Rstar*Lmb);
-    factor = Tmb/(Tmb + Lmb*deltaH);
-    pressure = StdPressureBreakpointVector[b]*pow(factor, Exp);
-  } else {
-    pressure = StdPressureBreakpointVector[b]*exp(-g0*Mair*deltaH/(Rstar*Tmb));
+  for (b=0; b < numRows-2; ++b) {
+    double testAlt = StdAtmosTemperatureTable(b+2,0);
+    if (GeoPotAlt < testAlt)
+      break;
+    BaseAlt = testAlt;
   }
 
-  return pressure;
-}
+  double Tmb = GetStdTemperature(GeometricAltitude(BaseAlt));
+  double deltaH = GeoPotAlt - BaseAlt;
+  double Lmb = LapseRateVector[b];
 
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-// This function calculates an approximation of the standard atmospheric pressure
-// up to an altitude of about 100,000 ft. If the temperature and pressure are not
-// altered for local conditions, the GetPressure(h) function should be used,
-// as that is valid to a much higher altitude. This function is accurate to within
-// a couple of psf up to 100K ft. This polynomial fit was determined using Excel.
-
-double FGStandardAtmosphere::GetStdPressure100K(double altitude) const
-{
-  // Limit this equation to input altitudes of 100000 ft.
-  if (altitude > 100000.0) altitude = 100000.0;
-
-  double alt[5];
-  const double coef[5] = {  2116.217,
-                          -7.648932746E-2,
-                           1.0925498604E-6,
-                          -7.1135726027E-12,
-                           1.7470331356E-17 };
-
-  alt[0] = 1;
-  for (int pwr=1; pwr<=4; pwr++) alt[pwr] = alt[pwr-1]*altitude;
-
-  double press = 0.0;
-  for (int ctr=0; ctr<=4; ctr++) press += coef[ctr]*alt[ctr];
-  return press;
+  if (Lmb != 0.0) {
+    double Exp = g0 / (Reng*Lmb);
+    double factor = Tmb/(Tmb + Lmb*deltaH);
+    return StdPressureBreakpointVector[b]*pow(factor, Exp);
+  } else
+    return StdPressureBreakpointVector[b]*exp(-g0*deltaH/(Reng*Tmb));
 }
 
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -348,7 +307,6 @@ void FGStandardAtmosphere::SetTemperature(double t, double h, eTemperature unit)
 {
   double targetSLtemp = ConvertToRankine(t, unit);
 
-  TemperatureBias = 0.0;
   TemperatureBias = targetSLtemp - GetTemperature(h);
   CalculatePressureBreakpoints();
 }
@@ -428,13 +386,16 @@ void FGStandardAtmosphere::SetTemperatureGradedDelta(double deltemp, double h, e
 
 void FGStandardAtmosphere::CalculateLapseRates()
 {
-  for (unsigned int bh=0; bh<LapseRateVector.size(); bh++)
+  unsigned int numRows = StdAtmosTemperatureTable.GetNumRows();
+  LapseRateVector.clear();
+
+  for (unsigned int bh=0; bh < numRows-1; bh++)
   {
-    double t0 = (*StdAtmosTemperatureTable)(bh+1,1);
-    double t1 = (*StdAtmosTemperatureTable)(bh+2,1);
-    double h0 = (*StdAtmosTemperatureTable)(bh+1,0);
-    double h1 = (*StdAtmosTemperatureTable)(bh+2,0);
-    LapseRateVector[bh] = (t1 - t0) / (h1 - h0) + TemperatureDeltaGradient;
+    double t0 = StdAtmosTemperatureTable(bh+1,1);
+    double t1 = StdAtmosTemperatureTable(bh+2,1);
+    double h0 = StdAtmosTemperatureTable(bh+1,0);
+    double h1 = StdAtmosTemperatureTable(bh+2,0);
+    LapseRateVector.push_back((t1 - t0) / (h1 - h0) + TemperatureDeltaGradient);
   }
 }
 
@@ -443,20 +404,20 @@ void FGStandardAtmosphere::CalculateLapseRates()
 void FGStandardAtmosphere::CalculatePressureBreakpoints()
 {
   for (unsigned int b=0; b<PressureBreakpointVector.size()-1; b++) {
-    double BaseTemp = (*StdAtmosTemperatureTable)(b+1,1);
-    double BaseAlt = (*StdAtmosTemperatureTable)(b+1,0);
-    double UpperAlt = (*StdAtmosTemperatureTable)(b+2,0);
+    double BaseTemp = StdAtmosTemperatureTable(b+1,1);
+    double BaseAlt = StdAtmosTemperatureTable(b+1,0);
+    double UpperAlt = StdAtmosTemperatureTable(b+2,0);
     double deltaH = UpperAlt - BaseAlt;
     double Tmb = BaseTemp
                  + TemperatureBias 
                  + (GradientFadeoutAltitude - BaseAlt)*TemperatureDeltaGradient;
     if (LapseRateVector[b] != 0.00) {
       double Lmb = LapseRateVector[b];
-      double Exp = (g0*Mair) / (Rstar*Lmb);
+      double Exp = g0 / (Reng*Lmb);
       double factor = Tmb/(Tmb + Lmb*deltaH);
       PressureBreakpointVector[b+1] = PressureBreakpointVector[b]*pow(factor, Exp);
     } else {
-      PressureBreakpointVector[b+1] = PressureBreakpointVector[b]*exp(-g0*Mair*deltaH/(Rstar*Tmb));
+      PressureBreakpointVector[b+1] = PressureBreakpointVector[b]*exp(-g0*deltaH/(Reng*Tmb));
     }
   }
 }

src/models/FGAtmosphere.cpp
@@ -63,7 +63,7 @@ CLASS IMPLEMENTATION
 const double FGAtmosphere::Rstar = 8.31432 * (2.20462 / (1.8 * 0.3048 * 0.3048 * 32.174049)); // ft*lbf/R/mol    
 const double FGAtmosphere::Mair = 28.9645 * 2.20462 / (32.174049 * 1000.0); // slug/mol
 const double FGAtmosphere::g0 = 9.80665 / 0.3048; // ft/s^2
-double FGAtmosphere::Reng = ((8.31432 * 1000) / 28.9645) / 1.8 / (0.3048 * 0.3048); // ft*lbf/slug/R
+double FGAtmosphere::Reng = Rstar / Mair;
 
 const double FGAtmosphere::SHRatio = 1.40;
 

src/models/atmosphere/FGStandardAtmosphere.h
@@ -217,9 +217,6 @@ public:
   /// Returns the pressure at a specified altitude in psf.
   virtual double GetPressure(double altitude) const;
 
-  /// Returns the standard pressure at a specified altitude in psf
-  virtual double GetStdPressure100K(double altitude) const;
-
   /// Returns the standard pressure at the specified altitude.
   virtual double GetStdPressure(double altitude) const;
 
@@ -253,7 +250,7 @@ protected:
   double TemperatureDeltaGradient;
   double GradientFadeoutAltitude;
 
-  FGTable* StdAtmosTemperatureTable;
+  FGTable StdAtmosTemperatureTable;
   std::vector<double> LapseRateVector;
   std::vector<double> PressureBreakpointVector;
   std::vector<double> StdPressureBreakpointVector;

commit 4b549670dff95ac03bd2cb191a990aee533897d5

Fixed an undesirable side effect in SetTemperature()
This bug was introduced by commit a4b78ea.

Before the commit above, FGStandardAtmosphere::SetTemperature() was setting TemperatureBias to 0.0 then was setting it to its final value. So at the first glance, it looked like the member TemperatureBias was set twice in a row. Hence the removal of the first occurence of `TemperatureBias=0.0` in the commit mentioned above.

Unfortunately, while updating the copy of JSBSim in FlightGear it appeared that this change broke SetTemperature(). Actually SetTemperature() was calling GetTemperature() to compute TemperatureBias but GetTemperature() was depending itself on TemperatureBias. Hence the need to reset TemperatureBias to 0.0 to break this circular dependency before calling GetTemperature().

The bug can be triggered by calling SetTemperature() twice in a row: after the 2nd call, the atmosphere temperature is incorrectly set.

This commit replaces the usage of GetTemperature() (and its vicious dependence on TemperatureBias) by an explicit computation which is based on the ISA temperature from GetStdTemperature() and corrected by the gradient TemperatureDeltaGradient.

Both the bug is squashed and the side effect removed.

python/jsbsim.pxd
@@ -22,30 +22,16 @@ from libcpp.vector cimport vector
 cdef extern from "ExceptionManagement.h":
     cdef void convertJSBSimToPyExc()
 
-cdef extern from "models/FGPropulsion.h" namespace "JSBSim":
-    cdef cppclass c_FGPropulsion "JSBSim::FGPropulsion":
-        c_FGPropulsion(c_FGFDMExec* fdm)
-        void InitRunning(int n)
-        int GetNumEngines()
-
-cdef extern from "simgear/misc/sg_path.hxx":
-    cdef cppclass c_SGPath "SGPath":
-        c_SGPath(const string& path, int* validator)
-        c_SGPath(const c_SGPath& p)
-        void set(const string& p)
-        string utf8Str()
-
 cdef extern from "initialization/FGInitialCondition.h" namespace "JSBSim":
     cdef cppclass c_FGInitialCondition "JSBSim::FGInitialCondition":
         c_FGInitialCondition(c_FGFDMExec* fdm)
         c_FGInitialCondition(c_FGInitialCondition* ic)
         bool Load(const c_SGPath& rstfile, bool useStoredPath)
 
-cdef extern from "math/FGMatrix33.h" namespace "JSBSim":
-    cdef cppclass c_FGMatrix33 "JSBSim::FGMatrix33":
-        c_FGMatrix33()
-        c_FGMatrix33(const c_FGMatrix33& m)
-        double Entry(unsigned int row, unsigned int col) const
+cdef extern from "input_output/FGPropertyManager.h" namespace "JSBSim":
+    cdef cppclass c_FGPropertyManager "JSBSim::FGPropertyManager":
+        c_FGPropertyManager()
+        bool HasNode(string path)
 
 cdef extern from "math/FGColumnVector3.h" namespace "JSBSim":
     cdef cppclass c_FGColumnVector3 "JSBSim::FGColumnVector3":
@@ -53,33 +39,11 @@ cdef extern from "math/FGColumnVector3.h" namespace "JSBSim":
         c_FGColumnVector3(const c_FGColumnVector3& m)
         double Entry(unsigned int idx) const
 
-cdef extern from "models/FGPropagate.h" namespace "JSBSim":
-    cdef cppclass c_FGPropagate "JSBSim::FGPropagate":
-        c_FGPropagate(c_FGFDMExec* fdm)
-        c_FGMatrix33& GetTl2b()
-        c_FGMatrix33& GetTec2b()
-        c_FGColumnVector3& GetUVW()
-
-cdef extern from "input_output/FGPropertyManager.h" namespace "JSBSim":
-    cdef cppclass c_FGPropertyManager "JSBSim::FGPropertyManager":
-        c_FGPropertyManager()
-        bool HasNode(string path)
-
-cdef extern from "models/FGGroundReactions.h" namespace "JSBSim":
-    cdef cppclass c_FGGroundReactions "JSBSim::FGGroundReactions":
-        c_FGGroundReactions(c_FGFDMExec* fdm)
-        c_FGLGear* GetGearUnit(int gear)
-        int GetNumGearUnits()
-
-cdef extern from "models/FGLGear.h" namespace "JSBSim":
-    cdef cppclass c_FGLGear "JSBSim::FGLGear":
-        double GetSteerNorm()
-
-cdef extern from "models/FGAuxiliary.h" namespace "JSBSim":
-    cdef cppclass c_FGAuxiliary "JSBSim::FGAuxiliary":
-        c_FGAuxiliary(c_FGFDMExec* fdm)
-        c_FGMatrix33& GetTw2b()
-        c_FGMatrix33& GetTb2w()
+cdef extern from "math/FGMatrix33.h" namespace "JSBSim":
+    cdef cppclass c_FGMatrix33 "JSBSim::FGMatrix33":
+        c_FGMatrix33()
+        c_FGMatrix33(const c_FGMatrix33& m)
+        double Entry(unsigned int row, unsigned int col) const
 
 cdef extern from "models/FGAerodynamics.h" namespace "JSBSim":
     cdef cppclass c_FGAerodynamics "JSBSim::FGAerodynamics":
@@ -92,10 +56,58 @@ cdef extern from "models/FGAircraft.h" namespace "JSBSim":
         c_FGAircraft(c_FGFDMExec* fdm)
         c_FGColumnVector3& GetXYZrp()
 
+cdef extern from "models/FGAtmosphere.h" namespace "JSBSim":
+    cdef enum eTemperature "JSBSim::FGAtmosphere::eTemperature":
+        eNoTempUnit = 0,
+        eFahrenheit = 1,
+        eCelsius    = 2,
+        eRankine    = 3,
+        eKelvin     = 4
+
+    cdef cppclass c_FGAtmosphere "JSBSim::FGAtmosphere":
+        c_FGAtmosphere(c_FGFDMExec* fdm)
+        void SetTemperature(double t, double h, eTemperature unit)
+
+cdef extern from "models/FGAuxiliary.h" namespace "JSBSim":
+    cdef cppclass c_FGAuxiliary "JSBSim::FGAuxiliary":
+        c_FGAuxiliary(c_FGFDMExec* fdm)
+        c_FGMatrix33& GetTw2b()
+        c_FGMatrix33& GetTb2w()
+
+cdef extern from "models/FGGroundReactions.h" namespace "JSBSim":
+    cdef cppclass c_FGGroundReactions "JSBSim::FGGroundReactions":
+        c_FGGroundReactions(c_FGFDMExec* fdm)
+        c_FGLGear* GetGearUnit(int gear)
+        int GetNumGearUnits()
+
+cdef extern from "models/FGLGear.h" namespace "JSBSim":
+    cdef cppclass c_FGLGear "JSBSim::FGLGear":
+        double GetSteerNorm()
+
 cdef extern from "models/FGMassBalance.h" namespace "JSBSim":
     cdef cppclass c_FGMassBalance "JSBSim::FGMassBalance":
         c_FGMassBalance(c_FGFDMExec* fdm)
         c_FGColumnVector3& GetXYZcg()
+
+cdef extern from "models/FGPropagate.h" namespace "JSBSim":
+    cdef cppclass c_FGPropagate "JSBSim::FGPropagate":
+        c_FGPropagate(c_FGFDMExec* fdm)
+        c_FGMatrix33& GetTl2b()
+        c_FGMatrix33& GetTec2b()
+        c_FGColumnVector3& GetUVW()
+
+cdef extern from "models/FGPropulsion.h" namespace "JSBSim":
+    cdef cppclass c_FGPropulsion "JSBSim::FGPropulsion":
+        c_FGPropulsion(c_FGFDMExec* fdm)
+        void InitRunning(int n)
+        int GetNumEngines()
+
+cdef extern from "simgear/misc/sg_path.hxx":
+    cdef cppclass c_SGPath "SGPath":
+        c_SGPath(const string& path, int* validator)
+        c_SGPath(const c_SGPath& p)
+        void set(const string& p)
+        string utf8Str()
 
 cdef extern from "FGFDMExec.h" namespace "JSBSim":
     cdef cppclass c_FGFDMExec "JSBSim::FGFDMExec":
@@ -161,4 +173,5 @@ cdef extern from "FGFDMExec.h" namespace "JSBSim":
         c_FGAuxiliary* GetAuxiliary()
         c_FGAerodynamics* GetAerodynamics()
         c_FGAircraft* GetAircraft()
+        c_FGAtmosphere* GetAtmosphere()
         c_FGMassBalance* GetMassBalance()

python/jsbsim.pyx
@@ -113,6 +113,16 @@ cdef class FGAircraft:
     def get_xyz_rp(self):
         return convertToNumpyVec(self.thisptr.GetXYZrp())
 
+cdef class FGAtmosphere:
+
+    cdef c_FGAtmosphere *thisptr
+
+    def __init__(self):
+        self.thisptr = NULL
+
+    def set_temperature(self, t, h, unit):
+        return self.thisptr.SetTemperature(t, h, unit)
+
 cdef class FGMassBalance:
 
     cdef c_FGMassBalance *thisptr
@@ -647,3 +657,8 @@ cdef class FGFDMExec:
         massbalance = FGMassBalance()
         massbalance.thisptr = self.thisptr.GetMassBalance()
         return massbalance
+
+    def get_atmosphere(self):
+        atmosphere = FGAtmosphere()
+        atmosphere.thisptr = self.thisptr.GetAtmosphere()
+        return atmosphere

src/models/atmosphere/FGStandardAtmosphere.cpp
@@ -307,9 +307,14 @@ double FGStandardAtmosphere::GetStdDensity(double altitude) const
 
 void FGStandardAtmosphere::SetTemperature(double t, double h, eTemperature unit)
 {
-  double targetSLtemp = ConvertToRankine(t, unit);
+  double targetTemp = ConvertToRankine(t, unit);
+  double GeoPotAlt = GeopotentialAltitude(h);
 
-  TemperatureBias = targetSLtemp - GetTemperature(h);
+  TemperatureBias = targetTemp - GetStdTemperature(h);
+
+  if (GeoPotAlt <= GradientFadeoutAltitude)
+    TemperatureBias -= TemperatureDeltaGradient * (GradientFadeoutAltitude - GeoPotAlt);
+
   CalculatePressureBreakpoints();
 }
 

tests/TestStdAtmosphere.py
@@ -89,7 +89,7 @@ class TestStdAtmosphere(JSBSimTestCase):
             h = alt
 
         # Check that the temperature gradient has no influence beyond a
-        # geometric altitude of 91 km
+        # geopotential altitude of 91 km
         dH = self.gradient_fade_out_h - h
         T_K -= T_gradient*dH
         fdm['ic/h-sl-ft'] = self.geometric_altitude(self.gradient_fade_out_h) * self.km_to_ft
@@ -262,6 +262,36 @@ class TestStdAtmosphere(JSBSimTestCase):
 
         self.check_temperature(fdm, T_sl + graded_delta_T_K, T_gradient)
         self.check_pressure(fdm, P_sl, T_sl + graded_delta_T_K, T_gradient)
+
+        del fdm
+
+    def test_set_temperature(self):
+        fdm = CreateFDM(self.sandbox)
+        fdm.load_model('ball')
+        fdm.run_ic()
+
+        atmos = fdm.get_atmosphere()
+        eRankine = 3
+
+        # Check that there are no side effects if we call SetTemperature()
+        # twice in a row.
+        atmos.set_temperature(520, 0.0, eRankine)
+        fdm.run_ic()
+        self.assertAlmostEqual(1.0, fdm['atmosphere/T-R']/520.0)
+
+        atmos.set_temperature(500, 0.0, eRankine)
+        fdm.run_ic()
+        self.assertAlmostEqual(1.0, fdm['atmosphere/T-R']/500.0)
+
+        # Check that it works while a temperature gradient is set
+        graded_delta_T_K = -10.0
+        fdm['atmosphere/SL-graded-delta-T'] = graded_delta_T_K*self.K_to_R
+
+        atmos.set_temperature(530, 1000.0, eRankine)
+        fdm['ic/h-sl-ft'] = 1000.
+        fdm.run_ic()
+
+        self.assertAlmostEqual(1.0, fdm['atmosphere/T-R']/530.0)
 
         del fdm
 