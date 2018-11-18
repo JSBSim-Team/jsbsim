baaab10bb6db17fa87b05a0ab568a36feae63d52
Bertrand Coconnier <bcoconni@users.sourceforge.net>
2018-11-18 16:46:28 +0100

    Added the tests of <sin> and <rotation_[alpha|beta]_local>

--- tests/TestFunctions.py
+++ tests/TestFunctions.py
@@ -33,6 +33,8 @@ class TestFunctions(JSBSimTestCase):
         self.assertAlmostEqual(fdm['test/sum-value-property'], 1.0)
         self.assertAlmostEqual(fdm['test/product-values'], 2.0*math.pi)
         self.assertAlmostEqual(fdm['test/product-value-property'], 0.0)
+        self.assertAlmostEqual(fdm['test/sin-value'], 0.5*math.sqrt(2.0))
+        self.assertAlmostEqual(fdm['test/sin-property'], 0.0)
 
         fdm['test/reference'] = 1.5
         fdm.run()
@@ -41,42 +43,123 @@ class TestFunctions(JSBSimTestCase):
         self.assertAlmostEqual(fdm['test/sum-value-property'], 2.5)
         self.assertAlmostEqual(fdm['test/product-values'], 2.0*math.pi)
         self.assertAlmostEqual(fdm['test/product-value-property'], 2.25)
+        self.assertAlmostEqual(fdm['test/sin-value'], 0.5*math.sqrt(2.0))
+        self.assertAlmostEqual(fdm['test/sin-property'], 0.0)
 
         fdm['test/input'] = -1.0
         fdm.run()
         self.assertAlmostEqual(fdm['test/interpolate1d'], -1.0)
+        self.assertAlmostEqual(fdm['test/sin-value'], 0.5*math.sqrt(2.0))
+        self.assertAlmostEqual(fdm['test/sin-property'], math.sin(-1.0))
 
         fdm['test/input'] = 0.0
         fdm.run()
         self.assertAlmostEqual(fdm['test/interpolate1d'], -1.0)
+        self.assertAlmostEqual(fdm['test/sin-value'], 0.5*math.sqrt(2.0))
+        self.assertAlmostEqual(fdm['test/sin-property'], 0.0)
 
         fdm['test/input'] = 0.5
         fdm.run()
         self.assertAlmostEqual(fdm['test/interpolate1d'], -0.5)
+        self.assertAlmostEqual(fdm['test/sin-value'], 0.5*math.sqrt(2.0))
+        self.assertAlmostEqual(fdm['test/sin-property'], math.sin(0.5))
 
         fdm['test/input'] = 1.0
         fdm.run()
         self.assertAlmostEqual(fdm['test/interpolate1d'], 0.0)
+        self.assertAlmostEqual(fdm['test/sin-value'], 0.5*math.sqrt(2.0))
+        self.assertAlmostEqual(fdm['test/sin-property'], math.sin(1.0))
 
         fdm['test/input'] = 1.5
         fdm.run()
         self.assertAlmostEqual(fdm['test/interpolate1d'], 0.0)
+        self.assertAlmostEqual(fdm['test/sin-value'], 0.5*math.sqrt(2.0))
+        self.assertAlmostEqual(fdm['test/sin-property'], math.sin(1.5))
 
         fdm['test/input'] = 3.0
         fdm.run()
         self.assertAlmostEqual(fdm['test/interpolate1d'], 0.0)
+        self.assertAlmostEqual(fdm['test/sin-value'], 0.5*math.sqrt(2.0))
+        self.assertAlmostEqual(fdm['test/sin-property'], math.sin(3.0))
 
         fdm['test/input'] = 3.5
         fdm.run()
         self.assertAlmostEqual(fdm['test/interpolate1d'], 1.0)
+        self.assertAlmostEqual(fdm['test/sin-value'], 0.5*math.sqrt(2.0))
+        self.assertAlmostEqual(fdm['test/sin-property'], math.sin(3.5))
 
         fdm['test/input'] = 4.0
         fdm.run()
         self.assertAlmostEqual(fdm['test/interpolate1d'], 2.0)
+        self.assertAlmostEqual(fdm['test/sin-value'], 0.5*math.sqrt(2.0))
+        self.assertAlmostEqual(fdm['test/sin-property'], math.sin(4.0))
 
         fdm['test/input'] = 5.0
         fdm.run()
         self.assertAlmostEqual(fdm['test/interpolate1d'], 2.0)
+        self.assertAlmostEqual(fdm['test/sin-value'], 0.5*math.sqrt(2.0))
+        self.assertAlmostEqual(fdm['test/sin-property'], math.sin(5.0))
+
+    def test_rotations(self):
+        tripod = FlightModel(self, 'tripod')
+        tripod.include_system_test_file('function.xml')
+        fdm = tripod.start()
+
+        for a in range(36):
+            alpha = (a-17)*10
+            for c in range(36):
+                gamma = (c-17)*10
+                fdm['test/alpha'] = alpha
+                fdm['test/gamma'] = gamma
+                fdm.run()
+                self.assertAlmostEqual(fdm['test/alpha_local'], alpha)
+
+        fdm['test/alpha'] = 0.
+
+        for b in range(17):
+            beta = (b-8)*10
+            for c in range(36):
+                gamma = (c-17)*10
+                fdm['test/beta'] = beta
+                fdm['test/gamma'] = gamma
+                fdm.run()
+                self.assertAlmostEqual(fdm['test/beta_local'], beta)
+
+        fdm['test/alpha'] = 10.
+        fdm['test/beta'] = 0.
+        fdm['test/theta'] = -10.
+        for c in range(36):
+            gamma = (c-17)*10
+            fdm['test/gamma'] = gamma
+            fdm.run()
+            self.assertAlmostEqual(fdm['test/alpha_local'], 0.0)
+            self.assertAlmostEqual(fdm['test/beta_local'], 0.0)
+
+        fdm['test/alpha'] = 0.
+        fdm['test/beta'] = 10.
+        fdm['test/theta'] = 0.
+        fdm['test/psi'] = 10.
+        for c in range(36):
+            gamma = (c-17)*10
+            fdm['test/gamma'] = gamma
+            fdm.run()
+            self.assertAlmostEqual(fdm['test/alpha_local'], 0.0)
+            self.assertAlmostEqual(fdm['test/beta_local'], 0.0)
+
+        alpha = 10.
+        fdm['test/alpha'] = alpha
+        fdm['test/beta'] =  5.
+        fdm['test/phi'] = 0.
+        fdm['test/psi'] = 0.
+        for t in range(18):
+            theta = (t-8)*10
+            fdm['test/theta'] = theta
+            for c in range(36):
+                gamma = (c-17)*10
+                fdm['test/gamma'] = gamma
+                fdm.run()
+                self.assertAlmostEqual(fdm['test/alpha_local'], alpha+theta)
+                self.assertAlmostEqual(fdm['test/beta_local'], 5.0)
 
 
 RunTest(TestFunctions)
