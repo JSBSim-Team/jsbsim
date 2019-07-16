2e21084a697700c8011523bf8835f7f50e807636
Bertrand Coconnier <bcoconni@users.sourceforge.net>
2019-07-16 02:14:27 +0200

    Prepare a special build for test coverage.

--- tests/unit_tests/test_coverage.sh
+++ tests/unit_tests/test_coverage.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -ev
 if [ "${TRAVIS_PULL_REQUEST}" = "false"] && ["${USE_SYSTEM_EXPAT}" = "OFF"]; then
+    make clean
+    make -j2 CFLAGS="--coverage" CXXFLAGS="--coverage"
+    lcov -d . -z
+    ctest -R Test1
     lcov -d . -c -o tmp.info
     lcov -r tmp.info /usr/include/c++/\* /usr/include/cxxtest/\* */test/unit_tests/\* -o coverage.info
     genhtml -o documentation/html/coverage -t "JSBSim unit tests" coverage.info
