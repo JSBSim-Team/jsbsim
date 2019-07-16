51fa8d3dcc38f2817280e83240e0b3e9d0f87ea1
Bertrand Coconnier <bcoconni@users.noreply.github.com>
2019-07-16 21:44:39 +0200

    Do not reset execution counts to zero.

--- tests/unit_tests/test_coverage.sh
+++ tests/unit_tests/test_coverage.sh
@@ -3,7 +3,6 @@ set -ev
 if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$USE_SYSTEM_EXPAT" = "OFF" ]; then
     make clean
     make -j2 CFLAGS="--coverage" CXXFLAGS="--coverage"
-    lcov -d . -z
     ctest -R Test1
     lcov -d . -c -o tmp.info
     lcov -r tmp.info /usr/include/c++/\* /usr/include/cxxtest/\* */test/unit_tests/\* -o coverage.info
