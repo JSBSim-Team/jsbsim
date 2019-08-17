dd1e2d7e8fc2c25bfccb58ffdef97ac98d0d474d
Bertrand Coconnier <bcoconni@users.noreply.github.com>
2019-08-17 18:04:32 +0200

    Fixed a typo and added a general comment

--- tests/unit_tests/test_coverage.sh
+++ tests/unit_tests/test_coverage.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Automatically generates the coverage reports of the C++ unit tests
 set -ev
 if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$USE_SYSTEM_EXPAT" = "OFF" ]; then
     make clean
@@ -6,6 +7,6 @@ if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$USE_SYSTEM_EXPAT" = "OFF" ]; then
     make -j2
     ctest -R Test1
     lcov -d . -c -o tmp.info
-    lcov -r tmp.info /usr/include/c++/\* /usr/include/cxxtest/\* */test/unit_tests/\* -o coverage.info
+    lcov -r tmp.info /usr/include/c++/\* /usr/include/cxxtest/\* \*/test/unit_tests/\* -o coverage.info
     genhtml -o documentation/html/coverage -t "JSBSim unit tests" coverage.info
 fi
