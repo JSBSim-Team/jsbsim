1348afe41c05a55fa877b3982669df546a30202c
Bertrand Coconnier <bcoconni@users.noreply.github.com>
2019-09-01 21:27:52 +0200

    [skip appveyor] Fixed the directory name.

--- doc/build_docs.sh
+++ doc/build_docs.sh
@@ -5,5 +5,5 @@ if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$USE_SYSTEM_EXPAT" = "OFF" ] && [ 
     # Make sure that GitHub does not skip underscored folders
     touch documentation/html/.nojekyll
     # Build the Python API documentation
-    sphinx-build -b html . documentation/html/python
+    sphinx-build -b html documentation documentation/html/python
 fi
