5aa0a86ae876e544d3e8d526c15e5dcc209fadb4
Bertrand Coconnier <bcoconni@users.sourceforge.net>
2019-09-01 19:27:32 +0200

    Small adjustments to the CD process.

--- doc/build_docs.sh
+++ doc/build_docs.sh
@@ -2,8 +2,6 @@
 # Automatically generates the docs (skip if we are building a pull request)
 set -ev
 if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$USE_SYSTEM_EXPAT" = "OFF" ] && [ "$TRAVIS_PYTHON_VERSION" = "3.6" ]; then
-    # Extracts the source code docs with Doxygen and build the C++ API documentation
-    make doc
     # Make sure that GitHub does not skip underscored folders
     touch documentation/html/.nojekyll
     # Build the Python API documentation
