e375a1ed00d0ea08f9e86a32c449eb1094889fe1
Bertrand Coconnier <bcoconni@users.sourceforge.net>
2019-09-07 22:48:02 +0200

    Fixed a couple of things
    - Make the build-wheels.sh script executable
    - Call auditwheel on each wheel package

--- python/build-wheels.sh
+++ python/build-wheels.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 set -e -x
 
+# Compile C++ code
 make clean
 make
 ctest
 
+# Compile wheels
 for PYBIN in /opt/python/*/bin; do
     "${PYBIN}/pip" install -r cython numpy
     "${PYBIN}/python" python/setup.py build_scripts
     "${PYBIN}/python" python/setup.py bdist_wheel
 done
 
+# Bundle external shared libraries into the wheels
+for whl in python/dist/*.whl; do
+    auditwheel repair "$whl" --plat manylinux2010_x86_64 -w /io/python/dist
+done
+
+# Install packages and test
 for PYBIN in /opt/python/*/bin; do
     "${PYBIN}/pip" install jsbsim --no-index -f python/dist
     "${PYBIN}/python" -c "import jsbsim;fdm=jsbsim.FGFDMExec('.', None);print(jsbsim.FGAircraft.__doc__)"
