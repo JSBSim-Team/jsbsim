b6c4894818921627e19766f9eee5a44863832489
Bertrand Coconnier <bcoconni@users.noreply.github.com>
2019-09-08 20:48:11 +0200

    Build & deploy portable Linux Python packages (#216)

---
+++ python/build-wheels.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e -x
+
+# Compile C++ code
+cd /io/build
+cmake ..
+make
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" install cython numpy
+    "${PYBIN}/cython" --cplus python/jsbsim.pyx -o python/jsbsim.cxx
+    "${PYBIN}/python" python/setup.py bdist_wheel
+done
+
+# Bundle external shared libraries into the wheels
+for whl in python/dist/*.whl; do
+    auditwheel repair "$whl" --plat manylinux2010_x86_64 -w python/dist
+done
+
+# Install packages and test
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" install jsbsim --no-index -f python/dist
+    "${PYBIN}/python" -c "import jsbsim;fdm=jsbsim.FGFDMExec('.', None);print(jsbsim.FGAircraft.__doc__)"
+    "${PYBIN}/JSBSim" --root=.. --script=scripts/c1721.xml
+done
