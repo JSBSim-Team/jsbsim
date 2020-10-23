78dc7f1f1565930ad4c15500353b854f5c156521
Bertrand Coconnier <bcoconni@users.sf.net>
2020-10-23 14:41:40 +0200

    Added a regression test for the bug reported in issue #332.

---
+++ tests/TestMagnetometer.py
@@ -0,0 +1,33 @@
+# TestMagnetometer.py
+#
+# Test that the <magnetometer> component is functional.
+#
+# Copyright (c) 2020 Bertrand Coconnier
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, see <http://www.gnu.org/licenses/>
+#
+
+from JSBSim_utils import JSBSimTestCase, RunTest, FlightModel
+
+
+class TestMagnetometer(JSBSimTestCase):
+    # Regression test for the bug reported in issue 332
+    def test(self):
+        tripod = FlightModel(self, "tripod")
+        tripod.include_system_test_file("magnetometer.xml")
+        fdm = tripod.start()
+        self.assertAlmostEqual(fdm['test/magnetic-field']/27661.104, 1.0)
+
+
+RunTest(TestMagnetometer)
