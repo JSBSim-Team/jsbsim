44f180d10832c737cf4ec28ae213f76c50be105c
Bertrand Coconnier <bcoconni@users.sf.net>
2020-12-04 19:02:41 +0100

    Make the build process compatible with the latest Docker container for ManyLinux.

--- python/build-wheels.sh
+++ python/build-wheels.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 set -e -x
+cd /io/build
+
+export PYTHON_MIN_VERSION=3,6
 
 # Compile C++ code
-cd /io/build
-cmake -DCMAKE_C_FLAGS_RELEASE="-g -O2 -DNDEBUG" -DCMAKE_CXX_FLAGS_RELEASE="-g -O2 -DNDEBUG" -DCMAKE_BUILD_TYPE=Release ..
-# Only build libJSBSim because that's all we need for Python wheels.
-cmake --build . --target libJSBSim
+for PYBIN in /opt/python/*/bin; do
+    # Skip deprecated or unsupported versions
+    if "${PYBIN}/python" -c "import sys;sys.stdout.write(str(sys.version_info >= (${PYTHON_MIN_VERSION})))" | grep -q 'True'; then
+        "${PYBIN}/pip" install cmake
+        "${PYBIN}/cmake" -DCMAKE_C_FLAGS_RELEASE="-g -O2 -DNDEBUG" -DCMAKE_CXX_FLAGS_RELEASE="-g -O2 -DNDEBUG" -DCMAKE_BUILD_TYPE=Release ..
+        # Only build libJSBSim because that's all we need for Python wheels.
+        "${PYBIN}/cmake" --build . --target libJSBSim
+        break
+    fi
+done
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
     # Skip deprecated or unsupported versions
-    if "${PYBIN}/python" -c "import sys;sys.stdout.write(str(int(sys.version_info >= (3,5))))" | grep -q '1'; then
+    if "${PYBIN}/python" -c "import sys;sys.stdout.write(str(sys.version_info >= (${PYTHON_MIN_VERSION})))" | grep -q 'True'; then
         "${PYBIN}/pip" install cython numpy
         "${PYBIN}/cython" --cplus python/jsbsim.pyx -o python/jsbsim.cxx
         "${PYBIN}/python" python/setup.py bdist_wheel --build-number=$GITHUB_RUN_NUMBER
@@ -25,7 +34,7 @@ done
 # Install packages and test
 for PYBIN in /opt/python/*/bin; do
     # Skip deprecated or unsupported versions
-    if "${PYBIN}/python" -c "import sys;sys.stdout.write(str(int(sys.version_info >= (3,5))))" | grep -q '1'; then
+    if "${PYBIN}/python" -c "import sys;sys.stdout.write(str(sys.version_info >= (${PYTHON_MIN_VERSION})))" | grep -q 'True'; then
         "${PYBIN}/pip" install jsbsim --no-index -f python/dist
         "${PYBIN}/python" -c "import jsbsim;fdm=jsbsim.FGFDMExec('.', None);print(jsbsim.FGAircraft.__doc__)"
         "${PYBIN}/JSBSim" --root=.. --script=scripts/c1721.xml
