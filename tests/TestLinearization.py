09cfda2b9d4829112660f0ac67214389e62f6d0d
Bertrand Coconnier <bcoconni@users.sf.net>
2021-01-15 15:37:58 +0100

    Trim the aircraft prior to linearization.

--- tests/TestLinearization.py
+++ tests/TestLinearization.py
@@ -13,6 +13,16 @@ class TestLinearization(JSBSimTestCase):
         fdm.load_script(script_path)
         fdm.run_ic()
 
+        # Set engines running
+        fdm['propulsion/engine[0]/set-running'] = 1
+        fdm['propulsion/engine[1]/set-running'] = 1
+        fdm.run()
+
+        # Trim
+        fdm.debug_lvl = 1 # Enable debug messages to log trimmed values
+        fdm['simulation/do_simple_trim'] = 1
+        fdm.debug_lvl = 0 # Disable debug messages
+
         linearization = jsbsim.FGLinearization(fdm)
 
         self.assertEqual(linearization.x0.shape, (12,))
