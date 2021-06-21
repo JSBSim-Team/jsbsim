e47295d4b5e24f4861e4ac9cdecc1f2e212218be
sthelia <52634491+sthelia@users.noreply.github.com>
2021-06-21 10:55:30 +0200

    Update matlab/JSBSimInterface.cpp
    Co-authored-by: Bertrand Coconnier <bcoconni@users.noreply.github.com>

--- matlab/JSBSimInterface.cpp
+++ matlab/JSBSimInterface.cpp
@@ -316,7 +316,7 @@ bool JSBSimInterface::EasySetValue(const string& prop, const double value)
 		//	mexPrintf("\tEasy-set: psi -> quaternion = (%f,%f,%f,%f)\n",
 		//		propagate->GetVState().qAttitudeLocal(1),propagate->GetVState().qAttitudeLocal(2),
 		//		propagate->GetVState().qAttitudeLocal(3),propagate->GetVState().qAttitudeLocal(4));
-		return 1;
+		return true;
 	}
 	else if (prop == "elevator-cmd-norm")
 	{
