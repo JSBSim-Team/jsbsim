03ffef3ab35cfe4f00e9ba9149745ad5b9f1175b
Guilherme A. L. da Silva <gasilva@ats4i.com.br>
2021-07-11 15:44:04 -0300

    Add plots to the AoA example script (#473)

--- examples/python/aoa.py
+++ examples/python/aoa.py
@@ -7,8 +7,14 @@
 # pip install jsbsim
 # or conda install jsbsim (if you have anaconda, this is the best way)
 
+# to install matplotlib module in Python
+# pip install matplotlib
+# or conda install matplotlib (if you have anaconda, this is the best way)
+
 import jsbsim
 import xml.etree.ElementTree as ET
+import matplotlib.pyplot as plt
+import os
 
 # function to change CG in aircraft xml
 # change the directory to the aircraft to be studied
@@ -36,9 +42,12 @@ fuel=[1000,fuelmax/2,fuelmax]
 #three cases for weight
 weight=["light","mid","heavy"]
 
-#get the original CG from aircraft xml
-cgOrig=float(changeCG(0,True))
+#put your path here
+PATH_TO_JSBSIM_FILES="../../"
 
+fdm = jsbsim.FGFDMExec(PATH_TO_JSBSIM_FILES) 
+#get the original CG from aircraft xml
+cgOrig=float(changeCG(fdm,0,True))
 #vary CG in the study
 cgPos=[cgOrig*0.95,cgOrig*1.05]
 
@@ -46,16 +55,23 @@ cgPos=[cgOrig*0.95,cgOrig*1.05]
 h_ft=[8000,30000]
 
 #run the simulation varying CG, altitude, speed and total weight
+
+#run for different CG's 
 for j in range(2):
+    #points to jsbsim executable and make an object
     fdm = jsbsim.FGFDMExec(PATH_TO_JSBSIM_FILES) 
-    cg=changeCG(fdm, cgPos[j],False)
     fdm.load_model('global5000')
     # Set engines running
+    cg=changeCG(fdm,cgPos[j],False)
     fdm['propulsion/engine[0]/set-running'] = 1
     fdm['propulsion/engine[1]/set-running'] = 1
+
+    #run for different weights
     for i in range(3):
         results = []
-        for speed in range(90, 460, 10):
+
+        #run for different speeds
+        for speed in range(90, 550, 10):
             fdm['ic/h-sl-ft'] = h_ft[j]
             fdm['ic/vc-kts'] = speed
             fdm['ic/gamma-deg'] = 0
@@ -64,7 +80,9 @@ for j in range(2):
             fdm['propulsion/tank[2]/contents-lbs'] = fuel[i]
             fdm['inertia/pointmass-weight-lbs[0]'] = payload[i]
 
-            fdm.run_ic() # Initialize the aircraft with initial conditions
+            # Initialize the aircraft with initial conditions
+            fdm.run_ic()
+            # run fdm model
             fdm.run()
 
             # Trim
@@ -84,6 +102,15 @@ for j in range(2):
             print(result[0], result[1])
 
         speed, alpha = zip(*results)
+        ax.plot(speed, alpha,label="{0} weight {1:.0f} kft {2:.2f} % cg".format(weight[i],h_ft[j]/1000,(float(cgPos[j])/float(cgOrig)-1)*100))
+
+#plot final results
+ax.legend(frameon=False)
+ax.set_xlabel('KCAS (kt)')
+ax.set_ylabel('AoA (deg)')
+ax.set_title('AoA vs KCAS')
+
+plt.show()
 
 #restore original CG for the aircraft xml
-cgx=changeCG(str(cgOrig),False)
+cgx=changeCG(fdm," {:.2f} ".format(cgOrig),False)
