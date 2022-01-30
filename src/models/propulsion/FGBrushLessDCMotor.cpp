edf99df473164ab9bccd0fd22e61a0002d51b934
pbecchi <pbecchi@aerobusiness.it>
2022-01-30 14:23:02 +0100

    Update src/models/propulsion/FGBrushLessDCMotor.cpp
    Co-authored-by: Bertrand Coconnier <bcoconni@users.noreply.github.com>

--- src/models/propulsion/FGBrushLessDCMotor.cpp
+++ src/models/propulsion/FGBrushLessDCMotor.cpp
@@ -33,7 +33,6 @@ It require 3 basic physical motor properties:
 Kv speed motor constant      [RPM/Volt]
 Rm internal coil resistance  [Ohms]
 I0 no load current           [Amperes]
-<<<<<<< HEAD
 
 REFERENCE:
 http://web.mit.edu/drela/Public/web/qprop/motor1_theory.pdf
