79974402ab4f1814998b52f3a9899919a9df3431
Bertrand Coconnier <bcoconni@users.sf.net>
2022-02-13 14:18:39 +0100

    Discard support of Python 3.6
    Python 3.6 has reached its end of life : the Python Foundation is no longer issuing security fixes since Dec 23, 2021 (see PEP494).

--- python/build-wheels.sh
+++ python/build-wheels.sh
@@ -2,13 +2,13 @@
 set -e -x
 cd /io/build
 
-export PYTHON_MIN_VERSION=3,6
+export PYTHON_MIN_VERSION=3,7
 export PYTHON_MAX_VERSION=3,11
 
 # Compile C++ code
 for PYBIN in /opt/python/*/bin; do
     # Skip deprecated or unsupported versions
-    if "${PYBIN}/python" -c "import sys;sys.stdout.write(str(sys.version_info < (${PYTHON_MAX_VERSION})))" | grep -q 'True'; then
+    if "${PYBIN}/python" -c "import sys;sys.stdout.write(str(sys.version_info >= (${PYTHON_MIN_VERSION})))" | grep -q 'True'; then
         "${PYBIN}/pip" install cmake
         "${PYBIN}/cmake" -DCMAKE_C_FLAGS_RELEASE="-g -O2 -DNDEBUG -fno-math-errno" -DCMAKE_CXX_FLAGS_RELEASE="-g -O2 -DNDEBUG -fno-math-errno" -DCMAKE_BUILD_TYPE=Release ..
         # Only build libJSBSim because that's all we need for Python wheels.
@@ -22,7 +22,7 @@ cd python
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
     # Skip deprecated or unsupported versions
-    if "${PYBIN}/python" -c "import sys;sys.stdout.write(str(sys.version_info < (${PYTHON_MAX_VERSION})))" | grep -q 'True'; then
+    if "${PYBIN}/python" -c "import sys;sys.stdout.write(str(sys.version_info >= (${PYTHON_MIN_VERSION})))" | grep -q 'True'; then
         "${PYBIN}/pip" install 'cython!=0.29.26' numpy  # Exclude Cython version 0.29.26 as it fails building for PyPy38
         "${PYBIN}/cython" --cplus jsbsim.pyx -o jsbsim.cxx
         "${PYBIN}/python" setup.py bdist_wheel --build-number=$GITHUB_RUN_NUMBER
@@ -37,7 +37,7 @@ done
 # Install packages and test
 for PYBIN in /opt/python/*/bin; do
     # Skip deprecated or unsupported versions
-    if "${PYBIN}/python" -c "import sys;sys.stdout.write(str(sys.version_info < (${PYTHON_MAX_VERSION})))" | grep -q 'True'; then
+    if "${PYBIN}/python" -c "import sys;sys.stdout.write(str(sys.version_info >= (${PYTHON_MIN_VERSION})))" | grep -q 'True'; then
         "${PYBIN}/pip" install jsbsim --no-index -f dist
         "${PYBIN}/python" -c "import jsbsim;fdm=jsbsim.FGFDMExec('.', None);print(jsbsim.FGAircraft.__doc__)"
         "${PYBIN}/JSBSim" --root=../.. --script=scripts/c1721.xml
