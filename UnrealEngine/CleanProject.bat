630f98412447496b509f24f731542470880e8be2
Alban BERGERET (Epic) <59740355+AlbanBERGERET-Epic@users.noreply.github.com>
2022-04-25 18:35:33 -0400

    Fixed scripts

--- UnrealEngine/CleanProject.bat
+++ UnrealEngine/CleanProject.bat
@@ -4,7 +4,8 @@ rd /s /q Saved
 rd /s /q Binaries
 rd /s /q Build
 rd /s /q Intermediate
-
+rd /s /q DerivedDataCache
+rd /s /q Script
 
 rd /s /q Plugins\JSBSimFlightDynamicsModel\Binaries
 rd /s /q Plugins\JSBSimFlightDynamicsModel\Intermediate
