4ed723dbf374081f29027e510dead945de89b7dd
Bertrand Coconnier <bcoconni@users.sf.net>
2022-10-29 16:44:19 +0200

    Triggers a bug in `FGPropertyNode::GetFullyQualifiedName()`
    Getting a segfault when calling `GetFullyQualifiedName()` on the root node of `FGPropertyManager`.

---
+++ tests/unit_tests/FGPropertyManagerTest.h
@@ -0,0 +1,19 @@
+#include <memory>
+
+#include <cxxtest/TestSuite.h>
+#include <input_output/FGPropertyManager.h>
+
+using namespace JSBSim;
+
+
+class FGPropertyManagerTest : public CxxTest::TestSuite
+{
+public:
+  void testConstructor() {
+    auto pm = std::make_shared<FGPropertyManager>();
+    auto root = pm->GetNode();
+
+    TS_ASSERT_EQUALS(root->GetName(), "");
+    TS_ASSERT_EQUALS(root->GetFullyQualifiedName(), "/");
+  }
+};
