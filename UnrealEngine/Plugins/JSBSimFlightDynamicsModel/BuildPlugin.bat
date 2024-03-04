b8b0c7624ae52a84532454004dabb63ec26b0331
tsky <thomas.kollakowsky@gmail.com>
2024-03-04 19:19:41 +0100

    fix iwyu fix/add missing category for blueprint compile with 5.1 add build batch

---
+++ UnrealEngine/Plugins/JSBSimFlightDynamicsModel/BuildPlugin.bat
@@ -0,0 +1,18 @@
+@echo off
+
+set EngineVesion=5.1
+
+for /f "skip=2 tokens=2*" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\EpicGames\Unreal Engine\%EngineVesion%" /v "InstalledDirectory"') do set "EngineDirectory=%%b"
+
+set AutomationToolPath="%EngineDirectory%\Engine\Build\BatchFiles\RunUAT.bat"
+set PluginPath="%cd%\JSBSimFlightDynamicsModel.uplugin"
+set OutputPath="%cd%\..\..\..\..\..\UE_Deployment"
+
+title Build Plugin
+echo Automation Tool Path: %AutomationToolPath%
+echo:
+
+call %AutomationToolPath% BuildPlugin -Plugin=%PluginPath% -Package=%OutputPath% -Rocket -TargetPlatforms=Win64
+echo:
+pause
+exit 0
