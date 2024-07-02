f90f79bd01731fe0a304b2bbd5b02f1680e47a78
Trent Conley <trentconley@machindustries.com>
2024-07-02 11:54:21 -0700

    test

--- src/math/FGFunction.cpp
+++ src/math/FGFunction.cpp
@@ -961,6 +961,11 @@ double FGFunction::GetValue(void) const
 {
   if (cached) return cachedValue;
   std::cout << "computing value for " << this->GetName() << endl;
+
+    for (size_t i = 1; i < Parameters.size(); ++i) {
+      std::cout << "The parameter is " << Parameters[i]->GetName() << endl;
+  }
+
   double val = Parameters[0]->GetValue();
 
   if (pCopyTo) pCopyTo->setDoubleValue(val);
