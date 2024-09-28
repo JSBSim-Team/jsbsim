d926776150cb572856cfc057c4c228d83bdb5497
Sean McLeod <sean@seanmcleod.com>
2024-09-28 22:22:47 +0200

    Consistency for some FGFDMExec properties (#1166)

--- src/FGFDMExec.cpp
+++ src/FGFDMExec.cpp
@@ -163,8 +163,8 @@ FGFDMExec::FGFDMExec(FGPropertyManager* root, std::shared_ptr<unsigned int> fdmc
   instance->Tie("simulation/reset", this, (iPMF)0, &FGFDMExec::ResetToInitialConditions);
   instance->Tie("simulation/disperse", this, &FGFDMExec::GetDisperse);
   instance->Tie("simulation/randomseed", this, (iPMF)&FGFDMExec::SRand, &FGFDMExec::SRand);
-  instance->Tie("simulation/terminate", (int *)&Terminate);
-  instance->Tie("simulation/pause", (int *)&holding);
+  instance->Tie("simulation/terminate", (bool *)&Terminate);
+  instance->Tie("simulation/pause", (bool *)&holding);
   instance->Tie("simulation/sim-time-sec", this, &FGFDMExec::GetSimTime);
   instance->Tie("simulation/dt", this, &FGFDMExec::GetDeltaT);
   instance->Tie("simulation/jsbsim-debug", this, &FGFDMExec::GetDebugLevel, &FGFDMExec::SetDebugLevel);
