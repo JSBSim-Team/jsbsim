a48cf0e6301de42cbb49e9442849caf7e44f8cb5
Sean McLeod <sean.mcleod@seanmcleod.com>
2025-04-21 17:19:54 +0200

    Ensure engine set-running works for piston engines (#1266)
    Co-authored-by: Sean McLeod <sean@seanmcleod.com>

--- src/models/FGPropulsion.cpp
+++ src/models/FGPropulsion.cpp
@@ -59,6 +59,7 @@ INCLUDES
 #include "models/propulsion/FGTurboProp.h"
 #include "models/propulsion/FGTank.h"
 #include "models/propulsion/FGBrushLessDCMotor.h"
+#include "models/FGFCS.h"
 
 
 using namespace std;
@@ -323,22 +324,27 @@ void FGPropulsion::InitRunning(int n)
       throw err;
     }
 
-    in.ThrottleCmd[n] = in.ThrottlePos[n] = 1; // Set the throttle command and position
-    in.MixtureCmd[n] = in.MixturePos[n] = 1;   // Set the mixture command and position
-
-    GetEngine(n)->InitRunning();
-    GetSteadyState();
+    SetEngineRunning(n);
 
   } else if (n < 0) { // -1 refers to "All Engines"
 
     for (unsigned int i=0; i<GetNumEngines(); i++) {
-      in.ThrottleCmd[i] = in.ThrottlePos[i] = 1; // Set the throttle command and position
-      in.MixtureCmd[i] = in.MixturePos[i] = 1;   // Set the mixture command and position
-      GetEngine(i)->InitRunning();
+      SetEngineRunning(i);
     }
-
-    GetSteadyState();
   }
+
+  GetSteadyState();
+}
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+void FGPropulsion::SetEngineRunning(int engineIndex)
+{
+  in.ThrottleCmd[engineIndex] = in.ThrottlePos[engineIndex] = 1; // Set the throttle command and position
+  in.MixtureCmd[engineIndex] = in.MixturePos[engineIndex] = 1;   // Set the mixture command and position
+  FDMExec->GetFCS()->SetMixturePos(engineIndex, 1);    // Also set FCS values     
+  FDMExec->GetFCS()->SetMixtureCmd(engineIndex, 1);
+  GetEngine(engineIndex)->InitRunning();
 }
 
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
