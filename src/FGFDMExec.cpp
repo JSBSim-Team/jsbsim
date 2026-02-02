2bad0f0d07975b3e66f8ab88b9d524e0b46d648a
Bertrand Coconnier <bcoconni@users.noreply.github.com>
2026-02-02 09:21:21 +0100

    Remove hard coded references to `/fdm/jsbsim/` (#1381)

--- src/FGFDMExec.cpp
+++ src/FGFDMExec.cpp
@@ -1086,6 +1086,9 @@ string FGFDMExec::GetPropulsionTankReport() const
 void FGFDMExec::BuildPropertyCatalog(struct PropertyCatalogStructure* pcs)
 {
   auto pcsNew = std::make_unique<struct PropertyCatalogStructure>();
+  const SGPropertyNode* root_node = instance->GetNode();
+  const string root_name = GetFullyQualifiedName(root_node) + "/";
+  const size_t root_name_length = root_name.length();
 
   for (int i=0; i<pcs->node->nChildren(); i++) {
     string access="";
@@ -1095,8 +1098,8 @@ void FGFDMExec::BuildPropertyCatalog(struct PropertyCatalogStructure* pcs)
       pcsNew->base_string = CreateIndexedPropertyName(pcsNew->base_string, node_idx);
     }
     if (pcs->node->getChild(i)->nChildren() == 0) {
-      if (pcsNew->base_string.substr(0,12) == string("/fdm/jsbsim/")) {
-        pcsNew->base_string = pcsNew->base_string.erase(0,12);
+      if (pcsNew->base_string.substr(0, root_name_length) == root_name) {
+        pcsNew->base_string = pcsNew->base_string.erase(0, root_name_length);
       }
       if (pcs->node->getChild(i)->getAttribute(SGPropertyNode::READ)) access="R";
       if (pcs->node->getChild(i)->getAttribute(SGPropertyNode::WRITE)) access+="W";
