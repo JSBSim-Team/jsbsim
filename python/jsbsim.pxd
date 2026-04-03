JSBSim-Team/jsbsim

commit fc8156d20df257f922047ca91ca9438343a5129f

Python binding of `FGLogger` (#1406)

python/jsbsim.pxd
@@ -29,7 +29,7 @@ from libcpp.memory cimport shared_ptr
 from libcpp.vector cimport vector
 from cpython.ref cimport PyObject
 
-cdef extern from "ExceptionManagement.h":
+cdef extern from "ExceptionManagement.h" namespace "JSBSim":
     cdef PyObject* base_error
     cdef PyObject* trimfailure_error
     cdef PyObject* geographic_error
@@ -92,6 +92,17 @@ cdef extern from "input_output/FGPropertyManager.h" namespace "JSBSim":
         c_SGPropertyNode* GetNode()
         c_SGPropertyNode* GetNode(const string& path, bool create)
         bool HasNode(const string& path) except +convertJSBSimToPyExc
+
+cdef extern from "PyLogger.h" namespace "JSBSim":
+    cdef PyObject* FGLogger_PyClass
+    cdef PyObject* LogLevel_PyClass
+    cdef PyObject* LogFormat_PyClass
+    cdef void ResetLogger()
+    cdef cppclass c_PyLogger "JSBSim::PyLogger":
+        c_PyLogger(PyObject* logger)
+
+cdef extern from "input_output/FGLog.h" namespace "JSBSim":
+    cdef void SetLogger(shared_ptr[c_PyLogger] logger)
 
 cdef extern from "math/FGColumnVector3.h" namespace "JSBSim":
     cdef cppclass c_FGColumnVector3 "JSBSim::FGColumnVector3":

python/jsbsim.pyx.in
@@ -28,8 +28,10 @@
    @DoxMainPage"""
 
 from cython.operator cimport dereference as deref
+from libcpp.memory cimport shared_ptr, make_shared
 from typing import Optional
 
+import atexit
 import enum
 import errno
 import os
@@ -86,6 +88,96 @@ cdef _convertToNumpyMat(const c_FGMatrix33& m):
 
 cdef _convertToNumpyVec(const c_FGColumnVector3& v):
     return numpy.matrix([v.Entry(1), v.Entry(2), v.Entry(3)]).T
+
+
+class LogLevel(enum.Enum):
+    """Enumeration of log levels."""
+    BULK = 0   # For frequent messages
+    DEBUG = 1  # Less frequent debug type messages
+    INFO = 2   # Informatory messages
+    WARN = 3   # Possible impending problem
+    ERROR = 4  # Problem that can be recovered
+    FATAL = 5  # Fatal problem => an exception will be thrown
+    STDOUT = 6 # Standard output - unconditionally displayed.
+
+
+class LogFormat(enum.Enum):
+    """Enumeration of log formats."""
+    RESET = 0
+    RED = 1
+    BLUE = 2
+    CYAN = 3
+    GREEN = 4
+    DEFAULT = 5
+    BOLD = 6
+    NORMAL = 7
+    UNDERLINE_ON = 8
+    UNDERLINE_OFF = 9
+
+
+class FGLogger:
+    """@Dox(JSBSim::FGLogger)"""
+    log_level = LogLevel.BULK
+
+    def set_level(self, level: LogLevel) -> None:
+        """@Dox(JSBSim::FGLogger::SetLevel)"""
+        self.log_level = level
+    def file_location(self, filename: str, line: int) -> None:
+        """@Dox(JSBSim::FGLogger::FileLocation)"""
+        pass
+    def message(self, message: str) -> None:
+        """@Dox(JSBSim::FGLogger::Message)"""
+        pass
+    def format(self, format: LogFormat) -> None:
+        """@Dox(JSBSim::FGLogger::Format)"""
+        pass
+    def flush(self) -> None:
+        """@Dox(JSBSim::FGLogger::Flush)"""
+        pass
+
+cdef shared_ptr[c_PyLogger] _logger  # hold the logger instance
+
+def set_logger(logger: FGLogger) -> None:
+    """@Dox(JSBSim::SetLogger)"""
+    global _logger
+    _logger = make_shared[c_PyLogger](<PyObject*>logger)
+    SetLogger(_logger)
+
+
+# Initializing pointers for the C++ interface
+FGLogger_PyClass = <PyObject*>FGLogger
+LogLevel_PyClass = <PyObject*>LogLevel
+LogFormat_PyClass = <PyObject*>LogFormat
+
+
+def inherit_docstring(cls):
+    for name, func in cls.__dict__.items():
+        if not func.__doc__:
+            func.__doc__ = getattr(super(cls, cls), name).__doc__
+    return cls
+
+
+@inherit_docstring
+class DefaultLogger(FGLogger):
+    """Default logger: print messages to stdout without formatting."""
+    def file_location(self, filename: str, line: int) -> None:
+        print(f"\nIn file {filename}: line {line}")
+    def message(self, message: str) -> None:
+        print(f"{message}", end='')
+
+
+# Reset the logger before Python shutdown to prevent crashes.
+# This function is called at module exit to clear the global logger variable.
+# This prevents the c_PyLogger destructor from trying to decrement reference
+# counts on Python objects after Python has already finalized.
+cdef _reset_logger():
+    global _logger
+    _logger.reset()
+    ResetLogger()
+
+
+atexit.register(_reset_logger)
+set_logger(DefaultLogger())
 
 
 cdef class FGPropagate:

python/src/ExceptionManagement.h
@@ -1,28 +1,36 @@
 /* Convert JSBSim exceptions to Python exceptions
  *
- * Copyright (c) 2016 Bertrand Coconnier
+ * Copyright (c) 2016-2026 Bertrand Coconnier
  *
  * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 3 of the License, or (at your option) any later
- * version.
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
  * details.
  *
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, see <http://www.gnu.org/licenses/>
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Further information about the GNU Lesser General Public License can also be
+ * found on the world wide web at http://www.gnu.org.
  */
 
 #include <string>
-#include "fpectl/fpectlmodule.h"
+#include "fpectlmodule.h"
 #include "FGFDMExec.h"
 #include "GeographicLib/Constants.hpp"
 #include "math/FGMatrix33.h"
 #include "math/FGTable.h"
 
+#ifndef EXCEPTIONMANAGEMENT_H
+#define EXCEPTIONMANAGEMENT_H
+
+namespace JSBSim {
 // Pointers to Python exception classes.
 // Their initialization take place in jsbsim.pyx
 PyObject* base_error;
@@ -55,3 +63,5 @@ void convertJSBSimToPyExc()
     PyErr_SetString(PyExc_RuntimeError, msg);
   }
 }
+}
+#endif // EXCEPTIONMANAGEMENT_H

python/src/PyLogger.cxx
@@ -1,0 +1,130 @@
+/*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+ Header:       PyLog.cxx
+ Author:       Bertrand Coconnier
+ Date started: 11/02/24
+
+ ------------- Copyright (C) 2024 Bertrand Coconnier -------------
+
+ This program is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2 of the License, or (at your option) any
+ later version.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ details.
+
+ You should have received a copy of the GNU Lesser General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+ Further information about the GNU Lesser General Public License can also be
+ found on the world wide web at http://www.gnu.org.
+*/
+
+#include <cassert>
+#include "PyLogger.h"
+
+namespace JSBSim {
+// These pointers are initialized in jsbsim.pyx
+PyObject* FGLogger_PyClass;
+PyObject* LogLevel_PyClass;
+PyObject* LogFormat_PyClass;
+
+void ResetLogger(void) { SetLogger(std::make_shared<FGLogConsole>()); }
+
+/*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+CLASS IMPLEMENTATION
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
+
+PyLogger::PyLogger(PyObject* logger)
+{
+  if (PyObject_IsInstance(logger, FGLogger_PyClass)) {
+    logger_pyclass = logger;
+    Py_INCREF(logger);
+
+    convert_level_enums[(int)LogLevel::BULK] = PyObject_GetAttrString(LogLevel_PyClass, "BULK");
+    convert_level_enums[(int)LogLevel::DEBUG] = PyObject_GetAttrString(LogLevel_PyClass, "DEBUG");
+    convert_level_enums[(int)LogLevel::INFO] = PyObject_GetAttrString(LogLevel_PyClass, "INFO");
+    convert_level_enums[(int)LogLevel::WARN] = PyObject_GetAttrString(LogLevel_PyClass, "WARN");
+    convert_level_enums[(int)LogLevel::ERROR] = PyObject_GetAttrString(LogLevel_PyClass, "ERROR");
+    convert_level_enums[(int)LogLevel::FATAL] = PyObject_GetAttrString(LogLevel_PyClass, "FATAL");
+    convert_level_enums[(int)LogLevel::STDOUT] = PyObject_GetAttrString(LogLevel_PyClass, "STDOUT");
+
+    convert_format_enums[(int)LogFormat::RESET] = PyObject_GetAttrString(LogFormat_PyClass, "RESET");
+    convert_format_enums[(int)LogFormat::RED] = PyObject_GetAttrString(LogFormat_PyClass, "RED");
+    convert_format_enums[(int)LogFormat::BLUE] = PyObject_GetAttrString(LogFormat_PyClass, "BLUE");
+    convert_format_enums[(int)LogFormat::CYAN] = PyObject_GetAttrString(LogFormat_PyClass, "CYAN");
+    convert_format_enums[(int)LogFormat::GREEN] = PyObject_GetAttrString(LogFormat_PyClass, "GREEN");
+    convert_format_enums[(int)LogFormat::DEFAULT] = PyObject_GetAttrString(LogFormat_PyClass, "DEFAULT");
+    convert_format_enums[(int)LogFormat::BOLD] = PyObject_GetAttrString(LogFormat_PyClass, "BOLD");
+    convert_format_enums[(int)LogFormat::NORMAL] = PyObject_GetAttrString(LogFormat_PyClass, "NORMAL");
+    convert_format_enums[(int)LogFormat::UNDERLINE_ON] = PyObject_GetAttrString(LogFormat_PyClass, "UNDERLINE_ON");
+    convert_format_enums[(int)LogFormat::UNDERLINE_OFF] = PyObject_GetAttrString(LogFormat_PyClass, "UNDERLINE_OFF");
+  } else {
+    PyErr_SetString(PyExc_TypeError, "The logger must be an instance of FGLogger");
+  }
+}
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+void PyLogger::SetLevel(LogLevel level) {
+  const int idx = static_cast<int>(level);
+  assert(idx >=0 && idx <= 6);
+  PyObjectPtr py_level = convert_level_enums[idx];
+  PyObjectPtr result = CallPythonMethodWithArguments("set_level", py_level);
+  if (result) FGLogger::SetLevel(level);
+}
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+void PyLogger::FileLocation(const std::string& filename, int line)
+{
+  PyObjectPtr py_filename = PyUnicode_FromString(filename.c_str());
+  PyObjectPtr py_line = PyLong_FromLong(line);
+  PyObjectPtr args = PyTuple_Pack(2, py_filename.get(), py_line.get());
+  CallPythonMethodWithTuple("file_location", args);
+}
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+void PyLogger::Message(const std::string& message)
+{
+  PyObjectPtr msg = PyUnicode_FromString(message.c_str());
+  CallPythonMethodWithArguments("message", msg);
+}
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+void PyLogger::Format(LogFormat format)
+{
+  const int idx = static_cast<int>(format);
+  assert(idx >= 0 && idx <= 9);
+  PyObjectPtr py_format = convert_format_enums[idx];
+  CallPythonMethodWithArguments("format", py_format);
+}
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+PyObjectPtr PyLogger::CallPythonMethodWithArguments(const char* method_name, const PyObjectPtr& arg)
+{
+  PyObjectPtr tuple = PyTuple_Pack(1, arg.get());
+  return CallPythonMethodWithTuple(method_name, tuple);
+}
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+PyObjectPtr PyLogger::CallPythonMethodWithTuple(const char* method_name, const PyObjectPtr& tuple)
+{
+  PyObjectPtr method = PyObject_GetAttrString(logger_pyclass.get(), method_name);
+  assert(method); // This should not fail as the constructor has checked the type of logger_pyclass.
+
+  PyObjectPtr result = PyObject_CallObject(method.get(), tuple.get());
+
+  if (!result) PyErr_Print();
+
+  return result;
+}
+} // namespace JSBSim

python/src/PyLogger.h
@@ -1,0 +1,117 @@
+/*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+ Header:       PyLog.h
+ Author:       Bertrand Coconnier
+ Date started: 11/02/24
+
+ ------------- Copyright (C) 2024 Bertrand Coconnier -------------
+
+ This program is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2 of the License, or (at your option) any
+ later version.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ details.
+
+ You should have received a copy of the GNU Lesser General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+ Further information about the GNU Lesser General Public License can also be
+ found on the world wide web at http://www.gnu.org.
+*/
+
+#include <array>
+#include <Python.h>
+#include "input_output/FGLog.h"
+
+#ifndef PYLOGGER_H
+#define PYLOGGER_H
+
+namespace JSBSim {
+/// Pointers to the Python counterparts of C++ classes.
+extern PyObject* FGLogger_PyClass;
+extern PyObject* LogLevel_PyClass;
+extern PyObject* LogFormat_PyClass;
+
+/// Reset the global logger to its default.
+JSBSIM_API void ResetLogger(void);
+
+// Helper class to manage the reference count of a PyObject.
+class PyObjectPtr {
+public:
+  PyObjectPtr(PyObject* obj = nullptr) noexcept
+    : object(obj) {}
+
+  // Copy constructor
+  PyObjectPtr(const PyObjectPtr& other) noexcept {
+    object = other.object;
+    Py_XINCREF(object);
+  }
+
+  // Move constructor
+  PyObjectPtr(PyObjectPtr&& other) noexcept
+    : object(other.object)
+  {
+    other.object = nullptr;
+  }
+
+  // Copy assignment operator
+  PyObjectPtr& operator=(const PyObjectPtr& other) noexcept {
+    if (this != &other) {
+      Py_XDECREF(object); // Decrement the reference count of the current object
+      object = other.object;
+      Py_XINCREF(object); // Increment the reference count of the new object
+    }
+    return *this;
+  }
+
+  PyObjectPtr& operator=(PyObject* src) noexcept {
+    Py_XDECREF(object);
+    object = src;
+    return *this;
+  }
+
+  // Move assignment operator
+  PyObjectPtr& operator=(PyObjectPtr&& other) noexcept {
+    if (this != &other) {
+      Py_XDECREF(object); // Decrement the reference count of the current object
+      object = other.object;
+      other.object = nullptr; // Prevent the source from decrementing the reference count
+    }
+    return *this;
+  }
+
+  ~PyObjectPtr() noexcept { Py_XDECREF(object); }
+
+  PyObject* get() const noexcept { return object; }
+  operator bool() const noexcept { return object != nullptr; }
+
+protected:
+  PyObject* object;
+};
+
+
+class PyLogger : public FGLogger
+{
+public:
+  explicit PyLogger(PyObject* logger);
+  void SetLevel(LogLevel level) override;
+  void FileLocation(const std::string& filename, int line) override;
+  void Message(const std::string& message) override;
+  void Format(LogFormat format) override;
+  void Flush(void) override { CallPythonMethodWithTuple("flush", nullptr); }
+
+private:
+  PyObjectPtr CallPythonMethodWithTuple(const char* method_name, const PyObjectPtr& tuple);
+  PyObjectPtr CallPythonMethodWithArguments(const char* method_name, const PyObjectPtr& arg);
+
+  PyObjectPtr logger_pyclass;
+  std::array<PyObjectPtr, 7> convert_level_enums;
+  std::array<PyObjectPtr, 10> convert_format_enums;
+};
+}
+#endif

python/CMakeLists.txt
@@ -50,8 +50,10 @@ install(DIRECTORY ${PROJECT_SOURCE_DIR}/systems DESTINATION jsbsim COMPONENT whe
 install(DIRECTORY ${PROJECT_SOURCE_DIR}/scripts DESTINATION jsbsim COMPONENT wheel)
 
 # Build the Python module
-python3_add_library(_jsbsim MODULE ${JSBSIM_CXX})
-target_include_directories(_jsbsim PRIVATE ${PROJECT_SOURCE_DIR}/src ${PROJECT_SOURCE_DIR}/python)
+python3_add_library(_jsbsim MODULE ${JSBSIM_CXX} ${CMAKE_CURRENT_SOURCE_DIR}/src/PyLogger.cxx)
+target_include_directories(_jsbsim PRIVATE ${PROJECT_SOURCE_DIR}/src
+                                           ${CMAKE_CURRENT_SOURCE_DIR}/src
+                                           ${CMAKE_CURRENT_SOURCE_DIR}/fpectl)
 target_link_libraries(_jsbsim PRIVATE libJSBSim)
 set_target_properties(_jsbsim PROPERTIES LIBRARY_OUTPUT_DIRECTORY ${JSBSIM_TEST_PACKAGE_DIR})
 install(TARGETS _jsbsim DESTINATION jsbsim COMPONENT wheel)

python/__init__.py
@@ -13,6 +13,7 @@ from ._jsbsim import (
     FGJSBBase,
     FGLGear,
     FGLinearization,
+    FGLogger,
     FGMassBalance,
     FGPropagate,
     FGPropertyManager,
@@ -20,6 +21,8 @@ from ._jsbsim import (
     FGPropulsion,
     GeographicError,
     TrimFailureError,
+    LogLevel,
+    LogFormat,
     ePressure,
     eTemperature,
     get_default_root_dir,

python/cython.lark
@@ -36,11 +36,11 @@ ctype: name | ref_type | pointer_type | const_type | ctemplate_type
 
 conversion: "<" ctype ">" name
 new_param: "new" name "(" [arguments] ")"
-cyassign: testlist_star_expr "=" (testlist_star_expr "=")* (new_param | conversion)
+cyassign: testlist_star_expr "=" (testlist_star_expr "=")* (new_param | conversion | ctemplate_type "(" [cparameters] ")")
 ?ctypedparam: ctype name
-cparameter: (ctypedparam ("=" test)?) | paramvalue
+cparameter: (ctypedparam ("=" test)?) | conversion | paramvalue
 cparameters: cparameter ("," cparameter)* ["," [starparams | kwparams]]
-cyfuncdef: name "(" cparameters ")" ":" suite
+cyfuncdef: name "(" [cparameters] ")" ":" suite
 cyclassdef: "class" name ["(" [arguments] ")"] ":" suite
 cdeclare_var: ctypedparam ("=" "&"? argvalue)? _NEWLINE
 cdef_stmt: "cdef" (cdeclare_var | cyfuncdef | cyclassdef)

commit 6c0e656f5b6983371c4734ebb55965d8387262f5

Fix the bug introduced by the PR #1406. (#1413)

python/jsbsim.pxd
@@ -33,6 +33,7 @@ cdef extern from "ExceptionManagement.h" namespace "JSBSim":
     cdef PyObject* base_error
     cdef PyObject* trimfailure_error
     cdef PyObject* geographic_error
+    cdef PyObject* logexception_error
     cdef void convertJSBSimToPyExc()
 
 cdef extern from "initialization/FGInitialCondition.h" namespace "JSBSim":

python/jsbsim.pyx.in
@@ -58,10 +58,14 @@ class GeographicError(BaseError):
     """Exception class for geographic computation errors."""
     pass
 
+class LogExceptionError(BaseError):
+    """Exception class for errors carrying detailed logging information."""
+    pass
 
 base_error = <PyObject*>BaseError
 trimfailure_error = <PyObject*>TrimFailureError
 geographic_error = <PyObject*>GeographicError
+logexception_error = <PyObject*>LogExceptionError
 
 
 def get_default_root_dir() -> str:

python/src/ExceptionManagement.h
@@ -36,7 +36,7 @@ namespace JSBSim {
 PyObject* base_error;
 PyObject* trimfailure_error;
 PyObject* geographic_error;
-PyObject* table_error;
+PyObject* logexception_error;
 
 void convertJSBSimToPyExc()
 {
@@ -49,6 +49,9 @@ void convertJSBSimToPyExc()
   }
   catch (const GeographicLib::GeographicErr& e) {
     PyErr_SetString(geographic_error, e.what());
+  }
+  catch (const JSBSim::LogException& e) {
+    PyErr_SetString(logexception_error, e.what());
   }
   catch (const JSBSim::BaseException& e) {
     PyErr_SetString(base_error, e.what());

python/src/PyLogger.cxx
@@ -29,9 +29,45 @@
 
 namespace JSBSim {
 // These pointers are initialized in jsbsim.pyx
+extern PyObject* logexception_error;
 PyObject* FGLogger_PyClass;
 PyObject* LogLevel_PyClass;
 PyObject* LogFormat_PyClass;
+
+/** Helper class to intercept and rethrow Python exceptions using RAII.
+ *  The constructor fetches the current Python exception, clearing the error
+ *  state of the thread. The destructor restores the exception state.
+ */
+class PyExceptionHandler {
+public:
+  /// Constructor - Catch the current Python error, if any.
+  explicit PyExceptionHandler(const PyObjectPtr& context)
+    : context_ref(context) { PyErr_Fetch(&type, &value, &traceback); }
+
+  /// Destructor - Manage the errors then re-raise the Python error, if there was one.
+  ~PyExceptionHandler() {
+    if (type) { // If an initial JSBSim error was pending (Error A)
+      if (PyErr_Occurred()) // And if the logger call also failed (Error B)
+        PyErr_WriteUnraisable(context_ref.get()); // Report Error B as unraisable to prevent it from overwriting Error A
+
+      PyErr_Restore(type, value, traceback); // Restore Error A
+    }
+    // If type is NULL (no initial Error A), any Error B is left in the thread
+    // state and will be naturally raised when returning to Python.
+  }
+
+  /** Returns true if there is no current Python error OR if the caught
+   *  error matches the given exception type.
+   */
+  bool NoExceptionOrMatches(PyObject* exc) const {
+    return !type || PyErr_GivenExceptionMatches(type, exc);
+  }
+private:
+  PyObject* type = nullptr;
+  PyObject* value = nullptr;
+  PyObject* traceback = nullptr;
+  PyObjectPtr context_ref;
+};
 
 void ResetLogger(void) { SetLogger(std::make_shared<FGLogConsole>()); }
 
@@ -71,44 +107,70 @@ PyLogger::PyLogger(PyObject* logger)
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 void PyLogger::SetLevel(LogLevel level) {
-  const int idx = static_cast<int>(level);
-  assert(idx >=0 && idx <= 6);
-  PyObjectPtr py_level = convert_level_enums[idx];
-  PyObjectPtr result = CallPythonMethodWithArguments("set_level", py_level);
-  if (result) FGLogger::SetLevel(level);
+  PyExceptionHandler handler(logger_pyclass);
+
+  if (handler.NoExceptionOrMatches(logexception_error)) {
+    const int idx = static_cast<int>(level);
+    assert(idx >= 0 && idx <= 6);
+    PyObjectPtr py_level = convert_level_enums[idx];
+    PyObjectPtr result = CallPythonMethodWithOneArgument("set_level", py_level);
+    if (result) FGLogger::SetLevel(level);
+  }
 }
 
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 void PyLogger::FileLocation(const std::string& filename, int line)
 {
-  PyObjectPtr py_filename = PyUnicode_FromString(filename.c_str());
-  PyObjectPtr py_line = PyLong_FromLong(line);
-  PyObjectPtr args = PyTuple_Pack(2, py_filename.get(), py_line.get());
-  CallPythonMethodWithTuple("file_location", args);
+  PyExceptionHandler handler(logger_pyclass);
+
+  if (handler.NoExceptionOrMatches(logexception_error)) {
+    PyObjectPtr py_filename = PyUnicode_FromString(filename.c_str());
+    PyObjectPtr py_line = PyLong_FromLong(line);
+    PyObjectPtr args = PyTuple_Pack(2, py_filename.get(), py_line.get());
+    CallPythonMethodWithTuple("file_location", args);
+  }
 }
 
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 void PyLogger::Message(const std::string& message)
 {
-  PyObjectPtr msg = PyUnicode_FromString(message.c_str());
-  CallPythonMethodWithArguments("message", msg);
+  PyExceptionHandler handler(logger_pyclass);
+
+  if (handler.NoExceptionOrMatches(logexception_error)) {
+    PyObjectPtr msg = PyUnicode_FromString(message.c_str());
+    CallPythonMethodWithOneArgument("message", msg);
+  }
 }
 
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 void PyLogger::Format(LogFormat format)
 {
-  const int idx = static_cast<int>(format);
-  assert(idx >= 0 && idx <= 9);
-  PyObjectPtr py_format = convert_format_enums[idx];
-  CallPythonMethodWithArguments("format", py_format);
+  PyExceptionHandler handler(logger_pyclass);
+
+  if (handler.NoExceptionOrMatches(logexception_error)) {
+    const int idx = static_cast<int>(format);
+    assert(idx >= 0 && idx <= 9);
+    PyObjectPtr py_format = convert_format_enums[idx];
+    CallPythonMethodWithOneArgument("format", py_format);
+  }
 }
 
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-PyObjectPtr PyLogger::CallPythonMethodWithArguments(const char* method_name, const PyObjectPtr& arg)
+void PyLogger::Flush(void)
+{
+  PyExceptionHandler handler(logger_pyclass);
+
+  if (handler.NoExceptionOrMatches(logexception_error))
+    CallPythonMethodWithTuple("flush", nullptr);
+}
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+PyObjectPtr PyLogger::CallPythonMethodWithOneArgument(const char* method_name, const PyObjectPtr& arg)
 {
   PyObjectPtr tuple = PyTuple_Pack(1, arg.get());
   return CallPythonMethodWithTuple(method_name, tuple);
@@ -121,10 +183,6 @@ PyObjectPtr PyLogger::CallPythonMethodWithTuple(const char* method_name, const P
   PyObjectPtr method = PyObject_GetAttrString(logger_pyclass.get(), method_name);
   assert(method); // This should not fail as the constructor has checked the type of logger_pyclass.
 
-  PyObjectPtr result = PyObject_CallObject(method.get(), tuple.get());
-
-  if (!result) PyErr_Print();
-
-  return result;
+  return PyObject_CallObject(method.get(), tuple.get());
 }
 } // namespace JSBSim

python/src/PyLogger.h
@@ -103,11 +103,11 @@ public:
   void FileLocation(const std::string& filename, int line) override;
   void Message(const std::string& message) override;
   void Format(LogFormat format) override;
-  void Flush(void) override { CallPythonMethodWithTuple("flush", nullptr); }
+  void Flush(void) override;
 
 private:
   PyObjectPtr CallPythonMethodWithTuple(const char* method_name, const PyObjectPtr& tuple);
-  PyObjectPtr CallPythonMethodWithArguments(const char* method_name, const PyObjectPtr& arg);
+  PyObjectPtr CallPythonMethodWithOneArgument(const char* method_name, const PyObjectPtr& arg);
 
   PyObjectPtr logger_pyclass;
   std::array<PyObjectPtr, 7> convert_level_enums;