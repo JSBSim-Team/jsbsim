4f87b78fa2f692eb52a6a0a19a2299debe6b5edd
Sean McLeod <sean@seanmcleod.com>
2026-04-13 21:12:06 +0200

    Update Python DefaultLogger to handle LogLevel filtering

---
+++ tests/TestPythonDefaultLoggerFiltering.py
@@ -0,0 +1,53 @@
+# TestPythonDefaultLoggerFiltering.py
+#
+# Test that the default logger is filtering messages according to the log level. 
+#
+# Copyright (c) 2015-2026 Sean McLeod
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, see <http://www.gnu.org/licenses/>
+#
+
+from JSBSim_utils import (ExecuteUntil, JSBSimTestCase, RunTest)
+from jsbsim import (DefaultLogger, LogLevel)
+
+
+class TestPythonDefaultLoggerFiltering(JSBSimTestCase):
+
+    def test_logoutput_fatal(self):
+        """Confirm that no log output occurs for LogLevel.FATAL"""
+        logger = DefaultLogger()
+        logger.set_min_level(LogLevel.FATAL)
+        jsbsim.set_logger(logger)
+
+        fdm = self.create_fdm()
+        fdm.load_script(self.sandbox.path_to_jsbsim_file('scripts', 'ball_orbit.xml'))
+        fdm.run_ic()
+        ExecuteUntil(fdm, 1000.)
+
+        self.assertEqual(logger.buffer, "")
+
+    def test_logoutput_bulk(self):
+        """Confirm that log output occurs for LogLevel.BULK"""
+        logger = DefaultLogger()
+        logger.set_min_level(LogLevel.BULK)
+        jsbsim.set_logger(logger)
+
+        fdm = self.create_fdm()
+        fdm.load_script(self.sandbox.path_to_jsbsim_file('scripts', 'ball_orbit.xml'))
+        fdm.run_ic()
+        ExecuteUntil(fdm, 1000.)
+
+        self.assertNotEqual(logger.buffer, "")
+
+RunTest(TestPythonDefaultLoggerFiltering)
